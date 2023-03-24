6b19c632ebe45decaa3777103cfc9b67b10cc23f
Laurent Montel <montel@kde.org>
2023-03-24 13:58:30 +0100

    Avoid to duplicate code

---
+++ textaddonswidgets/lineeditcatchreturnkey.h
@@ -0,0 +1,31 @@
+/*
+   SPDX-FileCopyrightText: 2020-2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "textaddonswidgets_export.h"
+#include <QObject>
+class QLineEdit;
+namespace TextAddonsWidgets
+{
+/**
+ * @brief The LineEditCatchReturnKey class
+ * @author Laurent Montel <montel@kde.org>
+ */
+class TEXTADDONSWIDGETS_EXPORT LineEditCatchReturnKey : public QObject
+{
+    Q_OBJECT
+public:
+    explicit LineEditCatchReturnKey(QLineEdit *lineEdit, QObject *parent = nullptr);
+    ~LineEditCatchReturnKey() override;
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
+private:
+    QLineEdit *const mLineEdit;
+};
+}
