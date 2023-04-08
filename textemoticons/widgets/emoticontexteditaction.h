bb4fdf0c22832641c72ddb32ebe3689afb1392d2
Laurent Montel <montel@kde.org>
2023-04-08 09:55:20 +0200

    Add emoticontexteditaction

---
+++ textemoticons/widgets/emoticontexteditaction.h
@@ -0,0 +1,28 @@
+/*
+  SPDX-FileCopyrightText: 2012-2023 Laurent Montel <montel@kde.org>
+  based on code from kopete
+
+  SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "textemoticonswidgets_export.h"
+#include <KActionMenu>
+namespace TextEmoticonsWidgets
+{
+class TEXTEMOTICONSWIDGETS_EXPORT EmoticonTextEditAction : public KActionMenu
+{
+    Q_OBJECT
+public:
+    explicit EmoticonTextEditAction(QObject *parent);
+    ~EmoticonTextEditAction() override;
+
+Q_SIGNALS:
+    void emoticonActivated(const QString &);
+
+private:
+    class EmoticonTextEditActionPrivate;
+    std::unique_ptr<EmoticonTextEditActionPrivate> const d;
+};
+}
