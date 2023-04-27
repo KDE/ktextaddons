fd436ff55d9587e50fd79f9ebf1c1e206e7728ac
Laurent Montel <montel@kde.org>
2023-04-27 07:57:34 +0200

    Add copyright

--- textemoticons/Messages.sh
+++ textemoticons/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
-# SPDX-FileCopyrightText: none
+# SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libtextemoticons.pot
