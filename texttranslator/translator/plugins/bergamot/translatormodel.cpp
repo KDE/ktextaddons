23a4cd19b839e7286bc5ec0af323bfb4593725ed
Laurent Montel <montel@kde.org>
2023-08-04 07:11:35 +0200

    improve model

--- texttranslator/translator/plugins/bergamot/translatormodel.cpp
+++ texttranslator/translator/plugins/bergamot/translatormodel.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "translatormodel.h"
+#include <KLocalizedString>
 
 TranslatorModel::TranslatorModel(QObject *parent)
     : QAbstractListModel{parent}
@@ -12,3 +13,62 @@ TranslatorModel::TranslatorModel(QObject *parent)
 }
 
 TranslatorModel::~TranslatorModel() = default;
+
+void TranslatorModel::insertTranslators(const QVector<Translator> &translators)
+{
+    if (rowCount() != 0) {
+        beginResetModel();
+        mTranslators.clear();
+        endResetModel();
+    }
+    if (!translators.isEmpty()) {
+        beginInsertRows(QModelIndex(), 0, translators.count() - 1);
+        mTranslators = translators;
+        endInsertRows();
+    }
+}
+
+int TranslatorModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return mTranslators.count();
+}
+
+QVariant TranslatorModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() >= mTranslators.count()) {
+        return {};
+    }
+    const auto translator = mTranslators.at(index.row());
+    switch (role) {
+    case TranslatorModel::Source: {
+        return translator.source();
+    }
+    case TranslatorModel::Target: {
+        return translator.target();
+    }
+    case TranslatorModel::TypeTranslator: {
+        return translator.type();
+    }
+    case Qt::DisplayRole:
+    case TranslatorModel::Repository: {
+        return translator.repository();
+    }
+    case TranslatorModel::Version: {
+        return translator.version();
+    }
+    case TranslatorModel::Available: {
+        // TODO return translator.version();
+    }
+    }
+    return {};
+}
+
+void TranslatorModel::clear()
+{
+    if (rowCount() != 0) {
+        beginResetModel();
+        mTranslators.clear();
+        endResetModel();
+    }
+}
--- texttranslator/translator/plugins/bergamot/translatormodel.h
+++ texttranslator/translator/plugins/bergamot/translatormodel.h
@@ -6,12 +6,35 @@
 
 #pragma once
 #include "libbergamot_private_export.h"
+#include "translator.h"
 #include <QAbstractListModel>
 
 class LIBBERGAMOT_TESTS_EXPORT TranslatorModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
+    enum TranslatorRoles {
+        Source = Qt::UserRole + 1,
+        Target,
+        TypeTranslator,
+        Repository,
+        Version,
+        Available,
+
+    };
+
+    Q_ENUM(TranslatorRoles)
+
     explicit TranslatorModel(QObject *parent = nullptr);
     ~TranslatorModel() override;
+
+    Q_REQUIRED_RESULT int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    Q_REQUIRED_RESULT QVariant data(const QModelIndex &index, int role) const override;
+
+    void clear();
+
+    void insertTranslators(const QVector<Translator> &translators);
+
+private:
+    QVector<Translator> mTranslators;
 };
