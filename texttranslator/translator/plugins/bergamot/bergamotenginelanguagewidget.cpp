3b77ae87e24fd8bd7047d050436b0f59cc5b007e
Laurent Montel <montel@kde.org>
2023-08-05 10:15:59 +0200

    Add clear button

--- texttranslator/translator/plugins/bergamot/bergamotenginelanguagewidget.cpp
+++ texttranslator/translator/plugins/bergamot/bergamotenginelanguagewidget.cpp
@@ -39,6 +39,7 @@ BergamotEngineLanguageWidget::BergamotEngineLanguageWidget(QWidget *parent)
     mSearchLineEdit->setObjectName(QStringLiteral("mSearchLineEdit"));
     vboxLayout->addWidget(mSearchLineEdit);
     mSearchLineEdit->setPlaceholderText(i18n("Search..."));
+    mSearchLineEdit->setClearButtonEnabled(true);
 
 #if QT_VERSION < QT_VERSION_CHECK(6, 4, 0)
     new TextAddonsWidgets::LineEditCatchReturnKey(mSearchLineEdit, this);
