e68b25e8fc09c753ccafd57599653e04e52e9ddf
Laurent Montel <montel@kde.org>
2023-08-19 11:24:30 +0200

    There is not ui file

--- textcustomeditor/Messages.sh
+++ textcustomeditor/Messages.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: none
-$EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libtextcustomeditor.pot
-rm -f rc.cpp
