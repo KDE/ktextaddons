03ccc65d094977b3a500ee0fb124e0f7570099f1
Laurent Montel <montel@kde.org>
2023-09-05 13:36:36 +0200

    Add translatorconfigurelistswidget test gui

---
+++ texttranslator/translator/tests/translatorconfigurelistswidget_gui.cpp
@@ -0,0 +1,27 @@
+/*
+  SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "translator/widgets/translatorconfigurelistswidget.h"
+
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.process(app);
+
+    auto w = new TextTranslator::TranslatorConfigureListsWidget();
+    w->show();
+    app.exec();
+    delete w;
+    return 0;
+}
