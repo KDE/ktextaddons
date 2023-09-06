0649a5477d5f19c9c9942941e9a2e9c8b2cc1387
Laurent Montel <montel@kde.org>
2023-09-06 08:25:10 +0200

    Add speechtotextengineloader

---
+++ textspeechtotext/speechtotext/speechtotextengineloader.cpp
@@ -0,0 +1,113 @@
+/*
+  SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "speechtotextengineloader.h"
+#include "speechtotextclient.h"
+#include "textspeechtotext_debug.h"
+#include <QCoreApplication>
+#include <QDir>
+#include <QPluginLoader>
+using namespace TextSpeechToText;
+class TextSpeechToText::SpeechToTextEngineLoaderPrivate
+{
+public:
+    QSet<QString> loadedPlugins;
+    QHash<QString, SpeechToTextClient *> translatorClients;
+};
+
+SpeechToTextEngineLoader *SpeechToTextEngineLoader::self()
+{
+    static SpeechToTextEngineLoader s_self;
+    return &s_self;
+}
+
+SpeechToTextEngineLoader::SpeechToTextEngineLoader(QObject *parent)
+    : QObject{parent}
+    , d(new TextSpeechToText::SpeechToTextEngineLoaderPrivate)
+{
+    loadPlugins();
+}
+
+SpeechToTextEngineLoader::~SpeechToTextEngineLoader() = default;
+
+void SpeechToTextEngineLoader::loadPlugins()
+{
+    const QStringList libPaths = QCoreApplication::libraryPaths();
+    const QString pathSuffix(QStringLiteral("/kf" QT_STRINGIFY(QT_VERSION_MAJOR)) + QStringLiteral("/translator/"));
+    for (const QString &libPath : libPaths) {
+        QDir dir(libPath + pathSuffix);
+        if (!dir.exists()) {
+            continue;
+        }
+        for (const QString &fileName : dir.entryList(QDir::Files)) {
+            loadPlugin(dir.absoluteFilePath(fileName));
+        }
+    }
+    if (d->loadedPlugins.isEmpty()) {
+        qCWarning(TEXTSPEECHTOTEXT_LOG) << "No translator plugins available!";
+    }
+}
+
+void SpeechToTextEngineLoader::loadPlugin(const QString &pluginPath)
+{
+    QPluginLoader plugin(pluginPath);
+    const QString pluginIID = plugin.metaData()[QStringLiteral("IID")].toString();
+    if (!pluginIID.isEmpty()) {
+        if (d->loadedPlugins.contains(pluginIID)) {
+            qCDebug(TEXTSPEECHTOTEXT_LOG) << "Skipping already loaded" << pluginPath;
+            return;
+        }
+        d->loadedPlugins.insert(pluginIID);
+    }
+
+    if (!plugin.load()) { // We do this separately for better error handling
+        qCDebug(TEXTSPEECHTOTEXT_LOG) << "Unable to load plugin" << pluginPath << "Error:" << plugin.errorString();
+        d->loadedPlugins.remove(pluginIID);
+        return;
+    }
+    SpeechToTextClient *client = qobject_cast<SpeechToTextClient *>(plugin.instance());
+    if (!client) {
+        qCWarning(TEXTSPEECHTOTEXT_LOG) << "Invalid plugin loaded" << pluginPath;
+        plugin.unload(); // don't leave it in memory
+        return;
+    }
+    // TODO d->translatorClients.insert(client->name(), client);
+}
+
+SpeechToTextClient *SpeechToTextEngineLoader::createTranslatorClient(const QString &clientName)
+{
+    auto clientsItr = d->translatorClients.constFind(clientName);
+    if (clientsItr == d->translatorClients.constEnd()) {
+        qCWarning(TEXTSPEECHTOTEXT_LOG) << "Client name not found: " << clientName;
+        Q_EMIT loadingTranslatorFailed();
+        return nullptr;
+    }
+    return (*clientsItr);
+}
+
+bool SpeechToTextEngineLoader::hasConfigurationDialog(const QString &clientName) const
+{
+    auto clientsItr = d->translatorClients.constFind(clientName);
+    if (clientsItr == d->translatorClients.constEnd()) {
+        qCWarning(TEXTSPEECHTOTEXT_LOG) << "Client name not found: " << clientName;
+        return false;
+    }
+    // TODO
+    return false;
+    //    return (*clientsItr)->hasConfigurationDialog();
+}
+
+bool SpeechToTextEngineLoader::showConfigureDialog(const QString &clientName, QWidget *parentWidget)
+{
+    auto clientsItr = d->translatorClients.constFind(clientName);
+    if (clientsItr == d->translatorClients.constEnd()) {
+        qCWarning(TEXTSPEECHTOTEXT_LOG) << "Client name not found: " << clientName;
+        return false;
+    }
+    // return (*clientsItr)->showConfigureDialog(parentWidget);
+    // TODO
+    return false;
+}
---
+++ textspeechtotext/speechtotext/speechtotextengineloader.h
@@ -0,0 +1,39 @@
+/*
+  SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "textspeechtotext_export.h"
+#include <QObject>
+#include <memory>
+namespace TextSpeechToText
+{
+class SpeechToTextEngineLoaderPrivate;
+class SpeechToTextClient;
+class TEXTSPEECHTOTEXT_EXPORT SpeechToTextEngineLoader : public QObject
+{
+    Q_OBJECT
+public:
+    static SpeechToTextEngineLoader *self();
+
+    explicit SpeechToTextEngineLoader(QObject *parent = nullptr);
+    ~SpeechToTextEngineLoader() override;
+
+    Q_REQUIRED_RESULT TextSpeechToText::SpeechToTextClient *createTranslatorClient(const QString &clientName);
+
+    Q_REQUIRED_RESULT bool hasConfigurationDialog(const QString &clientName) const;
+
+    Q_REQUIRED_RESULT bool showConfigureDialog(const QString &clientName, QWidget *parentWidget);
+
+Q_SIGNALS:
+    void loadingTranslatorFailed();
+
+private:
+    TEXTSPEECHTOTEXT_NO_EXPORT void loadPlugins();
+    TEXTSPEECHTOTEXT_NO_EXPORT void loadPlugin(const QString &pluginPath);
+    std::unique_ptr<SpeechToTextEngineLoaderPrivate> const d;
+};
+}
