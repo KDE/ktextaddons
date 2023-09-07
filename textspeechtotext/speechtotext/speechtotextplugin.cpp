cd965a5381c79f5782510183a3b659076d250b8b
Laurent Montel <montel@kde.org>
2023-09-07 07:40:13 +0200

    Return result

--- textspeechtotext/speechtotext/speechtotextplugin.cpp
+++ textspeechtotext/speechtotext/speechtotextplugin.cpp
@@ -7,11 +7,23 @@
 #include "speechtotextplugin.h"
 using namespace TextSpeechToText;
 
+class TextSpeechToText::SpeechToTextPluginPrivate
+{
+public:
+    QString mResult;
+};
+
 SpeechToTextPlugin::SpeechToTextPlugin(QObject *parent)
     : QObject{parent}
+    , d(new TextSpeechToText::SpeechToTextPluginPrivate())
 {
 }
 
 SpeechToTextPlugin::~SpeechToTextPlugin() = default;
+
+QString SpeechToTextPlugin::result() const
+{
+    return d->mResult;
+}
 
 #include "moc_speechtotextplugin.cpp"
--- textspeechtotext/speechtotext/speechtotextplugin.h
+++ textspeechtotext/speechtotext/speechtotextplugin.h
@@ -7,17 +7,27 @@
 #pragma once
 #include "textspeechtotext_export.h"
 #include <QObject>
+#include <memory.h>
 namespace TextSpeechToText
 {
 /**
  * @brief The SpeechToTextPlugin class
  * @author Laurent Montel <montel@kde.org>
  */
+class SpeechToTextPluginPrivate;
 class TEXTSPEECHTOTEXT_EXPORT SpeechToTextPlugin : public QObject
 {
     Q_OBJECT
 public:
     explicit SpeechToTextPlugin(QObject *parent = nullptr);
     ~SpeechToTextPlugin() override;
+
+    Q_REQUIRED_RESULT QString result() const;
+
+Q_SIGNALS:
+    void speechToTextDone();
+
+private:
+    std::unique_ptr<SpeechToTextPluginPrivate> const d;
 };
 }
