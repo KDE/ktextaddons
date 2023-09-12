b4c9d573d2072317b6afc033623dd760c0c3ce83
Laurent Montel <montel@kde.org>
2023-09-12 08:36:21 +0200

    Add speechtotextconfigurewidgettest

---
+++ textspeechtotext/speechtotext/widgets/autotests/speechtotextconfigurewidgettest.cpp
@@ -0,0 +1,12 @@
+/*
+  SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "speechtotextconfigurewidgettest.h"
+#include <QTest>
+QTEST_MAIN(SpeechToTextConfigureWidgetTest)
+SpeechToTextConfigureWidgetTest::SpeechToTextConfigureWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
