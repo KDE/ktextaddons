0b95e81663306405547188598b6fcfe11c35f52a
Laurent Montel <montel@kde.org>
2023-10-08 13:46:05 +0200

    Add voskspeechtotextinfo

--- textspeechtotext/speechtotext/plugins/vosk/voskspeechtotextinfo.cpp
+++ textspeechtotext/speechtotext/plugins/vosk/voskspeechtotextinfo.cpp
@@ -9,3 +9,85 @@
 VoskSpeechToTextInfo::VoskSpeechToTextInfo() = default;
 
 VoskSpeechToTextInfo::~VoskSpeechToTextInfo() = default;
+
+QString VoskSpeechToTextInfo::lang() const
+{
+    return mLang;
+}
+
+void VoskSpeechToTextInfo::setLang(const QString &newLang)
+{
+    mLang = newLang;
+}
+
+QString VoskSpeechToTextInfo::langText() const
+{
+    return mLangText;
+}
+
+void VoskSpeechToTextInfo::setLangText(const QString &newLangText)
+{
+    mLangText = newLangText;
+}
+
+QString VoskSpeechToTextInfo::md5() const
+{
+    return mMd5;
+}
+
+void VoskSpeechToTextInfo::setMd5(const QString &newMd5)
+{
+    mMd5 = newMd5;
+}
+
+QString VoskSpeechToTextInfo::version() const
+{
+    return mVersion;
+}
+
+void VoskSpeechToTextInfo::setVersion(const QString &newVersion)
+{
+    mVersion = newVersion;
+}
+
+QString VoskSpeechToTextInfo::url() const
+{
+    return mUrl;
+}
+
+void VoskSpeechToTextInfo::setUrl(const QString &newUrl)
+{
+    mUrl = newUrl;
+}
+
+quint64 VoskSpeechToTextInfo::size() const
+{
+    return mSize;
+}
+
+void VoskSpeechToTextInfo::setSize(quint64 newSize)
+{
+    mSize = newSize;
+}
+
+bool VoskSpeechToTextInfo::obsolete() const
+{
+    return mObsolete;
+}
+
+void VoskSpeechToTextInfo::setObsolete(bool newObsolete)
+{
+    mObsolete = newObsolete;
+}
+
+bool VoskSpeechToTextInfo::operator==(const VoskSpeechToTextInfo &other) const
+{
+    // TODO
+    return false;
+}
+
+QDebug operator<<(QDebug d, const VoskSpeechToTextInfo &t)
+{
+    // TODO
+    return d;
+}
--- textspeechtotext/speechtotext/plugins/vosk/voskspeechtotextinfo.h
+++ textspeechtotext/speechtotext/plugins/vosk/voskspeechtotextinfo.h
@@ -5,10 +5,46 @@
 */
 
 #pragma once
+#include <QJsonObject>
+#include <QString>
 
 class VoskSpeechToTextInfo
 {
 public:
     VoskSpeechToTextInfo();
     ~VoskSpeechToTextInfo();
+    Q_REQUIRED_RESULT QString lang() const;
+    void setLang(const QString &newLang);
+
+    Q_REQUIRED_RESULT QString langText() const;
+    void setLangText(const QString &newLangText);
+
+    Q_REQUIRED_RESULT QString md5() const;
+    void setMd5(const QString &newMd5);
+
+    Q_REQUIRED_RESULT QString version() const;
+    void setVersion(const QString &newVersion);
+
+    Q_REQUIRED_RESULT QString url() const;
+    void setUrl(const QString &newUrl);
+
+    Q_REQUIRED_RESULT quint64 size() const;
+    void setSize(quint64 newSize);
+
+    Q_REQUIRED_RESULT bool obsolete() const;
+    void setObsolete(bool newObsolete);
+
+    Q_REQUIRED_RESULT bool operator==(const VoskSpeechToTextInfo &other) const;
+
+private:
+    QString mLang;
+    QString mLangText;
+    QString mMd5;
+    QString mVersion;
+    QString mUrl;
+    quint64 mSize = 0;
+    bool mObsolete = false;
 };
+QDebug operator<<(QDebug d, const VoskSpeechToTextInfo &t);
+Q_DECLARE_METATYPE(VoskSpeechToTextInfo)
+Q_DECLARE_TYPEINFO(VoskSpeechToTextInfo, Q_MOVABLE_TYPE);
