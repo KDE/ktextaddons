a6475da35660fa312c7606fd39f548464d238f94
Laurent Montel <montel@kde.org>
2023-10-08 18:04:15 +0200

    Implement autotest

--- textspeechtotext/speechtotext/plugins/vosk/autotests/voskspeechtotextinfotest.cpp
+++ textspeechtotext/speechtotext/plugins/vosk/autotests/voskspeechtotextinfotest.cpp
@@ -16,4 +16,11 @@ VoskSpeechToTextInfoTest::VoskSpeechToTextInfoTest(QObject *parent)
 void VoskSpeechToTextInfoTest::shouldHaveDefaultValues()
 {
     VoskSpeechToTextInfo w;
+    QVERIFY(w.lang().isEmpty());
+    QVERIFY(w.langText().isEmpty());
+    QVERIFY(w.md5().isEmpty());
+    QVERIFY(w.version().isEmpty());
+    QVERIFY(w.url().isEmpty());
+    QCOMPARE(w.size(), 0);
+    QVERIFY(!w.obsolete());
 }
