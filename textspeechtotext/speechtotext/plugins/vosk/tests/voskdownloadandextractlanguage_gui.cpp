f9d3b4305b68c7c8dd9a85078ebb6d67d985d959
Laurent Montel <montel@kde.org>
2023-10-09 13:33:13 +0200

    Add voskdownloadandextractlanguage_gui apps

---
+++ textspeechtotext/speechtotext/plugins/vosk/tests/voskdownloadandextractlanguage_gui.cpp
@@ -0,0 +1,39 @@
+/*
+  SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "downloadlanguagejob.h"
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QStandardPaths>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.addOption(QCommandLineOption(QStringList() << QStringLiteral("+[file]"), QStringLiteral("Add specific archive file name")));
+
+    parser.process(app);
+    if (!parser.positionalArguments().isEmpty()) {
+        const QString fileName = parser.positionalArguments().at(0);
+        auto download = new DownloadLanguageJob();
+        download->setUrl(QUrl(fileName));
+        QObject::connect(download, &DownloadLanguageJob::extractDone, &app, []() {
+            qDebug() << "Extraction Done";
+        });
+        QObject::connect(download, &DownloadLanguageJob::errorText, &app, [](const QString &str) {
+            qDebug() << "Extraction error: " << str;
+        });
+        download->start();
+        app.exec();
+    } else {
+        qWarning() << "Missing argument";
+    }
+    return 0;
+}
