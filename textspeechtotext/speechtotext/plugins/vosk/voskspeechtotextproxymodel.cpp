c5ab5630e192f54b86c0ea41d8668f324df6ff26
Laurent Montel <montel@kde.org>
2023-10-09 13:58:24 +0200

    Reactive proxymodel

--- textspeechtotext/speechtotext/plugins/vosk/voskspeechtotextproxymodel.cpp
+++ textspeechtotext/speechtotext/plugins/vosk/voskspeechtotextproxymodel.cpp
@@ -16,16 +16,13 @@ VoskSpeechToTextProxyModel::~VoskSpeechToTextProxyModel() = default;
 
 bool VoskSpeechToTextProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
 {
-#if 0 // TODO
     if (!mSearchString.isEmpty()) {
         const QString source = sourceModel()->index(source_row, VoskSpeechToTextModel::LangText, source_parent).data().toString();
-        const QString target = sourceModel()->index(source_row, VoskSpeechToTextModel::Target, source_parent).data().toString();
-        if (source.contains(mSearchString, Qt::CaseInsensitive) || target.contains(mSearchString, Qt::CaseInsensitive)) {
+        if (source.contains(mSearchString, Qt::CaseInsensitive)) {
             return true;
         }
         return false;
     }
-#endif
     return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
 }
 
