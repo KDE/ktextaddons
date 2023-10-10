711e1271a77d2ef3a3a867e711088c41b7bd24fc
Laurent Montel <montel@kde.org>
2023-10-10 07:13:38 +0200

    Add managermodelvoskspeechtotext autotest

--- textspeechtotext/speechtotext/plugins/vosk/autotests/managermodelvoskspeechtotexttest.cpp
+++ textspeechtotext/speechtotext/plugins/vosk/autotests/managermodelvoskspeechtotexttest.cpp
@@ -21,4 +21,24 @@ void ManagerModelVoskSpeechToTextTest::shouldHaveDefaultValues()
     QVERIFY(w.needDownloadModelList());
 }
 
+void ManagerModelVoskSpeechToTextTest::shouldLoadModelList_data()
+{
+    QTest::addColumn<QString>("fileName");
+    QTest::addColumn<int>("numberOfTranslator");
+    QTest::newRow("empty") << QStringLiteral("empty") << 0;
+    QTest::newRow("fullmodels") << QStringLiteral("model-list") << 106;
+    // 1 correct 1 invalid
+    QTest::newRow("invalid") << QStringLiteral("invalid") << 1;
+}
+
+void ManagerModelVoskSpeechToTextTest::shouldLoadModelList()
+{
+    QFETCH(QString, fileName);
+    QFETCH(int, numberOfTranslator);
+    const QString originalJsonFile = QLatin1String(VOSK_DATA_DIR) + QStringLiteral("/modellanguages/") + fileName + QStringLiteral(".json");
+    ManagerModelVoskSpeechToText w;
+    w.loadModelList(originalJsonFile);
+    QCOMPARE(w.speechToTextInfos().count(), numberOfTranslator);
+}
+
 #include "moc_managermodelvoskspeechtotexttest.cpp"
