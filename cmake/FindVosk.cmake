5d83edef0aa2e18d0fb3c87b6f930ed33cce3b79
Laurent Montel <montel@kde.org>
2024-01-04 13:21:11 +0100

    Copy cmake module from VoiceAssistant project

--- cmake/FindVosk.cmake
+++ cmake/FindVosk.cmake
@@ -1,31 +1,38 @@
-# - Find Vosk
-# Find the Vosk library.
-#
-# This module defines
-#  vosk_FOUND - whether the vosk library was found
-#  vosk_LIBRARIES - the vosk library
-#  vosk_INCLUDE_DIRS - the include path of the vosk library
+if (TARGET Vosk)
+    return()
+endif()
 
-# SPDX-FileCopyrightText: 2023-2024 Laurent Montel <montel.org>
-#
-# SPDX-License-Identifier: BSD-3-Clause
+include(Dirs)
 
-if (vosk_INCLUDE_DIRS AND vosk_LIBRARIES)
-  # Already in cache
-  set (vosk_FOUND TRUE)
-else ()
-  find_library(vosk_LIBRARIES
+isFileEmpty(${VOSK_ZIP} __voskZipIsEmpty)
+
+if(${__voskZipIsEmpty})
+    include(DownloadVosk)
+    download_vosk_if_needed()
+endif()
+
+if (CMAKE_CROSSCOMPILING)
+    set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+endif()
+
+find_library(VOSK_LIBRARY
     NAMES vosk libvosk
+    PATHS ${VOSK_DIR}
     NO_DEFAULT_PATH
-  )
-  MESSAGE(STATUS "vosk_LIBRARIES **************** ${vosk_LIBRARIES}")
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(vosk DEFAULT_MSG vosk_LIBRARIES vosk_INCLUDE_DIRS)
+)
+
+if (VOSK_LIBRARY STREQUAL "VOSK_LIBRARY-NOTFOUND")
+    set(Vosk_FOUND OFF)
+    return()
 endif()
 
-mark_as_advanced(vosk_INCLUDE_DIRS vosk_LIBRARIES)
-if (vosk_FOUND)
-   add_library(vosk::Lib UNKNOWN IMPORTED)
-   set_target_properties(vosk::Lib PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${vosk_INCLUDE_DIRS} IMPORTED_LOCATION ${vosk_LIBRARIES})
-endif()
+set(Vosk_FOUND ON)
 
+add_library(Vosk INTERFACE)
+target_link_libraries(Vosk INTERFACE "${VOSK_LIBRARY}")
+target_link_directories(Vosk INTERFACE "${VOSK_DIR}")
+target_include_directories(Vosk INTERFACE "${VOSK_DIR}")
+
+if (CMAKE_CROSSCOMPILING)
+    set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+endif()
