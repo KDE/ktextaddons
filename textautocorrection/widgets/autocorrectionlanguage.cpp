0ed9dc355521f4eed195f55857c365fe3da59d91
Laurent Montel <montel@kde.org>
2024-03-25 13:04:33 +0100

    Add model for language

--- textautocorrection/widgets/autocorrectionlanguage.cpp
+++ textautocorrection/widgets/autocorrectionlanguage.cpp
@@ -5,58 +5,21 @@
 */
 
 #include "autocorrectionlanguage.h"
-#include <QLocale>
-#include <QSet>
+#include "autocorrectionlanguagemodel.h"
+#include <QSortFilterProxyModel>
 
 using namespace TextAutoCorrectionWidgets;
-
-static bool stripCountryCode(QString *languageCode)
-{
-    const int idx = languageCode->indexOf(QLatin1Char('_'));
-    if (idx != -1) {
-        *languageCode = languageCode->left(idx);
-        return true;
-    }
-    return false;
-}
 
 AutoCorrectionLanguage::AutoCorrectionLanguage(QWidget *parent)
     : QComboBox(parent)
 {
-    QLocale cLocale(QLocale::C);
-    QSet<QString> insertedLanguages;
-    const QList<QLocale> allLocales = QLocale::matchingLocales(QLocale::AnyLanguage, QLocale::AnyScript, QLocale::AnyCountry);
-    for (const QLocale &lang : allLocales) {
-        QString languageCode = lang.name();
-        if (lang != cLocale) {
-            const QString nativeName = lang.nativeLanguageName();
-            // For some languages the native name might be empty.
-            // In this case use the non native language name as fallback.
-            // See: QTBUG-51323
-            QString languageName = nativeName.isEmpty() ? QLocale::languageToString(lang.language()) : nativeName;
-            languageName = languageName.toLower();
-            if (!insertedLanguages.contains(languageName)) {
-                addItem(languageName, languageCode);
-                insertedLanguages << languageName;
-            } else if (stripCountryCode(&languageCode)) {
-                if (!insertedLanguages.contains(languageName)) {
-                    addItem(languageName, languageCode);
-                    insertedLanguages << languageName;
-                }
-            }
-        }
-    }
+    auto sortFilterModel = new QSortFilterProxyModel(this);
+    sortFilterModel->setSourceModel(new AutoCorrectionLanguageModel(this));
+    sortFilterModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+    sortFilterModel->setFilterRole(AutoCorrectionLanguageModel::LocaleI18n);
+    sortFilterModel->sort(0);
 
-    QString defaultLang;
-    if (!QLocale::system().uiLanguages().isEmpty()) {
-        defaultLang = QLocale::system().uiLanguages().constFirst();
-        if (defaultLang == QLatin1Char('C')) {
-            defaultLang = QStringLiteral("en_US");
-        }
-    }
-    const int index = findData(defaultLang);
-    setCurrentIndex(index);
-    model()->sort(0);
+    setModel(sortFilterModel);
 }
 
 AutoCorrectionLanguage::~AutoCorrectionLanguage() = default;
---
+++ textautocorrection/widgets/autocorrectionlanguagemodel.h
@@ -0,0 +1,35 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QAbstractListModel>
+
+class AutoCorrectionLanguageModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    enum AutoCorrectionLanguageModelRoles {
+        LocaleI18n = Qt::UserRole + 1,
+        Locale,
+    };
+    Q_ENUM(AutoCorrectionLanguageModelRoles)
+
+    explicit AutoCorrectionLanguageModel(QObject *parent = nullptr);
+    ~AutoCorrectionLanguageModel() override;
+
+    [[nodiscard]] int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    [[nodiscard]] QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+    struct LocaleInfo {
+        QString localeTranslated;
+        QString localeCode;
+    };
+    void fillModel();
+
+    QList<LocaleInfo> mLocalInfos;
+};
