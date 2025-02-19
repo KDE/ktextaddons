64c401f6ee309fdc4a26b7205d9aa220d94b5167
Laurent Montel <montel@kde.org>
2025-02-19 06:49:57 +0100

    Add Messages.sh

---
+++ textautogeneratetext/Messages.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+$XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libtextautogeneratetext.pot
