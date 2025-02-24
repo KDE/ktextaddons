c0b5d62e0c4fa9c7301199fbfd7838a95ddf7bf6
Laurent Montel <montel@kde.org>
2025-02-24 08:34:55 +0100

    Continue to implement it

--- textautogeneratetext/autogenerate/plugins/mistral/mistralconfigurewidget.cpp
+++ textautogeneratetext/autogenerate/plugins/mistral/mistralconfigurewidget.cpp
@@ -5,10 +5,16 @@
 */
 
 #include "mistralconfigurewidget.h"
+#include <QFormLayout>
 
 MistralConfigureWidget::MistralConfigureWidget(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QFormLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins({});
+    // TODO add key here.
+    // Store key in wallet ?
 }
 
 MistralConfigureWidget::~MistralConfigureWidget() = default;
