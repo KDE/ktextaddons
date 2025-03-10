092e49e2c81a4d84f1f5e03c75f07f7a6daa3964
Laurent Montel <montel@kde.org>
2025-03-10 13:28:58 +0100

    Add more autotest

---
+++ textautogeneratetext/autotests/textautogeneratemessagetest.cpp
@@ -0,0 +1,21 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogeneratemessagetest.h"
+#include "core/textautogeneratemessage.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(TextAutoGenerateMessageTest)
+TextAutoGenerateMessageTest::TextAutoGenerateMessageTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TextAutoGenerateMessageTest::shouldHaveDefaultValues()
+{
+    TextAutogenerateText::TextAutoGenerateMessage msg;
+    QVERIFY(msg.content().isEmpty());
+    QCOMPARE(msg.sender(), TextAutogenerateText::TextAutoGenerateMessage::Sender::Unknown);
+}
---
+++ textautogeneratetext/autotests/textautogeneratemessagetest.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class TextAutoGenerateMessageTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateMessageTest(QObject *parent = nullptr);
+    ~TextAutoGenerateMessageTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
