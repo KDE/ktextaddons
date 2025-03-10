e5136396770999e1df3f434144191f179fe6aca0
Laurent Montel <montel@kde.org>
2025-03-10 08:35:11 +0100

    Add textautogeneratemessage

---
+++ textautogeneratetext/core/textautogeneratemessage.cpp
@@ -0,0 +1,12 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogeneratemessage.h"
+
+using namespace TextAutogenerateText;
+TextAutoGenerateMessage::TextAutoGenerateMessage() = default;
+
+TextAutoGenerateMessage::~TextAutoGenerateMessage() = default;
---
+++ textautogeneratetext/core/textautogeneratemessage.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "textautogeneratetext_export.h"
+#include <QAbstractListModel>
+namespace TextAutogenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateMessage
+{
+public:
+    TextAutoGenerateMessage();
+    ~TextAutoGenerateMessage();
+};
+}
