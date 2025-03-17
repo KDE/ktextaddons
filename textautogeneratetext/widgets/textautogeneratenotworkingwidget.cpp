db7062224aec1de1d4bd78cfb3f3a91f5c8ee648
Laurent Montel <montel@kde.org>
2025-03-17 07:12:52 +0100

    Add textautogeneratenotworkingwidget

---
+++ textautogeneratetext/widgets/textautogeneratenotworkingwidget.cpp
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogeneratenotworkingwidget.h"
+#include <QLabel>
+
+using namespace TextAutogenerateText;
+TextAutogenerateNotWorkingWidget::TextAutogenerateNotWorkingWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+TextAutogenerateNotWorkingWidget::~TextAutogenerateNotWorkingWidget() = default;
---
+++ textautogeneratetext/widgets/textautogeneratenotworkingwidget.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetext_export.h"
+#include <QWidget>
+
+namespace TextAutogenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutogenerateNotWorkingWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TextAutogenerateNotWorkingWidget(QWidget *parent = nullptr);
+    ~TextAutogenerateNotWorkingWidget() override;
+};
+}
