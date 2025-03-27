f933c6a18a56f74a0516402008db074d8b4224ee
Laurent Montel <montel@kde.org>
2025-03-27 06:58:40 +0100

    add request

---
+++ textautogeneratetext/plugins/ollama/ollamarequest.cpp
@@ -0,0 +1,27 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "ollamarequest.h"
+
+OllamaRequest::OllamaRequest() = default;
+
+OllamaRequest::~OllamaRequest() = default;
+
+QString OllamaRequest::message() const
+{
+    return mMessage;
+}
+
+void OllamaRequest::setMessage(const QString &newMessage)
+{
+    mMessage = newMessage;
+}
+
+QDebug operator<<(QDebug d, const OllamaRequest &t)
+{
+    d.space() << "message:" << t.message();
+    return d;
+}
---
+++ textautogeneratetext/plugins/ollama/ollamarequest.h
@@ -0,0 +1,26 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QDebug>
+#include <QObject>
+
+class OllamaRequest
+{
+    Q_GADGET
+public:
+    OllamaRequest();
+    ~OllamaRequest();
+
+    [[nodiscard]] QString message() const;
+    void setMessage(const QString &newMessage);
+
+private:
+    QString mMessage;
+};
+
+QDebug operator<<(QDebug d, const OllamaRequest &t);
