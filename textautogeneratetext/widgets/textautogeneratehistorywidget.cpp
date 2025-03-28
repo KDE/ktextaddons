d4fbf339b6c959d19b7e89838aec426092066fc4
Laurent Montel <montel@kde.org>
2025-03-28 07:22:44 +0100

    Prepare to implement textautogeneratehistorywidget

---
+++ textautogeneratetext/widgets/textautogeneratehistorywidget.cpp
@@ -0,0 +1,14 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratehistorywidget.h"
+
+using namespace TextAutogenerateText;
+TextAutogenerateHistoryWidget::TextAutogenerateHistoryWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+TextAutogenerateHistoryWidget::~TextAutogenerateHistoryWidget() = default;
---
+++ textautogeneratetext/widgets/textautogeneratehistorywidget.h
@@ -0,0 +1,18 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetext_private_export.h"
+#include <QWidget>
+namespace TextAutogenerateText
+{
+class TEXTAUTOGENERATETEXT_TESTS_EXPORT TextAutogenerateHistoryWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TextAutogenerateHistoryWidget(QWidget *parent = nullptr);
+    ~TextAutogenerateHistoryWidget() override;
+};
+}
