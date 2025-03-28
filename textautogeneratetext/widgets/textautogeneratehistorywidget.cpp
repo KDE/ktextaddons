a3e41da4b86fc9c4be0234f3a8cdbb82603bf895
Laurent Montel <montel@kde.org>
2025-03-28 07:24:43 +0100

    Add layout

--- textautogeneratetext/widgets/textautogeneratehistorywidget.cpp
+++ textautogeneratetext/widgets/textautogeneratehistorywidget.cpp
@@ -4,11 +4,18 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratehistorywidget.h"
+#include <QVBoxLayout>
 
 using namespace TextAutogenerateText;
+using namespace Qt::Literals::StringLiterals;
 TextAutogenerateHistoryWidget::TextAutogenerateHistoryWidget(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName("mainLayout"_L1);
+    mainLayout->setContentsMargins({});
+
+    // TODO
 }
 
 TextAutogenerateHistoryWidget::~TextAutogenerateHistoryWidget() = default;
