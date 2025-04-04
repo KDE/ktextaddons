82178857498ca9dca03c8debed52c9a0ea836c4f
Laurent Montel <montel@kde.org>
2025-04-04 13:50:52 +0200

    Prepare to implement update message

--- textautogeneratetext/plugins/ollama/ollamaplugin.cpp
+++ textautogeneratetext/plugins/ollama/ollamaplugin.cpp
@@ -65,6 +65,39 @@ void OllamaPlugin::setCurrentModel(const QString &newCurrentModel)
 
 void OllamaPlugin::sendToLLM(const QString &message)
 {
+    OllamaRequest req;
+    req.setMessage(message);
+    /*
+    req.setModel(KLLMCoreSettings::model());
+    for (const auto &message : m_messages | std::views::reverse) {
+        if (message.sender == Sender::LLM) {
+            req.setContext(message.context);
+            break;
+        }
+    }
+    */
+    auto reply = OllamaManager::self()->getCompletion(req);
+
+    /*
+    m_connections.insert(rep, connect(rep, &KLLMReply::contentAdded, this, [this, i = m_messages.size() - 1] {
+                             auto &message = m_messages[i];
+                             message.content = message.llmReply->readResponse();
+                             Q_EMIT dataChanged(index(i), index(i), {Roles::MessageRole});
+                         }));
+    m_connections.insert(rep, connect(rep, &KLLMReply::finished, this, [this, i = m_messages.size() - 1] {
+                             auto &message = m_messages[i];
+                             m_connections.remove(message.llmReply);
+                             message.context = message.llmReply->context();
+                             message.info = message.llmReply->info();
+                             message.llmReply->deleteLater();
+                             message.llmReply = nullptr;
+                             message.inProgress = false;
+                             Q_EMIT dataChanged(index(i),
+                                                index(i),
+                                                {Roles::FinishedRole, Roles::TokensPerSecondRole, Roles::TokenCountRole, Roles::DurationRole});
+                             Q_EMIT replyInProgressChanged();
+                         }));
+                         */
     // TODO
 }
 
