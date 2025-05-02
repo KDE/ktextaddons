2b9d1a8fb8f390471c894567652ce7779193fdec
Laurent Montel <montel@kde.org>
2025-05-02 21:30:47 +0200

    Improve textautogeneratehistorylistheadingsproxymodel

--- textautogeneratetext/core/textautogeneratehistorylistheadingsproxymodel.cpp
+++ textautogeneratetext/core/textautogeneratehistorylistheadingsproxymodel.cpp
@@ -13,3 +13,298 @@ TextAutoGenerateHistoryListHeadingsProxyModel::TextAutoGenerateHistoryListHeadin
 }
 
 TextAutoGenerateHistoryListHeadingsProxyModel::~TextAutoGenerateHistoryListHeadingsProxyModel() = default;
+
+QVariant TextAutoGenerateHistoryListHeadingsProxyModel::data(const QModelIndex &index, int role) const
+{
+#if 0
+    switch (type(index)) {
+    case IndexType::Root:
+        return {};
+    case IndexType::Section:
+        switch (role) {
+        case Qt::ItemDataRole::DisplayRole:
+            return RoomModel::sectionName(RoomModel::Section(index.row()));
+        case Qt::BackgroundRole:
+            return QApplication::palette().brush(QPalette::Window);
+        default:
+            return {};
+        }
+    case IndexType::Channel:
+        return sourceModel()->data(mapToSource(index), role);
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+QModelIndex TextAutoGenerateHistoryListHeadingsProxyModel::index(int row, int column, const QModelIndex &parent) const
+{
+#if 0
+    switch (type(parent)) {
+    case IndexType::Root:
+        return createIndex(row, column, sectionCount);
+    case IndexType::Section:
+        return createIndex(row, column, parent.row());
+    case IndexType::Channel:
+        return {};
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+QModelIndex TextAutoGenerateHistoryListHeadingsProxyModel::parent(const QModelIndex &child) const
+{
+#if 0
+    switch (type(child)) {
+    case IndexType::Root:
+    case IndexType::Section:
+        return {};
+    case IndexType::Channel:
+        return createIndex(int(child.internalId()), 0, sectionCount);
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+int TextAutoGenerateHistoryListHeadingsProxyModel::rowCount(const QModelIndex &parent) const
+{
+#if 0
+    switch (type(parent)) {
+    case IndexType::Root:
+        return sectionCount;
+    case IndexType::Section:
+        return int(mSections.at(parent.row()).size());
+    case IndexType::Channel:
+        return 0;
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+int TextAutoGenerateHistoryListHeadingsProxyModel::columnCount(const QModelIndex &parent) const
+{
+#if 0
+    switch (type(parent)) {
+    case IndexType::Root:
+    case IndexType::Section:
+        return 1;
+    case IndexType::Channel:
+        return 0;
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+Qt::ItemFlags TextAutoGenerateHistoryListHeadingsProxyModel::flags(const QModelIndex &proxyIndex) const
+{
+#if 0
+    switch (type(proxyIndex)) {
+    case IndexType::Root:
+        return {};
+    case IndexType::Section:
+        return Qt::ItemFlag::ItemIsEnabled;
+    case IndexType::Channel:
+        return QAbstractProxyModel::flags(proxyIndex);
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+bool TextAutoGenerateHistoryListHeadingsProxyModel::hasChildren(const QModelIndex &index) const
+{
+    // Don't use the QAbstractProxyModel override which fails on non-proxied indices
+    return QAbstractItemModel::hasChildren(index); // NOLINT(bugprone-parent-virtual-call)
+}
+
+void TextAutoGenerateHistoryListHeadingsProxyModel::setSourceModel(QAbstractItemModel *sourceModel)
+{
+    beginResetModel();
+
+    if (auto *oldModel = this->sourceModel()) {
+        disconnect(oldModel, nullptr, this, nullptr);
+    }
+
+    QAbstractProxyModel::setSourceModel(sourceModel);
+
+    connect(sourceModel, &QAbstractItemModel::rowsInserted, this, &TextAutoGenerateHistoryListHeadingsProxyModel::onRowsInserted);
+
+    connect(sourceModel, &QAbstractItemModel::rowsAboutToBeRemoved, this, &TextAutoGenerateHistoryListHeadingsProxyModel::onRowsAboutToBeRemoved);
+
+    connect(sourceModel, &QAbstractItemModel::rowsAboutToBeMoved, this, [this] {
+        Q_EMIT layoutAboutToBeChanged();
+    });
+    connect(sourceModel, &QAbstractItemModel::rowsMoved, this, &TextAutoGenerateHistoryListHeadingsProxyModel::rebuildSections);
+    connect(sourceModel, &QAbstractItemModel::rowsMoved, this, [this] {
+        Q_EMIT layoutChanged();
+    });
+
+    connect(sourceModel, &QAbstractItemModel::dataChanged, this, &TextAutoGenerateHistoryListHeadingsProxyModel::onDataChanged);
+
+    connect(sourceModel, &QAbstractItemModel::modelAboutToBeReset, this, &TextAutoGenerateHistoryListHeadingsProxyModel::beginResetModel);
+    connect(sourceModel, &QAbstractItemModel::modelReset, this, &TextAutoGenerateHistoryListHeadingsProxyModel::rebuildSections);
+    connect(sourceModel, &QAbstractItemModel::modelReset, this, &TextAutoGenerateHistoryListHeadingsProxyModel::endResetModel);
+
+    connect(sourceModel, &QAbstractItemModel::layoutAboutToBeChanged, this, &TextAutoGenerateHistoryListHeadingsProxyModel::layoutAboutToBeChanged);
+    connect(sourceModel, &QAbstractItemModel::layoutChanged, this, &TextAutoGenerateHistoryListHeadingsProxyModel::rebuildSections);
+    connect(sourceModel, &QAbstractItemModel::layoutChanged, this, &TextAutoGenerateHistoryListHeadingsProxyModel::layoutChanged);
+
+    rebuildSections();
+
+    endResetModel();
+}
+
+QModelIndex TextAutoGenerateHistoryListHeadingsProxyModel::mapToSource(const QModelIndex &proxyIndex) const
+{
+#if 0
+    if (!sourceModel())
+        return {};
+
+    switch (type(proxyIndex)) {
+    case IndexType::Root:
+    case IndexType::Section:
+        return {};
+    case IndexType::Channel:
+        return mSections.at(proxyIndex.internalId()).at(proxyIndex.row());
+    }
+    Q_UNREACHABLE();
+#endif
+    return {};
+}
+
+QModelIndex TextAutoGenerateHistoryListHeadingsProxyModel::mapFromSource(const QModelIndex &sourceIndex) const
+{
+#if 0
+    if (!sourceModel())
+        return {};
+
+    if (!sourceIndex.isValid())
+        return {};
+
+    for (auto sectionId = size_t(0), iMax = mSections.size(); sectionId < iMax; ++sectionId) {
+        const auto &section = mSections.at(sectionId);
+
+        const auto it = std::lower_bound(section.cbegin(), section.cend(), sourceIndex);
+        if (it != section.cend() && *it == sourceIndex)
+            return createIndex(int(it - section.cbegin()), 0, sectionId);
+    }
+
+    return {};
+#endif
+    return {};
+}
+
+void TextAutoGenerateHistoryListHeadingsProxyModel::onRowsInserted(const QModelIndex &parent, int first, int last)
+{
+#if 0
+    for (auto row = first; row <= last; ++row) {
+        const QPersistentModelIndex index = sourceModel()->index(row, 0, parent);
+        const auto newSectionId = int(index.data(RoomModel::RoomSection).value<RoomModel::Section>());
+        auto &newSection = mSections.at(newSectionId);
+
+        const auto newLocation = std::lower_bound(newSection.cbegin(), newSection.cend(), index);
+        const auto newLocationRow = int(newLocation - newSection.cbegin());
+
+        beginInsertRows(createIndex(newSectionId, 0, sectionCount), newLocationRow, newLocationRow);
+
+        newSection.insert(newLocation, index);
+
+        endInsertRows();
+    }
+#endif
+}
+
+void TextAutoGenerateHistoryListHeadingsProxyModel::onRowsAboutToBeRemoved(const QModelIndex &parent, int first, int last)
+{
+#if 0
+    for (auto row = first; row <= last; ++row) {
+        const auto index = sourceModel()->index(row, 0, parent);
+
+        const auto ourOldIndex = mapFromSource(index);
+        const auto oldSectionId = ourOldIndex.internalId();
+        auto &oldSection = mSections.at(oldSectionId);
+
+        beginRemoveRows(ourOldIndex.parent(), ourOldIndex.row(), ourOldIndex.row());
+
+        oldSection.erase(oldSection.begin() + ourOldIndex.row());
+
+        endRemoveRows();
+    }
+#endif
+}
+
+void TextAutoGenerateHistoryListHeadingsProxyModel::onDataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QList<int> &roles)
+{
+#if 0
+    for (auto row = topLeft.row(), last = bottomRight.row(); row <= last; ++row) {
+        const auto sourceIndex = topLeft.siblingAtRow(row);
+        const auto proxyIndex = mapFromSource(sourceIndex);
+        Q_EMIT dataChanged(proxyIndex, proxyIndex, roles);
+    }
+
+    if (!roles.empty() && !roles.contains(RoomModel::RoomSection))
+        return;
+
+    for (auto row = topLeft.row(), last = bottomRight.row(); row <= last; ++row) {
+        const auto sourceIndex = topLeft.siblingAtRow(row);
+        const auto ourOldIndex = mapFromSource(sourceIndex);
+
+        const auto oldSectionId = int(ourOldIndex.internalId());
+        const auto newSectionId = int(sourceIndex.data(RoomModel::RoomSection).value<RoomModel::Section>());
+
+        if (oldSectionId == newSectionId)
+            continue;
+
+        auto &oldSection = mSections.at(oldSectionId);
+        auto &newSection = mSections.at(newSectionId);
+
+        const auto newLocation = std::lower_bound(newSection.cbegin(), newSection.cend(), sourceIndex);
+        const auto newLocationRow = int(newLocation - newSection.cbegin());
+
+        beginMoveRows(ourOldIndex.parent(), ourOldIndex.row(), ourOldIndex.row(), createIndex(newSectionId, 0, sectionCount), newLocationRow);
+
+        auto persistantIndex = oldSection[ourOldIndex.row()];
+        oldSection.erase(oldSection.begin() + ourOldIndex.row());
+        newSection.insert(newLocation, persistantIndex);
+
+        endMoveRows();
+    }
+#endif
+}
+
+void TextAutoGenerateHistoryListHeadingsProxyModel::rebuildSections()
+{
+#if 0
+    for (auto &section : mSections)
+        section.clear();
+
+    for (auto row = 0, until = sourceModel()->rowCount(); row < until; ++row) {
+        const QPersistentModelIndex index = sourceModel()->index(row, 0);
+        const auto newSectionId = uint(index.data(RoomModel::RoomSection).value<RoomModel::Section>());
+        auto &newSection = mSections.at(newSectionId);
+
+        newSection.push_back(index);
+    }
+
+    for (auto &section : mSections)
+        std::sort(section.begin(), section.end());
+#endif
+}
+#if 0
+auto TextAutoGenerateHistoryListHeadingsProxyModel::type(const QModelIndex &index) const -> IndexType
+{
+    if (!index.isValid())
+        return IndexType::Root;
+
+    if (index.internalId() == sectionCount)
+        return IndexType::Section;
+
+    return IndexType::Channel;
+}
+#endif
+
+#include "moc_textautogeneratehistorylistheadingsproxymodel.cpp"
--- textautogeneratetext/core/textautogeneratehistorylistheadingsproxymodel.h
+++ textautogeneratetext/core/textautogeneratehistorylistheadingsproxymodel.h
@@ -9,11 +9,38 @@
 #include <QAbstractProxyModel>
 namespace TextAutogenerateText
 {
-class TextAutoGenerateHistoryListHeadingsProxyModel : public QAbstractProxyModel
+class TextAutoGenerateHistoryListHeadingsProxyModel final : public QAbstractProxyModel
 {
+    Q_OBJECT
 public:
     explicit TextAutoGenerateHistoryListHeadingsProxyModel(QObject *parent = nullptr);
     ~TextAutoGenerateHistoryListHeadingsProxyModel() override;
+
+    // QAbstractItemModel interface
+    [[nodiscard]] QVariant data(const QModelIndex &index, int role) const override;
+    [[nodiscard]] QModelIndex index(int row, int column, const QModelIndex &parent) const override;
+    [[nodiscard]] QModelIndex parent(const QModelIndex &child) const override;
+    [[nodiscard]] int rowCount(const QModelIndex &parent) const override;
+    [[nodiscard]] int columnCount(const QModelIndex &parent) const override;
+    [[nodiscard]] Qt::ItemFlags flags(const QModelIndex &index) const override;
+    [[nodiscard]] bool hasChildren(const QModelIndex &index) const override;
+
+    // QAbstractProxyModel interface
+    void setSourceModel(QAbstractItemModel *sourceModel) override;
+    [[nodiscard]] QModelIndex mapToSource(const QModelIndex &proxyIndex) const override;
+    [[nodiscard]] QModelIndex mapFromSource(const QModelIndex &sourceIndex) const override;
+
+private:
+    void onRowsInserted(const QModelIndex &parent, int first, int last);
+    void onRowsAboutToBeRemoved(const QModelIndex &parent, int first, int last);
+    void onDataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QList<int> &roles);
+    void rebuildSections();
+
+    enum class IndexType : uint8_t {
+        Root,
+        Section,
+        History,
+    };
 };
 
 }
