bcc88d843cb77db89ffb29f61c7b81d5c9c49463
Laurent Montel <montel@kde.org>
2025-05-08 09:40:04 +0200

    Prepare chat class

---
+++ textautogeneratetext/core/textautogeneratechat.cpp
@@ -0,0 +1,11 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratechat.h"
+
+using namespace TextAutogenerateText;
+TextAutoGenerateChat::TextAutoGenerateChat() = default;
+
+TextAutoGenerateChat::~TextAutoGenerateChat() = default;
---
+++ textautogeneratetext/core/textautogeneratechat.h
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include "textautogeneratetext_export.h"
+#include <QDebug>
+namespace TextAutogenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateChat
+{
+    Q_GADGET
+public:
+    TextAutoGenerateChat();
+    ~TextAutoGenerateChat();
+};
+
+}
