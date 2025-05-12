fde53532442b7a3d11b34f5c7db74a7c2c79fcfa
Laurent Montel <montel@kde.org>
2025-05-12 13:30:34 +0200

    fix menu

--- textautogeneratetext/widgets/view/textautogeneratehistorylistview.cpp
+++ textautogeneratetext/widgets/view/textautogeneratehistorylistview.cpp
@@ -73,72 +73,70 @@ void TextAutoGenerateHistoryListView::slotClicked(const QModelIndex &idx)
 
 void TextAutoGenerateHistoryListView::contextMenuEvent(QContextMenuEvent *event)
 {
-    const QModelIndex index = indexAt(event->pos());
-    if (!index.parent().isValid()) {
-        return;
-    }
-
     QMenu menu(this);
     auto newChatHistory = new QAction(QIcon::fromTheme(QStringLiteral("document-new")), i18nc("@action", "New Chat…"), &menu);
     connect(newChatHistory, &QAction::triggered, this, [this]() {
         mManager->createNewChat();
     });
     menu.addAction(newChatHistory);
-    menu.addSeparator();
-
-    if (index.isValid()) {
-        auto renameHistory = new QAction(QIcon::fromTheme(QStringLiteral("document-edit")), i18nc("@action", "Modify…"), &menu);
-        connect(renameHistory, &QAction::triggered, this, [index, this]() {
-            const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
-            if (!uuid.isEmpty()) {
-                edit(index);
-            }
-        });
-        menu.addAction(renameHistory);
-
+    const QModelIndex index = indexAt(event->pos());
+    if (index.parent().isValid()) {
         menu.addSeparator();
 
-        const bool isFavorite = index.data(TextAutoGenerateChatsModel::Favorite).toBool();
-        auto changeFavoriteHistory = new QAction(QIcon::fromTheme(QStringLiteral("favorite")),
-                                                 isFavorite ? i18nc("@action", "Remove as Favorite") : i18nc("@action", "Set as Favorite"),
-                                                 &menu);
-        const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
-        connect(changeFavoriteHistory, &QAction::triggered, this, [index, isFavorite, this]() {
-            const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
-            if (!uuid.isEmpty()) {
-                mManager->changeFavoriteHistory(uuid, !isFavorite);
-            }
-        });
-        menu.addAction(changeFavoriteHistory);
-
-        const bool archived = index.data(TextAutoGenerateChatsModel::Archived).toBool();
-        menu.addSeparator();
-        auto archivedAction = new QAction(archived ? i18nc("@action", "Restore") : i18nc("@action", "Archive"), &menu);
-        connect(archivedAction, &QAction::triggered, this, [index, archived, this]() {
-            const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
-            if (!uuid.isEmpty()) {
-                mManager->archiveDiscussion(uuid, !archived);
-            }
-        });
-        menu.addAction(archivedAction);
-
-        menu.addSeparator();
-
-        auto removeHistory = new QAction(QIcon::fromTheme(QStringLiteral("list-remove")), i18nc("@action", "Remove…"), &menu);
-        connect(removeHistory, &QAction::triggered, this, [index, this]() {
-            if (KMessageBox::ButtonCode::PrimaryAction
-                == KMessageBox::questionTwoActions(this,
-                                                   i18n("Do you want to remove this discussion?"),
-                                                   i18nc("@title:window", "Remove Discussion"),
-                                                   KStandardGuiItem::remove(),
-                                                   KStandardGuiItem::cancel())) {
+        if (index.isValid()) {
+            auto renameHistory = new QAction(QIcon::fromTheme(QStringLiteral("document-edit")), i18nc("@action", "Modify…"), &menu);
+            connect(renameHistory, &QAction::triggered, this, [index, this]() {
                 const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
                 if (!uuid.isEmpty()) {
-                    mManager->removeDiscussion(mManager->currentChatId(), uuid);
+                    edit(index);
                 }
-            }
-        });
-        menu.addAction(removeHistory);
+            });
+            menu.addAction(renameHistory);
+
+            menu.addSeparator();
+
+            const bool isFavorite = index.data(TextAutoGenerateChatsModel::Favorite).toBool();
+            auto changeFavoriteHistory = new QAction(QIcon::fromTheme(QStringLiteral("favorite")),
+                                                     isFavorite ? i18nc("@action", "Remove as Favorite") : i18nc("@action", "Set as Favorite"),
+                                                     &menu);
+            const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
+            connect(changeFavoriteHistory, &QAction::triggered, this, [index, isFavorite, this]() {
+                const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
+                if (!uuid.isEmpty()) {
+                    mManager->changeFavoriteHistory(uuid, !isFavorite);
+                }
+            });
+            menu.addAction(changeFavoriteHistory);
+
+            const bool archived = index.data(TextAutoGenerateChatsModel::Archived).toBool();
+            menu.addSeparator();
+            auto archivedAction = new QAction(archived ? i18nc("@action", "Restore") : i18nc("@action", "Archive"), &menu);
+            connect(archivedAction, &QAction::triggered, this, [index, archived, this]() {
+                const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
+                if (!uuid.isEmpty()) {
+                    mManager->archiveDiscussion(uuid, !archived);
+                }
+            });
+            menu.addAction(archivedAction);
+
+            menu.addSeparator();
+
+            auto removeHistory = new QAction(QIcon::fromTheme(QStringLiteral("list-remove")), i18nc("@action", "Remove…"), &menu);
+            connect(removeHistory, &QAction::triggered, this, [index, this]() {
+                if (KMessageBox::ButtonCode::PrimaryAction
+                    == KMessageBox::questionTwoActions(this,
+                                                       i18n("Do you want to remove this discussion?"),
+                                                       i18nc("@title:window", "Remove Discussion"),
+                                                       KStandardGuiItem::remove(),
+                                                       KStandardGuiItem::cancel())) {
+                    const QByteArray uuid = index.data(TextAutoGenerateChatsModel::Identifier).toByteArray();
+                    if (!uuid.isEmpty()) {
+                        mManager->removeDiscussion(mManager->currentChatId());
+                    }
+                }
+            });
+            menu.addAction(removeHistory);
+        }
     }
     if (!menu.actions().isEmpty()) {
         menu.exec(event->globalPos());
