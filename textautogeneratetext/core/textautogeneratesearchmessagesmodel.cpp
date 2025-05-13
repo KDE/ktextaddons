7bbd48240c45df04247f387a898307191e133daf
Laurent Montel <montel@kde.org>
2025-05-13 19:42:57 +0200

    Add model

---
+++ textautogeneratetext/core/textautogeneratesearchmessagesmodel.cpp
@@ -0,0 +1,55 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogeneratesearchmessagesmodel.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateSearchMessagesModel::TextAutoGenerateSearchMessagesModel(QObject *parent)
+    : QAbstractListModel{parent}
+{
+}
+
+TextAutoGenerateSearchMessagesModel::~TextAutoGenerateSearchMessagesModel() = default;
+
+QList<TextAutoGenerateSearchMessage> TextAutoGenerateSearchMessagesModel::searchMessages() const
+{
+    return mSearchMessages;
+}
+
+void TextAutoGenerateSearchMessagesModel::setSearchMessages(const QList<TextAutoGenerateSearchMessage> &newSearchMessages)
+{
+    mSearchMessages = newSearchMessages;
+}
+
+int TextAutoGenerateSearchMessagesModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0; // flat model
+    }
+    return mSearchMessages.count();
+}
+
+QVariant TextAutoGenerateSearchMessagesModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() >= mSearchMessages.count()) {
+        return {};
+    }
+    const auto &message = mSearchMessages[index.row()];
+    switch (role) {
+    case Qt::DisplayRole:
+    case MessageSearchRoles::PreviewText:
+        return message.previewText();
+    case MessageSearchRoles::ChatUuid:
+        return message.chatId();
+    case MessageSearchRoles::MessageUuid:
+        return message.messageId();
+    case MessageSearchRoles::DateTime:
+        return message.dateTime();
+    }
+    return {};
+}
+
+#include "moc_textautogeneratesearchmessagesmodel.cpp"
---
+++ textautogeneratetext/core/textautogeneratesearchmessagesmodel.h
@@ -0,0 +1,36 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "textautogeneratetext_export.h"
+#include <QAbstractListModel>
+#include <TextAutoGenerateText/TextAutoGenerateSearchMessage>
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateSearchMessagesModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    enum MessageSearchRoles : uint16_t {
+        ChatUuid = Qt::UserRole + 1,
+        MessageUuid,
+        PreviewText,
+        DateTime,
+    };
+
+    explicit TextAutoGenerateSearchMessagesModel(QObject *parent = nullptr);
+    ~TextAutoGenerateSearchMessagesModel() override;
+
+    [[nodiscard]] QList<TextAutoGenerateSearchMessage> searchMessages() const;
+    void setSearchMessages(const QList<TextAutoGenerateSearchMessage> &newSearchMessages);
+
+    [[nodiscard]] int rowCount(const QModelIndex & = {}) const override;
+    [[nodiscard]] QVariant data(const QModelIndex &index, int role) const override;
+
+private:
+    QList<TextAutoGenerateSearchMessage> mSearchMessages;
+};
+}
