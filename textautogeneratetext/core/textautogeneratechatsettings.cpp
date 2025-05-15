b2d24a96efa6b756adcfcf9bf2cf2d016f1762a8
Laurent Montel <montel@kde.org>
2025-05-15 08:05:23 +0200

    Prepare to implement textautogeneratechatsettings

---
+++ textautogeneratetext/core/textautogeneratechatsettings.cpp
@@ -0,0 +1,43 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratechatsettings.h"
+
+using namespace Qt::Literals::StringLiterals;
+using namespace TextAutoGenerateText;
+TextAutoGenerateChatSettings::TextAutoGenerateChatSettings() = default;
+
+TextAutoGenerateChatSettings::~TextAutoGenerateChatSettings() = default;
+
+void TextAutoGenerateChatSettings::remove(const QByteArray &chatId)
+{
+    mPendingTypedTexts.remove(chatId);
+}
+
+void TextAutoGenerateChatSettings::add(const QByteArray &chatId, const PendingTypedInfo &info)
+{
+    mPendingTypedTexts[chatId] = info;
+}
+
+TextAutoGenerateChatSettings::PendingTypedInfo TextAutoGenerateChatSettings::value(const QByteArray &chatId)
+{
+    return mPendingTypedTexts.value(chatId);
+}
+
+bool TextAutoGenerateChatSettings::isEmpty() const
+{
+    return mPendingTypedTexts.isEmpty();
+}
+
+bool TextAutoGenerateChatSettings::PendingTypedInfo::isValid() const
+{
+    return !text.isEmpty();
+}
+
+QDebug operator<<(QDebug d, const TextAutoGenerateChatSettings::PendingTypedInfo &t)
+{
+    d.space() << "text" << t.text;
+    return d;
+}
---
+++ textautogeneratetext/core/textautogeneratechatsettings.h
@@ -0,0 +1,35 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetext_export.h"
+#include <QDebug>
+#include <QMap>
+#include <QString>
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateChatSettings
+{
+public:
+    struct TEXTAUTOGENERATETEXT_EXPORT PendingTypedInfo {
+        QString text;
+        [[nodiscard]] bool isValid() const;
+    };
+    TextAutoGenerateChatSettings();
+    ~TextAutoGenerateChatSettings();
+
+    void remove(const QByteArray &roomId);
+
+    void add(const QByteArray &roomId, const PendingTypedInfo &info);
+
+    [[nodiscard]] PendingTypedInfo value(const QByteArray &roomId);
+
+    [[nodiscard]] bool isEmpty() const;
+
+private:
+    QMap<QByteArray /*ChatId*/, PendingTypedInfo> mPendingTypedTexts;
+};
+}
+TEXTAUTOGENERATETEXT_EXPORT QDebug operator<<(QDebug d, const TextAutoGenerateText::TextAutoGenerateChatSettings::PendingTypedInfo &t);
