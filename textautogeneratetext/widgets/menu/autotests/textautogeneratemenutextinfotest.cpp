c0781d8cb0e193d980208561ab53be9270e4e2e7
Laurent Montel <montel@kde.org>
2025-05-19 08:53:25 +0200

    Add more autotest

---
+++ textautogeneratetext/widgets/menu/autotests/textautogeneratemenutextinfotest.cpp
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "textautogeneratemenutextinfotest.h"
+#include "widgets/menu/textautogeneratemenutextinfo.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(TextAutoGenerateMenuTextInfoTest)
+
+TextAutoGenerateMenuTextInfoTest::TextAutoGenerateMenuTextInfoTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TextAutoGenerateMenuTextInfoTest::shouldHaveDefaultValues()
+{
+    TextAutoGenerateText::TextAutoGenerateMenuTextInfo info;
+    QVERIFY(info.enabled());
+    QVERIFY(info.requestText().isEmpty());
+}
