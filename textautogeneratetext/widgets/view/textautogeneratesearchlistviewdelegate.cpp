e26322c29993a777655175d17ef72fafe909474d
Laurent Montel <montel@kde.org>
2025-05-19 13:53:34 +0200

    Fix delegate

--- textautogeneratetext/widgets/view/textautogeneratesearchlistviewdelegate.cpp
+++ textautogeneratetext/widgets/view/textautogeneratesearchlistviewdelegate.cpp
@@ -4,6 +4,7 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratesearchlistviewdelegate.h"
+#include "core/textautogeneratesearchmessagesmodel.h"
 #include <QListView>
 
 using namespace TextAutoGenerateText;
@@ -16,8 +17,26 @@ TextAutoGenerateSearchListViewDelegate::~TextAutoGenerateSearchListViewDelegate(
 
 QTextDocument *TextAutoGenerateSearchListViewDelegate::documentForIndex(const QModelIndex &index, int width) const
 {
-    // TODO
-    return nullptr;
+    Q_ASSERT(index.isValid());
+    const QByteArray uuid = index.data(TextAutoGenerateSearchMessagesModel::MessageUuid).toByteArray();
+    Q_ASSERT(!uuid.isEmpty());
+    auto it = mDocumentCache.find(uuid);
+    if (it != mDocumentCache.end()) {
+        auto ret = it->value.get();
+        if (width != -1 && !qFuzzyCompare(ret->textWidth(), width)) {
+            ret->setTextWidth(width);
+        }
+        return ret;
+    }
+
+    const QString text = index.data(TextAutoGenerateSearchMessagesModel::PreviewText).toString();
+    if (text.isEmpty()) {
+        return nullptr;
+    }
+    auto doc = createTextDocument(text, width);
+    auto ret = doc.get();
+    mDocumentCache.insert(uuid, std::move(doc));
+    return ret;
 }
 
 #include "moc_textautogeneratesearchlistviewdelegate.cpp"
