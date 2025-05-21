309298acc905ecf25715676f7faed6af4fcec1a2
Laurent Montel <montel@kde.org>
2025-05-21 18:26:55 +0200

    Implement TextAutoGenerateText::TextAutoGenerateTextReplyInfo qDebug

--- textautogeneratetext/core/textautogeneratetextreplyinfo.cpp
+++ textautogeneratetext/core/textautogeneratetextreplyinfo.cpp
@@ -4,3 +4,14 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratetextreplyinfo.h"
+
+QDebug operator<<(QDebug d, const TextAutoGenerateText::TextAutoGenerateTextReplyInfo &t)
+{
+    d.space() << "totalDuration:" << t.totalDuration;
+    d.space() << "loadDuration:" << t.loadDuration;
+    d.space() << "promptEvalTokenCount:" << t.promptEvalTokenCount;
+    d.space() << "promptEvalDuration:" << t.promptEvalDuration;
+    d.space() << "tokenCount:" << t.tokenCount;
+    d.space() << "duration:" << t.duration;
+    return d;
+}
