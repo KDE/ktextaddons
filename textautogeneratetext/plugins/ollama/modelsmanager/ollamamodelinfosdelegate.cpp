3b5f9b15b9f6b632ee717b636be6e7a81aa08284
Laurent Montel <montel@kde.org>
2025-05-22 19:57:11 +0200

    Prepare documentForIndex

--- textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelinfosdelegate.cpp
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelinfosdelegate.cpp
@@ -4,7 +4,7 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "ollamamodelinfosdelegate.h"
-
+#include "ollamamodelinfosmodel.h"
 OllamaModelInfosDelegate::OllamaModelInfosDelegate(QListView *view)
     : TextAutoGenerateText::TextAutoGenerateListViewBaseDelegate(view)
 {
@@ -14,7 +14,27 @@ OllamaModelInfosDelegate::~OllamaModelInfosDelegate() = default;
 
 QTextDocument *OllamaModelInfosDelegate::documentForIndex(const QModelIndex &index, int width) const
 {
-    // TODO
+    Q_ASSERT(index.isValid());
+    const QByteArray uuid = index.data(OllamaModelInfosModel::ModelName).toByteArray();
+    Q_ASSERT(!uuid.isEmpty());
+    auto it = mDocumentCache.find(uuid);
+    if (it != mDocumentCache.end()) {
+        auto ret = it->value.get();
+        if (width != -1 && !qFuzzyCompare(ret->textWidth(), width)) {
+            ret->setTextWidth(width);
+        }
+        return ret;
+    }
+    /*
+        const QString text = index.data(OllamaModelInfosModel::MessageRole).toString();
+        if (text.isEmpty()) {
+            return nullptr;
+        }
+        auto doc = createTextDocument(text, width);
+        auto ret = doc.get();
+        mDocumentCache.insert(uuid, std::move(doc));
+        return ret;
+        */
     return nullptr;
 }
 
