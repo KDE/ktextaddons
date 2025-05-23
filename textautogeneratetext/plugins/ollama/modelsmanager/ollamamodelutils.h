f7746d57d65c0459543b92a4d14ca7f80e9d3e5f
Laurent Montel <montel@kde.org>
2025-05-23 07:34:51 +0200

    Add model description

---
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelutils.h
@@ -0,0 +1,12 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include <QString>
+
+namespace OllamaModelUtils
+{
+[[nodiscard]] QString description(const QString &modelName);
+};
