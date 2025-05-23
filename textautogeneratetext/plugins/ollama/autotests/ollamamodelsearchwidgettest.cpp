ee340b224c3561b6f743633cc091014395cceb1c
Laurent Montel <montel@kde.org>
2025-05-23 07:51:42 +0200

    Add autotest

---
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodelsearchwidgettest.cpp
@@ -0,0 +1,28 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodelsearchwidgettest.h"
+#include "modelsmanager/ollamamodelsearchwidget.h"
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QTest>
+QTEST_MAIN(OllamaModelSearchWidgetTest)
+OllamaModelSearchWidgetTest::OllamaModelSearchWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void OllamaModelSearchWidgetTest::shouldHaveDefaultValues()
+{
+    OllamaModelSearchWidget w;
+    auto mainLayout = w.findChild<QHBoxLayout *>(QStringLiteral("mainlayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mSearchLineEdit = w.findChild<QLineEdit *>(QStringLiteral("mSearchLineEdit"));
+    QVERIFY(mSearchLineEdit);
+    QVERIFY(mSearchLineEdit->text().isEmpty());
+    QVERIFY(mSearchLineEdit->placeholderText().isEmpty());
+}
---
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodelsearchwidgettest.h
@@ -0,0 +1,18 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class OllamaModelSearchWidgetTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit OllamaModelSearchWidgetTest(QObject *parent = nullptr);
+    ~OllamaModelSearchWidgetTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
