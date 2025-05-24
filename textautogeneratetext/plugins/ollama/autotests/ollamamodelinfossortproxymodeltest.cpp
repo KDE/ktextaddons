e7910266f2b15b133ca3f00cc76887b71c8514bb
Laurent Montel <montel@kde.org>
2025-05-24 10:03:46 +0200

    Add autotest

---
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodelinfossortproxymodeltest.cpp
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodelinfossortproxymodeltest.h"
+#include "modelsmanager/ollamamodelinfossortproxymodel.h"
+#include <QTest>
+QTEST_MAIN(OllamaModelInfosSortProxyModelTest)
+
+OllamaModelInfosSortProxyModelTest::OllamaModelInfosSortProxyModelTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void OllamaModelInfosSortProxyModelTest::shouldHaveDefaultValues()
+{
+    OllamaModelInfosSortProxyModel w;
+    QCOMPARE(w.categories(), OllamaModelInfo::Category::Unknown);
+}
