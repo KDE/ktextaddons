f2bbeb84d692276c9e7299a8ddc1e8169fdc7023
Laurent Montel <montel@kde.org>
2025-05-25 09:26:48 +0200

    Add ollamamodeldownloadprogresswidget

---
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodeldownloadprogresswidget.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodeldownloadprogresswidget.h"
+
+OllamaModelDownloadProgressWidget::OllamaModelDownloadProgressWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+OllamaModelDownloadProgressWidget::~OllamaModelDownloadProgressWidget() = default;
