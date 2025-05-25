6ef617d1d64ceb6b94f747c83c7abeedf180c800
Laurent Montel <montel@kde.org>
2025-05-25 08:38:00 +0200

    Add ollamamodeldownloadwidget

---
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodeldownloadwidgettest.cpp
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodeldownloadwidgettest.h"
+#include "modelsmanager/ollamamodeldownloadwidget.h"
+#include <QTest>
+QTEST_MAIN(OllamaModelDownloadWidgetTest)
+
+OllamaModelDownloadWidgetTest::OllamaModelDownloadWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void OllamaModelDownloadWidgetTest::shouldHaveDefaultValues()
+{
+    OllamaModelDownloadWidget w;
+    // TODO
+}
---
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodeldownloadwidget.cpp
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodeldownloadwidget.h"
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QToolButton>
+
+OllamaModelDownloadWidget::OllamaModelDownloadWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+OllamaModelDownloadWidget::~OllamaModelDownloadWidget() = default;
