37c927666a67f732302c837e3bdd6827683a514c
Laurent Montel <montel@kde.org>
2025-06-01 12:08:34 +0200

    Prepare instance support.
    For the moment we can't create multi instance for ollama etc.

---
+++ textautogeneratetext/core/textautogeneratetextinstance.cpp
@@ -0,0 +1,11 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratetextinstance.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateTextInstance::TextAutoGenerateTextInstance() = default;
+
+TextAutoGenerateTextInstance::~TextAutoGenerateTextInstance() = default;
---
+++ textautogeneratetext/core/textautogeneratetextinstance.h
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetext_export.h"
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateTextInstance
+{
+public:
+    TextAutoGenerateTextInstance();
+    ~TextAutoGenerateTextInstance();
+};
+}
