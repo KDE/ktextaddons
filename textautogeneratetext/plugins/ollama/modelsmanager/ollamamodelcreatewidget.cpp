23724c6834741aa623498c967f5eca7779f280f3
Laurent Montel <montel@kde.org>
2025-06-01 10:14:59 +0200

    Prepare widget for creating new model

---
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelcreatewidget.cpp
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodelcreatewidget.h"
+#include <KLocalizedString>
+#include <QVBoxLayout>
+
+OllamaModelCreateWidget::OllamaModelCreateWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+OllamaModelCreateWidget::~OllamaModelCreateWidget() = default;
---
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelcreatewidget.h
@@ -0,0 +1,17 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include "textautogenerateollama_private_export.h"
+#include <QWidget>
+
+class TEXTAUTOGENERATEOLLAMA_TESTS_EXPORT OllamaModelCreateWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OllamaModelCreateWidget(QWidget *parent = nullptr);
+    ~OllamaModelCreateWidget() override;
+};
