0706b37edc8f08f34382e314b26957fbace6b61c
Laurent Montel <montel@kde.org>
2025-06-01 10:38:01 +0200

    Add autotest

---
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodelcreatewidgettest.cpp
@@ -0,0 +1,22 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodelcreatewidgettest.h"
+#include "modelsmanager/ollamamodelcreatewidget.h"
+#include <QTest>
+QTEST_MAIN(OllamaModelCreateWidgetTest)
+
+OllamaModelCreateWidgetTest::OllamaModelCreateWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+OllamaModelCreateWidgetTest::~OllamaModelCreateWidgetTest()
+{
+    OllamaModelCreateWidget w;
+    // TODO
+}
+
+#include "moc_ollamamodelcreatewidgettest.cpp"
---
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodelcreatewidgettest.h
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class OllamaModelCreateWidgetTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit OllamaModelCreateWidgetTest(QObject *parent = nullptr);
+    ~OllamaModelCreateWidgetTest() override;
+};
