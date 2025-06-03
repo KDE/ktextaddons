fae540de26fdf928b46749c4256a4cf3bc57443b
Laurent Montel <montel@kde.org>
2025-06-03 19:30:40 +0200

    continue to implemeny ollamamodelcreatefromexistingmodelwidget

--- textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelcreatefromexistingmodelwidget.cpp
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelcreatefromexistingmodelwidget.cpp
@@ -4,10 +4,31 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "ollamamodelcreatefromexistingmodelwidget.h"
-
-OllamaModelCreateFromExistingModelWidget::OllamaModelCreateFromExistingModelWidget(QWidget *parent)
+#include <KLocalizedString>
+#include <QFormLayout>
+#include <QLineEdit>
+OllamaModelCreateFromExistingModelWidget::OllamaModelCreateFromExistingModelWidget(OllamaManager *manager, QWidget *parent)
     : QWidget{parent}
+    , mModelName(new QLineEdit(this))
+    , mTagName(new QLineEdit(this))
+    , mOllamaManager(manager)
 {
+    auto mainLayout = new QFormLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainlayout"));
+    mainLayout->setContentsMargins({});
+    mainLayout->setSpacing(0);
+
+    mModelName->setObjectName(QStringLiteral("mModelName"));
+    mainLayout->addRow(i18n("Name:"), mModelName);
+
+    mTagName->setObjectName(QStringLiteral("mTagName"));
+    mainLayout->addRow(i18n("Tag:"), mTagName);
+
+    // TODO base
+
+    // TODO prompt
 }
 
 OllamaModelCreateFromExistingModelWidget::~OllamaModelCreateFromExistingModelWidget() = default;
+
+#include "moc_ollamamodelcreatefromexistingmodelwidget.cpp"
