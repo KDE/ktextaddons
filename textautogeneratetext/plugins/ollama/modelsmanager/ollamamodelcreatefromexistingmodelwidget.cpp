0c9edb3200aa4b53e0524ff30bbffb1ead9408a6
Laurent Montel <montel@kde.org>
2025-06-03 13:55:17 +0200

    Prepare to create model

---
+++ textautogeneratetext/plugins/ollama/modelsmanager/ollamamodelcreatefromexistingmodelwidget.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ollamamodelcreatefromexistingmodelwidget.h"
+
+OllamaModelCreateFromExistingModelWidget::OllamaModelCreateFromExistingModelWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+OllamaModelCreateFromExistingModelWidget::~OllamaModelCreateFromExistingModelWidget() = default;
