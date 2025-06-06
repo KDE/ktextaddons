c8de954d3c93e6c177eb0b412ad65436965066bd
Laurent Montel <montel@kde.org>
2025-06-06 13:45:49 +0200

    Prepare to implement quickaskdialog

---
+++ textautogeneratetext/widgets/quickask/quickaskdialog.cpp
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "quickaskdialog.h"
+
+using namespace TextAutoGenerateText;
+QuickAskDialog::QuickAskDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    // TODO
+}
+
+QuickAskDialog::~QuickAskDialog()
+{
+    // TODO
+}
+
+#include "moc_quickaskdialog.cpp"
