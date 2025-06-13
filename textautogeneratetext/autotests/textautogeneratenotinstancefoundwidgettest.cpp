8ccd436b8bdda12eca4aa0bea487957fbdb9fcd1
Laurent Montel <montel@kde.org>
2025-06-13 07:25:40 +0200

    Add textautogeneratenotinstancefoundwidgettest

---
+++ textautogeneratetext/autotests/textautogeneratenotinstancefoundwidgettest.cpp
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratenotinstancefoundwidgettest.h"
+#include "widgets/common/textautogeneratenotinstancefoundwidget.h"
+#include <QTest>
+QTEST_MAIN(TextAutoGenerateNotInstanceFoundWidgetTest)
+
+TextAutoGenerateNotInstanceFoundWidgetTest::TextAutoGenerateNotInstanceFoundWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TextAutoGenerateNotInstanceFoundWidgetTest::shouldHaveDefaultValues()
+{
+    TextAutoGenerateText::TextAutoGenerateNotInstanceFoundWidget w;
+    // TODO
+}
---
+++ textautogeneratetext/autotests/textautogeneratenotinstancefoundwidgettest.h
@@ -0,0 +1,18 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class TextAutoGenerateNotInstanceFoundWidgetTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateNotInstanceFoundWidgetTest(QObject *parent = nullptr);
+    ~TextAutoGenerateNotInstanceFoundWidgetTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
