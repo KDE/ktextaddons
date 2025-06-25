30bad0397bed33782a8500cf6aca73b04d3c0bb0
Laurent Montel <montel@kde.org>
2025-06-25 13:23:23 +0200

    Add genericnetworkmodelavailableinfowidgettest

---
+++ textautogeneratetext/plugins/genericnetworkplugin/autotests/genericnetworkmodelavailableinfowidgettest.cpp
@@ -0,0 +1,21 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "genericnetworkmodelavailableinfowidgettest.h"
+#include "modelsmanager/genericnetworkmodelavailableinfowidget.h"
+#include <QTest>
+QTEST_MAIN(GenericNetworkModelAvailableInfoWidgetTest)
+
+GenericNetworkModelAvailableInfoWidgetTest::GenericNetworkModelAvailableInfoWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void GenericNetworkModelAvailableInfoWidgetTest::shouldHaveDefaultValues()
+{
+    GenericNetworkModelAvailableInfoWidget w;
+    // TODO
+}
