3c20581968ca69a4f54c0a7dd8ef88f9a3a06006
Laurent Montel <montel@kde.org>
2025-06-25 07:57:18 +0200

    Add genericnetworkmodelavailableinfowidget

---
+++ textautogeneratetext/plugins/genericnetworkplugin/modelsmanager/genericnetworkmodelavailableinfowidget.cpp
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "genericnetworkmodelavailableinfowidget.h"
+
+GenericNetworkModelAvailableInfoWidget::GenericNetworkModelAvailableInfoWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+GenericNetworkModelAvailableInfoWidget::~GenericNetworkModelAvailableInfoWidget() = default;
+
+#include "moc_genericnetworkmodelavailableinfowidget.cpp"
