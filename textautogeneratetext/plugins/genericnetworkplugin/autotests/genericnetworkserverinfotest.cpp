4d32702089003d912138ce46d69e12ee0c730875
Laurent Montel <montel@kde.org>
2025-06-28 14:28:15 +0200

    Fix autotest

--- textautogeneratetext/plugins/genericnetworkplugin/autotests/genericnetworkserverinfotest.cpp
+++ textautogeneratetext/plugins/genericnetworkplugin/autotests/genericnetworkserverinfotest.cpp
@@ -27,7 +27,7 @@ void GenericNetworkServerInfoTest::shouldConvertPluginNetworkTypeFromString()
 void GenericNetworkServerInfoTest::shouldCheckApiUrl()
 {
     GenericNetworkServerInfo info;
-    QCOMPARE(info.apiUrl(GenericNetworkManager::PluginNetworkType::MistralAI), u"https://chat.mistral.ai/"_s);
+    QCOMPARE(info.apiUrl(GenericNetworkManager::PluginNetworkType::MistralAI), u"https://api.mistral.ai/v1/"_s);
     QCOMPARE(info.apiUrl(GenericNetworkManager::PluginNetworkType::OpenAI), u"https://api.openai.com/v1/"_s);
     QCOMPARE(info.apiUrl(GenericNetworkManager::PluginNetworkType::KlusterAI), u"https://api.kluster.ai/v1/"_s);
 }
