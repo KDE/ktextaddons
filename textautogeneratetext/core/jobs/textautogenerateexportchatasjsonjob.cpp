38c29cb0d5d1f8adde7048c9c5316ace634b70c9
Laurent Montel <montel@kde.org>
2025-07-01 19:37:56 +0200

    Move job from kaichat

---
+++ textautogeneratetext/core/jobs/textautogenerateexportchatasjsonjob.cpp
@@ -0,0 +1,44 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "textautogenerateexportchatasjsonjob.h"
+
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+using namespace TextAutoGenerateText;
+using namespace Qt::Literals::StringLiterals;
+TextAutoGenerateExportChatAsJsonJob::TextAutoGenerateExportChatAsJsonJob(QObject *parent)
+    : TextAutoGenerateText::TextAutoGenerateExportChatBaseJob{parent}
+{
+}
+
+TextAutoGenerateExportChatAsJsonJob::~TextAutoGenerateExportChatAsJsonJob() = default;
+
+void TextAutoGenerateExportChatAsJsonJob::exportChat()
+{
+    QJsonObject obj;
+    QJsonArray messages;
+    for (const auto &message : mInfo.listMessages) {
+        const QByteArray ba = TextAutoGenerateText::TextAutoGenerateMessage::serialize(message, false);
+        const QJsonDocument d = QJsonDocument::fromJson(ba);
+        messages.append(d.object());
+        // qDebug() << " ba " << ba;
+    }
+    obj[u"messages"_s] = messages;
+    obj[u"title"_s] = mInfo.chatTitle;
+    // qDebug() << " obj" << QJsonDocument(obj).toJson();
+    QFile data(mInfo.filename);
+    if (data.open(QFile::WriteOnly)) {
+        QTextStream out(&data);
+        out << QJsonDocument(obj).toJson();
+    }
+    deleteLater();
+}
+
+#include "moc_textautogenerateexportchatasjsonjob.cpp"
---
+++ textautogeneratetext/core/jobs/textautogenerateexportchatasjsonjob.h
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+#pragma once
+
+#include "textautogeneratetext_export.h"
+#include <TextAutoGenerateText/TextAutoGenerateExportChatBaseJob>
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateExportChatAsJsonJob : public TextAutoGenerateText::TextAutoGenerateExportChatBaseJob
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateExportChatAsJsonJob(QObject *parent = nullptr);
+    ~TextAutoGenerateExportChatAsJsonJob() override;
+
+protected:
+    void exportChat() override;
+};
+}
