d9bd4f75aabb707c24392e8ddcca1e03255d8efe
Laurent Montel <montel@kde.org>
2025-08-05 08:32:51 +0200

    Specify action against OS

--- textautogeneratetext/widgets/common/textautogeneratenotworkingmessagewidget.cpp
+++ textautogeneratetext/widgets/common/textautogeneratenotworkingmessagewidget.cpp
@@ -24,18 +24,26 @@ TextAutoGenerateNotWorkingMessageWidget::~TextAutoGenerateNotWorkingMessageWidge
 
 void TextAutoGenerateNotWorkingMessageWidget::slotDownloadOllama()
 {
-    QDesktopServices::openUrl(QUrl(u"https://ollama.com/download/"_s));
+#if defined(Q_OS_WIN)
+    QDesktopServices::openUrl(QUrl(u"https://ollama.com/download/windows"_s));
+#elif defined(Q_OS_MACOS)
+    QDesktopServices::openUrl(QUrl(u"https://ollama.com/download/mac"_s));
+#endif
 }
 
 void TextAutoGenerateNotWorkingMessageWidget::setMessageInfo(const QString &errorMessage)
 {
     const QString ollamaPath = TextAutoGenerateText::TextAutoGenerateTextUtils::findExecutable(u"ollama"_s);
     if (ollamaPath.isEmpty()) {
+#if !defined(Q_OS_WIN) && !defined(Q_OS_MACOS)
+        setText(i18n("Ollama not found on system. Ask to your administrator system to install it."));
+#else
         setText(i18n("Ollama not found on system. Please install it."));
         auto downloadOllamaAction = new QAction(i18nc("@action", "Download Ollama"), this);
         downloadOllamaAction->setObjectName(u"downloadOllamaAction"_s);
         connect(downloadOllamaAction, &QAction::triggered, this, &TextAutoGenerateNotWorkingMessageWidget::slotDownloadOllama);
         addAction(downloadOllamaAction);
+#endif
     } else {
         setText(errorMessage);
         auto startOllamaAction = new QAction(i18nc("@action", "Start Ollama"), this);
