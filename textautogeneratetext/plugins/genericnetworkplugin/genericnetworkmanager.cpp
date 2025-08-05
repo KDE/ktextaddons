42b2ca92f2b1e613ef7b535e4361c74259727c40
Laurent Montel <montel@kde.org>
2025-08-05 13:35:03 +0200

    Remove unused code

--- textautogeneratetext/plugins/genericnetworkplugin/genericnetworkmanager.cpp
+++ textautogeneratetext/plugins/genericnetworkplugin/genericnetworkmanager.cpp
@@ -102,11 +102,6 @@ TextAutoGenerateText::TextAutoGenerateReply *GenericNetworkManager::getChatCompl
     if (mGenericNetworkSettings->seed() > 0) {
         data["seed"_L1] = mGenericNetworkSettings->seed();
     }
-    /*
-    if (!OllamaSettings::systemPrompt().isEmpty()) {
-        data["system"_L1] = OllamaSettings::systemPrompt();
-    }
-    */
     qCDebug(AUTOGENERATETEXT_GENERICNETWORK_LOG) << " JSon: " << data << " req " << req.url();
     auto reply = new GenericNetworkReply{
         TextAutoGenerateText::TextAutoGenerateEngineAccessManager::self()->networkManager()->post(req, QJsonDocument(data).toJson(QJsonDocument::Compact)),
