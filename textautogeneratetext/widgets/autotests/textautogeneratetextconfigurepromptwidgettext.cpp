7c23f775be65131099e48d8ca1939afebde985af
Laurent Montel <montel@kde.org>
2025-08-06 07:35:57 +0200

    Add textautogeneratetextconfigurepromptwidgettext

---
+++ textautogeneratetext/widgets/autotests/textautogeneratetextconfigurepromptwidgettext.cpp
@@ -0,0 +1,33 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratetextconfigurepromptwidgettext.h"
+#include "widgets/textautogeneratetextconfigurepromptwidget.h"
+#include <QLabel>
+#include <QPlainTextEdit>
+#include <QTest>
+#include <QVBoxLayout>
+using namespace Qt::Literals::StringLiterals;
+
+QTEST_MAIN(TextAutoGenerateTextConfigurePromptWidgetText)
+TextAutoGenerateTextConfigurePromptWidgetText::TextAutoGenerateTextConfigurePromptWidgetText(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TextAutoGenerateTextConfigurePromptWidgetText::shouldHaveDefaultValues()
+{
+    TextAutoGenerateText::TextAutoGenerateTextConfigurePromptWidget w;
+    auto mainLayout = w.findChild<QVBoxLayout *>(u"mainLayout"_s);
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto label = w.findChild<QLabel *>(u"label"_s);
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+
+    auto mSystemPrompt = w.findChild<QPlainTextEdit *>(u"mSystemPrompt"_s);
+    QVERIFY(mSystemPrompt);
+}
