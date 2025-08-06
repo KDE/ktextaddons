c3217e68c702f5adbf86527a21bacb9cc45323b4
Laurent Montel <montel@kde.org>
2025-08-06 13:16:20 +0200

    Remove debug

--- textautogeneratetext/core/jobs/textautogenerateaskjob.cpp
+++ textautogeneratetext/core/jobs/textautogenerateaskjob.cpp
@@ -88,7 +88,6 @@ QString TextAutoGenerateAskJob::text() const
 void TextAutoGenerateAskJob::setText(const QString &newText)
 {
     mText = newText;
-    qDebug() << " TextAutoGenerateAskJob::setText " << mText;
 }
 
 #include "moc_textautogenerateaskjob.cpp"
