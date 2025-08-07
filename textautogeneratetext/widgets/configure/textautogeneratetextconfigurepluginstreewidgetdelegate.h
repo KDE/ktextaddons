f935eff908ca609e827ed6691a33dc5ec6cfb6d1
Laurent Montel <montel@kde.org>
2025-08-07 13:26:24 +0200

    Add delegate support

---
+++ textautogeneratetext/widgets/configure/textautogeneratetextconfigurepluginstreewidgetdelegate.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include <QStyledItemDelegate>
+namespace TextAutoGenerateText
+{
+class TextAutoGenerateTextConfigurePluginsTreeWidgetDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateTextConfigurePluginsTreeWidgetDelegate(QObject *parent = nullptr);
+    ~TextAutoGenerateTextConfigurePluginsTreeWidgetDelegate() override;
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    [[nodiscard]] QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+};
+}
