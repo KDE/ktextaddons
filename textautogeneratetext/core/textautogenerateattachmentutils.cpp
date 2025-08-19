6ca86975e1ebb0e17b523327013a2c235baf630a
Laurent Montel <montel@kde.org>
2025-08-19 13:49:55 +0200

    Prepare to add utils clas for attachment

---
+++ textautogeneratetext/core/textautogenerateattachmentutils.cpp
@@ -0,0 +1,11 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogenerateattachmentutils.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateAttachmentUtils::TextAutoGenerateAttachmentUtils()
+{
+}
