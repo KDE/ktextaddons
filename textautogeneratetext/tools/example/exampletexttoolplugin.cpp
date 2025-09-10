e270ec66d134bf1d1e2b0b30a38636608e6926a0
Laurent Montel <montel@kde.org>
2025-09-10 13:52:58 +0200

    Implement plugin

--- textautogeneratetext/tools/example/exampletexttoolplugin.cpp
+++ textautogeneratetext/tools/example/exampletexttoolplugin.cpp
@@ -5,12 +5,36 @@
 */
 
 #include "exampletexttoolplugin.h"
+#include <KPluginFactory>
+using namespace Qt::Literals::StringLiterals;
+K_PLUGIN_CLASS_WITH_JSON(ExampleTextToolPlugin, "textautogeneratetext_exampleplugin.json")
 
-ExampleTextToolPlugin::ExampleTextToolPlugin(QObject *parent)
+ExampleTextToolPlugin::ExampleTextToolPlugin(QObject *parent, const QVariantList &)
     : TextAutoGenerateTextToolPlugin{parent}
 {
 }
 
 ExampleTextToolPlugin::~ExampleTextToolPlugin() = default;
 
+QString ExampleTextToolPlugin::executeTool()
+{
+    return {};
+}
+
+QString ExampleTextToolPlugin::displayName()
+{
+    return {};
+}
+
+QString ExampleTextToolPlugin::descriptions() const
+{
+    return {};
+}
+
+QJsonObject ExampleTextToolPlugin::metadata() const
+{
+    return {};
+}
+
+#include "exampletexttoolplugin.moc"
 #include "moc_exampletexttoolplugin.cpp"
--- textautogeneratetext/tools/example/exampletexttoolplugin.h
+++ textautogeneratetext/tools/example/exampletexttoolplugin.h
@@ -7,11 +7,16 @@
 #pragma once
 
 #include "core/tools/textautogeneratetexttoolplugin.h"
-
+#include <QVariant>
 class ExampleTextToolPlugin : public TextAutoGenerateText::TextAutoGenerateTextToolPlugin
 {
     Q_OBJECT
 public:
-    explicit ExampleTextToolPlugin(QObject *parent = nullptr);
+    explicit ExampleTextToolPlugin(QObject *parent = nullptr, const QVariantList & = {});
     ~ExampleTextToolPlugin() override;
+
+    [[nodiscard]] QString executeTool() override;
+    [[nodiscard]] QString displayName() override;
+    [[nodiscard]] QString descriptions() const override;
+    [[nodiscard]] QJsonObject metadata() const override;
 };
