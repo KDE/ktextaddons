76b5865dae04e4d7680214209f625617799870de
Laurent Montel <montel@kde.org>
2025-09-10 06:53:10 +0200

    We don't have kcfg* file

--- textautogeneratetext/Messages.sh
+++ textautogeneratetext/Messages.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
-$EXTRACTRC `find . -name '*.kcfg' -or -name '*.kcfg.in'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libtextautogeneratetext.pot
