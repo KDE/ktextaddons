e339fa395fd3cd8d639a89362ff1c1a8ea0012d2
Laurent Montel <montel@kde.org>
2025-09-11 19:55:53 +0200

    Prepare to create textautogeneratetoolswidget

---
+++ textautogeneratetext/widgets/toolswidget/textautogeneratetoolswidget.cpp
@@ -0,0 +1,14 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratetoolswidget.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateToolsWidget::TextAutoGenerateToolsWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+TextAutoGenerateToolsWidget::~TextAutoGenerateToolsWidget() = default;
