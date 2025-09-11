a7d79d9cd5275038d5600e57f12e208cd93aa702
Laurent Montel <montel@kde.org>
2025-09-11 07:46:09 +0200

    Prepare exampletexttoolplugindialog

---
+++ textautogeneratetext/tools/example/exampletexttoolplugindialog.h
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include <QDialog>
+
+class ExampleTextToolPluginDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit ExampleTextToolPluginDialog(QWidget *parent = nullptr);
+    ~ExampleTextToolPluginDialog() override;
+};
