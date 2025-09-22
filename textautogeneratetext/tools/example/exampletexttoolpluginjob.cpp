e80829c514707264210df8ea59ac89f0627f69e2
Laurent Montel <montel@kde.org>
2025-09-22 13:24:57 +0200

    Implement job

--- textautogeneratetext/tools/example/exampletexttoolpluginjob.cpp
+++ textautogeneratetext/tools/example/exampletexttoolpluginjob.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "exampletexttoolpluginjob.h"
+#include <QDebug>
 
 ExampleTextToolPluginJob::ExampleTextToolPluginJob(QObject *parent)
     : QObject{parent}
@@ -12,3 +13,30 @@ ExampleTextToolPluginJob::ExampleTextToolPluginJob(QObject *parent)
 }
 
 ExampleTextToolPluginJob::~ExampleTextToolPluginJob() = default;
+
+const QList<TextAutoGenerateText::TextAutoGenerateReply::ToolCallArgument> &ExampleTextToolPluginJob::toolArguments() const
+{
+    return mToolArguments;
+}
+
+void ExampleTextToolPluginJob::setToolArguments(const QList<TextAutoGenerateText::TextAutoGenerateReply::ToolCallArgument> &newToolArguments)
+{
+    mToolArguments = newToolArguments;
+}
+
+bool ExampleTextToolPluginJob::canStart() const
+{
+    return !mToolArguments.isEmpty();
+}
+
+void ExampleTextToolPluginJob::start()
+{
+    if (!canStart()) {
+        qWarning() << " Impossible to start ExampleTextToolPluginJob";
+        deleteLater();
+        return;
+    }
+    // TODO
+}
+
+#include "moc_exampletexttoolpluginjob.cpp"
--- textautogeneratetext/tools/example/exampletexttoolpluginjob.h
+++ textautogeneratetext/tools/example/exampletexttoolpluginjob.h
@@ -5,11 +5,21 @@
 */
 #pragma once
 #include <QObject>
-
+#include <TextAutoGenerateText/TextAutoGenerateReply>
 class ExampleTextToolPluginJob : public QObject
 {
     Q_OBJECT
 public:
     explicit ExampleTextToolPluginJob(QObject *parent = nullptr);
     ~ExampleTextToolPluginJob() override;
+
+    const QList<TextAutoGenerateText::TextAutoGenerateReply::ToolCallArgument> &toolArguments() const;
+    void setToolArguments(const QList<TextAutoGenerateText::TextAutoGenerateReply::ToolCallArgument> &newToolArguments);
+
+    [[nodiscard]] bool canStart() const;
+
+    void start();
+
+private:
+    QList<TextAutoGenerateText::TextAutoGenerateReply::ToolCallArgument> mToolArguments;
 };
