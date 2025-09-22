385db97403e4840db59437322981e3cb0507eb95
Laurent Montel <montel@kde.org>
2025-09-22 09:00:35 +0200

    Prepare job

---
+++ textautogeneratetext/tools/example/exampletexttoolpluginjob.cpp
@@ -0,0 +1,14 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "exampletexttoolpluginjob.h"
+
+ExampleTextToolPluginJob::ExampleTextToolPluginJob(QObject *parent)
+    : QObject{parent}
+{
+}
+
+ExampleTextToolPluginJob::~ExampleTextToolPluginJob() = default;
---
+++ textautogeneratetext/tools/example/exampletexttoolpluginjob.h
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include <QObject>
+
+class ExampleTextToolPluginJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExampleTextToolPluginJob(QObject *parent = nullptr);
+    ~ExampleTextToolPluginJob() override;
+};
