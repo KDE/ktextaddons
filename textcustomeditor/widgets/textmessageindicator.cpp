ad74399016beb8efd0075dfa753643613b40d721
Laurent Montel <montel@kde.org>
2025-09-25 08:44:05 +0200

    Coding style

--- textcustomeditor/widgets/textmessageindicator.cpp
+++ textcustomeditor/widgets/textmessageindicator.cpp
@@ -5,7 +5,6 @@
 */
 
 #include "textmessageindicator.h"
-using namespace Qt::Literals::StringLiterals;
 
 #include <QAbstractScrollArea>
 #include <QApplication>
@@ -14,6 +13,7 @@ using namespace Qt::Literals::StringLiterals;
 #include <QStyle>
 #include <QTimer>
 
+using namespace Qt::Literals::StringLiterals;
 using namespace TextCustomEditor;
 TextMessageIndicator::TextMessageIndicator(QWidget *parent)
     : QWidget(parent)
