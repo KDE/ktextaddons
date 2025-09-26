16fa2bc28952cacb32ccb5dbbe6a3965f52f4b06
Laurent Montel <montel@kde.org>
2025-09-26 08:53:08 +0200

    Improve test apps

--- textemoticons/widgets/tests/emoticonwidgetactionwidgetgui/emoticonwidgetactionwidgetgui.cpp
+++ textemoticons/widgets/tests/emoticonwidgetactionwidgetgui/emoticonwidgetactionwidgetgui.cpp
@@ -8,9 +8,11 @@
 #include <QApplication>
 #include <QContextMenuEvent>
 #include <QMenu>
+#include <QStandardPaths>
 #include <QTextEdit>
 #include <QVBoxLayout>
 #include <TextEmoticonsWidgets/EmoticonWidgetAction>
+
 using namespace Qt::Literals::StringLiterals;
 EmoticonWidgetActionWidgetGui::EmoticonWidgetActionWidgetGui(QWidget *parent)
     : QWidget{parent}
@@ -18,8 +20,12 @@ EmoticonWidgetActionWidgetGui::EmoticonWidgetActionWidgetGui(QWidget *parent)
 {
     auto mainLayout = new QVBoxLayout(this);
     mainLayout->setObjectName(u"mainLayout"_s);
+    mainLayout->setContentsMargins({});
 
     mainLayout->addWidget(mTextEdit);
+    QFont f = mTextEdit->font();
+    f.setPointSize(24);
+    mTextEdit->setFont(f);
 }
 
 EmoticonWidgetActionWidgetGui::~EmoticonWidgetActionWidgetGui() = default;
@@ -35,6 +41,10 @@ void EmoticonWidgetTextEdit::contextMenuEvent(QContextMenuEvent *event)
 {
     QMenu *menu = createStandardContextMenu();
     auto emoticonAction = new TextEmoticonsWidgets::EmoticonWidgetAction({}, this);
+    connect(emoticonAction, &TextEmoticonsWidgets::EmoticonWidgetAction::insertEmoji, this, [this](const QString &str) {
+        insertPlainText(str);
+    });
+    menu->addSeparator();
     menu->addAction(emoticonAction);
     menu->exec(event->globalPos());
     delete menu;
@@ -44,7 +54,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     EmoticonWidgetActionWidgetGui window;
-
+    QStandardPaths::setTestModeEnabled(true);
     window.show();
     return app.exec();
 }
