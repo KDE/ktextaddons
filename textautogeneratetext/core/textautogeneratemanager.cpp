ce06e05aa244632fac3ba392abca48f9e241b27f
Laurent Montel <montel@kde.org>
2025-09-29 07:52:32 +0200

    Improve debug

--- textautogeneratetext/core/textautogeneratemanager.cpp
+++ textautogeneratetext/core/textautogeneratemanager.cpp
@@ -262,7 +262,7 @@ void TextAutoGenerateManager::setSaveInDatabase(bool newSaveInDatabase)
 
 void TextAutoGenerateManager::callTools(const QByteArray &chatId, const QByteArray &uuid, const QList<TextAutoGenerateReply::ToolCallArgumentInfo> &info)
 {
-    qDebug() << " chatId : " << chatId << " uuid : " << uuid << " info: " << info;
+    qDebug() << "TextAutoGenerateManager::callTools chatId : " << chatId << " uuid : " << uuid << " info: " << info;
     // TODO
 }
 
