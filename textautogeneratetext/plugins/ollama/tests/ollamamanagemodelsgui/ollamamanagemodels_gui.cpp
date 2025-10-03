66e5de8e54740c7a52c34e081166f8e69a1e69e2
Laurent Montel <montel@kde.org>
2025-10-03 07:45:32 +0200

    const'ify

--- textautogeneratetext/plugins/ollama/tests/ollamamanagemodelsgui/ollamamanagemodels_gui.cpp
+++ textautogeneratetext/plugins/ollama/tests/ollamamanagemodelsgui/ollamamanagemodels_gui.cpp
@@ -13,7 +13,7 @@
 
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
+    const QApplication app(argc, argv);
     QStandardPaths::setTestModeEnabled(true);
     QCommandLineParser parser;
     parser.addVersionOption();
