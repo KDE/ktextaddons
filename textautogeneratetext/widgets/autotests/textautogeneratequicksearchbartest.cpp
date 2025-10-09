8a1467b7dad68e9d1d9e6592a61176b02b526c57
Laurent Montel <montel@kde.org>
2025-10-09 22:32:02 +0200

    Add autotest

--- textautogeneratetext/widgets/autotests/textautogeneratequicksearchbartest.cpp
+++ textautogeneratetext/widgets/autotests/textautogeneratequicksearchbartest.cpp
@@ -5,9 +5,12 @@
 */
 #include "textautogeneratequicksearchbartest.h"
 #include "widgets/view/textautogeneratequicksearchbar.h"
+#include <QHBoxLayout>
+#include <QLineEdit>
 #include <QTest>
+#include <QToolButton>
 QTEST_MAIN(TextAutoGenerateQuickSearchBarTest)
-
+using namespace Qt::Literals::StringLiterals;
 TextAutoGenerateQuickSearchBarTest::TextAutoGenerateQuickSearchBarTest(QObject *parent)
     : QObject{parent}
 {
@@ -16,7 +19,26 @@ TextAutoGenerateQuickSearchBarTest::TextAutoGenerateQuickSearchBarTest(QObject *
 void TextAutoGenerateQuickSearchBarTest::shouldHaveDefaultValues()
 {
     TextAutoGenerateText::TextAutoGenerateQuickSearchBar w;
-    // TODO
+
+    auto mainLayout = w.findChild<QHBoxLayout *>(u"mainLayout"_s);
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mSearchLineEdit = w.findChild<QLineEdit *>(u"mSearchLineEdit"_s);
+    QVERIFY(mSearchLineEdit);
+    QVERIFY(mSearchLineEdit->isClearButtonEnabled());
+    QVERIFY(mSearchLineEdit->text().isEmpty());
+    QVERIFY(!mSearchLineEdit->placeholderText().isEmpty());
+
+    auto mNextButton = w.findChild<QToolButton *>(u"mNextButton"_s);
+    QVERIFY(mNextButton);
+    QVERIFY(mNextButton->autoRaise());
+    QVERIFY(!mNextButton->toolTip().isEmpty());
+
+    auto mPreviousButton = w.findChild<QToolButton *>(u"mPreviousButton"_s);
+    QVERIFY(mPreviousButton);
+    QVERIFY(mPreviousButton->autoRaise());
+    QVERIFY(!mPreviousButton->toolTip().isEmpty());
 }
 
 #include "moc_textautogeneratequicksearchbartest.cpp"
