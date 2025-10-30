4c6565a1f5c9cc881b4ac4294c21123b8bb395e5
Laurent Montel <montel@kde.org>
2025-10-30 08:15:21 +0100

    Implement TextAutoGenerateTextToSpeechInfo

---
+++ textautogeneratetext/core/texttospeech/textautogeneratetexttospeechinfo.h
@@ -0,0 +1,31 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetext_export.h"
+#include <QDebug>
+
+#include <QByteArray>
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateTextToSpeechInfo
+{
+public:
+    TextAutoGenerateTextToSpeechInfo();
+    ~TextAutoGenerateTextToSpeechInfo();
+
+    [[nodiscard]] QByteArray chatId() const;
+    void setChatId(const QByteArray &newChatId);
+
+    [[nodiscard]] QByteArray messageId() const;
+    void setMessageId(const QByteArray &newMessageId);
+
+private:
+    QByteArray mChatId;
+    QByteArray mMessageId;
+};
+}
+Q_DECLARE_TYPEINFO(TextAutoGenerateText::TextAutoGenerateTextToSpeechInfo, Q_RELOCATABLE_TYPE);
+TEXTAUTOGENERATETEXT_EXPORT QDebug operator<<(QDebug d, const TextAutoGenerateText::TextAutoGenerateTextToSpeechInfo &t);
