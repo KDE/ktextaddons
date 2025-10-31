KDE/ktextaddons

commit 83a3d86ae46587e13508d4c2629ce85e3463fcf8

Don't add  TextToSpeech support when we don't have Qt6::TextToSpeech

textautogeneratetext/widgets/view/delegate/textautogeneratelistviewdelegate.cpp
@@ -4,6 +4,7 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratelistviewdelegate.h"
+#include "config-textautogeneratetext.h"
 #include "textautogeneratetextglobalconfig.h"
 
 #include "core/models/textautogeneratemessagesmodel.h"
@@ -197,7 +198,10 @@ void TextAutoGenerateListViewDelegate::drawDateAndIcons(QPainter *painter,
         int iconSizeAreaWidth = 0;
         const bool inProgress = index.data(TextAutoGenerateMessagesModel::FinishedRole).toBool();
         if (isMouseOver) {
-            const int offset = TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech() ? 1 : 0;
+            int offset = 0;
+#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+            offset = TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech() ? 1 : 0;
+#endif
 
             if (!inProgress) {
                 iconSizeAreaWidth = (3 + offset) * (buttonIconSize(option) + TextAutoGenerateDelegateUtils::iconSpacing());
@@ -220,9 +224,11 @@ void TextAutoGenerateListViewDelegate::drawDateAndIcons(QPainter *painter,
         if (layout.removeIconRect.isValid()) {
             mRemoveIcon.paint(painter, layout.removeIconRect);
         }
+#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
         if (TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech() && layout.textToSpeechIconRect.isValid()) {
             mTextToSpeechIcon.paint(painter, layout.textToSpeechIconRect);
         }
+#endif
         if (layout.cancelIconRect.isValid()) {
             mCancelIcon.paint(painter, layout.cancelIconRect);
         }
@@ -335,11 +341,12 @@ TextAutoGenerateListViewDelegate::MessageLayout TextAutoGenerateListViewDelegate
 
         layout.infoIconRect = QRect(layout.textRect.topRight().x(), layout.textRect.topRight().y(), iconSize, iconSize);
     }
-
+#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
     if (TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech()) {
         positionIcon -= iconSize + TextAutoGenerateDelegateUtils::iconSpacing();
         layout.textToSpeechIconRect = QRect(positionIcon, lineY, iconSize, iconSize);
     }
+#endif
 
     const QSize progressIndicatorSize = TextAutoGenerateDelegateUtils::progressIndicatorSize();
     layout.inProgressRect = QRect(layout.textRect.topRight().x() - progressIndicatorSize.width(),

textautogeneratetext/widgets/view/textautogeneratebaselistview.cpp
@@ -4,7 +4,7 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "textautogeneratebaselistview.h"
-
+#include "config-textautogeneratetext.h"
 #include "delegate/textautogeneratelistviewbasedelegate.h"
 #include <KLocalizedString>
 #include <QApplication>
@@ -16,6 +16,9 @@
 #include <TextAutoGenerateText/TextAutoGeneratePluginText>
 #include <TextAutoGenerateText/TextAutoGeneratePluginTextInterface>
 #include <TextAutoGenerateText/TextAutoGeneratePluginTextManager>
+#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+#include <TextEditTextToSpeech/TextToSpeech>
+#endif
 
 using namespace TextAutoGenerateText;
 using namespace Qt::Literals::StringLiterals;
@@ -165,10 +168,12 @@ void TextAutoGenerateBaseListView::slotCopyMessage(const QModelIndex &index)
 
 void TextAutoGenerateBaseListView::slotTextToSpeechRequested(const QModelIndex &index)
 {
+#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
     const QString messageText = selectedText(index);
     if (messageText.isEmpty()) {
         return;
     }
+#endif
     // TODO text to speech
 }
 

CMakeLists.txt
@@ -209,16 +209,6 @@ if(BERGAMOT_TRANSLATOR_PLUGIN)
     )
 endif()
 
-add_subdirectory(textaddonswidgets)
-add_subdirectory(textautocorrection)
-add_subdirectory(textgrammarcheck)
-add_subdirectory(texttranslator)
-add_subdirectory(textemoticons)
-add_subdirectory(textutils)
-find_package(Qt6 ${REQUIRED_QT_VERSION} CONFIG REQUIRED MultimediaWidgets)
-add_subdirectory(textspeechtotext)
-add_subdirectory(textautogeneratetext)
-
 find_package(Qt6TextToSpeech ${QT_REQUIRED_VERSION} CONFIG)
 set_package_properties(
     Qt6TextToSpeech
@@ -231,6 +221,16 @@ set_package_properties(
 if(TARGET Qt6::TextToSpeech)
     add_subdirectory(textedittexttospeech)
 endif()
+
+add_subdirectory(textaddonswidgets)
+add_subdirectory(textautocorrection)
+add_subdirectory(textgrammarcheck)
+add_subdirectory(texttranslator)
+add_subdirectory(textemoticons)
+add_subdirectory(textutils)
+find_package(Qt6 ${REQUIRED_QT_VERSION} CONFIG REQUIRED MultimediaWidgets)
+add_subdirectory(textspeechtotext)
+add_subdirectory(textautogeneratetext)
 
 add_subdirectory(textcustomeditor)
 ecm_qt_install_logging_categories(

textautogeneratetext/CMakeLists.txt
@@ -35,6 +35,10 @@ if(NOT WIN32 AND NOT APPLE)
     set(HAVE_KTEXTADDONS_TEXTAUTOGENERATE_DBUS_SUPPORT TRUE)
 endif()
 
+if(TARGET Qt6::TextToSpeech)
+    set(HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT TRUE)
+endif()
+
 configure_file(config-textautogeneratetext.h.in ${CMAKE_CURRENT_BINARY_DIR}/config-textautogeneratetext.h)
 
 add_library(KF6TextAutoGenerateText)
@@ -370,6 +374,10 @@ target_link_libraries(
         KF6::TextUtils
         KF6::TextAddonsWidgets
 )
+
+if(TARGET Qt6::TextToSpeech)
+    target_link_libraries(KF6TextAutoGenerateText PRIVATE KF6::TextEditTextToSpeech)
+endif()
 
 if(NOT WIN32 AND NOT APPLE)
     target_link_libraries(KF6TextAutoGenerateText PRIVATE Qt::DBus)

textautogeneratetext/config-textautogeneratetext.h.in
@@ -7,3 +7,4 @@
 #pragma once
 
 #cmakedefine01 HAVE_KTEXTADDONS_TEXTAUTOGENERATE_DBUS_SUPPORT
+#cmakedefine01 HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT

commit 2cf9bbac9f2a0599a1d37f364586dd53981b8f32

Fix use of HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
Its always defined due to use of cmakedefine01, either 1 or 0 depending on if the cmake variable
HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT is set to true.

Fixes: 83a3d86ae46587e13508d4c2629ce85e3463fcf8
Signed-off-by: Alfred Wingate <parona@protonmail.com>

textautogeneratetext/widgets/view/delegate/textautogeneratelistviewdelegate.cpp
@@ -205,7 +205,7 @@ void TextAutoGenerateListViewDelegate::drawDateAndIcons(QPainter *painter,
         const bool inProgress = index.data(TextAutoGenerateMessagesModel::FinishedRole).toBool();
         if (isMouseOver) {
             int offset = 0;
-#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+#if HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
             offset = TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech() ? 1 : 0;
 #endif
 
@@ -230,7 +230,7 @@ void TextAutoGenerateListViewDelegate::drawDateAndIcons(QPainter *painter,
         if (layout.removeIconRect.isValid()) {
             mRemoveIcon.paint(painter, layout.removeIconRect);
         }
-#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+#if HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
         if (TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech() && layout.textToSpeechIconRect.isValid()) {
             mTextToSpeechIcon.paint(painter, layout.textToSpeechIconRect);
         }
@@ -347,7 +347,7 @@ TextAutoGenerateListViewDelegate::MessageLayout TextAutoGenerateListViewDelegate
 
         layout.infoIconRect = QRect(layout.textRect.topRight().x(), layout.textRect.topRight().y(), iconSize, iconSize);
     }
-#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+#if HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
     if (TextAutoGenerateText::TextAutogenerateTextGlobalConfig::self()->enableTextToSpeech()) {
         positionIcon -= iconSize + TextAutoGenerateDelegateUtils::iconSpacing();
         layout.textToSpeechIconRect = QRect(positionIcon, lineY, iconSize, iconSize);

textautogeneratetext/widgets/view/textautogeneratebaselistview.cpp
@@ -19,7 +19,7 @@
 #include <TextAutoGenerateText/TextAutoGeneratePluginText>
 #include <TextAutoGenerateText/TextAutoGeneratePluginTextInterface>
 #include <TextAutoGenerateText/TextAutoGeneratePluginTextManager>
-#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+#if HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
 #include <TextEditTextToSpeech/TextToSpeech>
 #endif
 
@@ -178,7 +178,7 @@ void TextAutoGenerateBaseListView::slotStopTextToSpeechRequested([[maybe_unused]
 
 void TextAutoGenerateBaseListView::slotTextToSpeechRequested(const QModelIndex &index)
 {
-#ifdef HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
+#if HAVE_KTEXTADDONS_TEXT_TO_SPEECH_SUPPORT
     const QString messageText = selectedText(index);
     if (messageText.isEmpty()) {
         return;