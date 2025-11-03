da88470a1c8fbb0e405ae8f1999ed52434148153
Laurent Montel <montel@kde.org>
2025-11-03 08:12:13 +0100

    Prepare support for multi attachments

---
+++ textautogeneratetext/core/textautogenerateattachments.cpp
@@ -0,0 +1,12 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogenerateattachments.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateAttachments::TextAutoGenerateAttachments()
+{
+}
