554c9b5b38c909ac3fb96b95955246402540b821
Laurent Montel <montel@kde.org>
2025-11-04 19:43:33 +0100

    Prepare to add attachment widget

---
+++ textautogeneratetext/widgets/common/textautogeneratetextlineeditattachmentwidget.cpp
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogeneratetextlineeditattachmentwidget.h"
+#include "widgets/common/textautogenerateflowlayout.h"
+using namespace TextAutoGenerateText;
+using namespace Qt::Literals::StringLiterals;
+TextAutoGenerateTextLineEditAttachmentWidget::TextAutoGenerateTextLineEditAttachmentWidget(QWidget *parent)
+    : QWidget{parent}
+{
+    auto mainLayout = new TextAutoGenerateFlowLayout(this);
+    mainLayout->setObjectName(u"mainLayout"_s);
+    mainLayout->setContentsMargins({});
+    // TODO
+}
+
+TextAutoGenerateTextLineEditAttachmentWidget::~TextAutoGenerateTextLineEditAttachmentWidget() = default;
