2facc58bfc8a9332bd0a7543ad559a8e93489153
Laurent Montel <montel@kde.org>
2025-11-05 07:55:21 +0100

    Add autotests

---
+++ textautogeneratetext/widgets/autotests/textautogeneratetextlineeditattachmentclickablewidgettest.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratetextlineeditattachmentclickablewidgettest.h"
+#include <QTest>
+QTEST_MAIN(TextAutoGenerateTextLineEditAttachmentClickableWidgetTest)
+
+TextAutoGenerateTextLineEditAttachmentClickableWidgetTest::TextAutoGenerateTextLineEditAttachmentClickableWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
---
+++ textautogeneratetext/widgets/autotests/textautogeneratetextlineeditattachmentwidgettest.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "textautogeneratetextlineeditattachmentwidgettest.h"
+#include <QTest>
+QTEST_MAIN(TextAutoGenerateTextLineEditAttachmentWidgetTest)
+
+TextAutoGenerateTextLineEditAttachmentWidgetTest::TextAutoGenerateTextLineEditAttachmentWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
