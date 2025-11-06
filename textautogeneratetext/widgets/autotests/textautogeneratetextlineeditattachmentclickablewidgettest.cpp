f29f2aeee3119e660650226c562c80f92eee4fac
Laurent Montel <montel@kde.org>
2025-11-06 06:58:12 +0100

    improve autotest

--- textautogeneratetext/widgets/autotests/textautogeneratetextlineeditattachmentclickablewidgettest.cpp
+++ textautogeneratetext/widgets/autotests/textautogeneratetextlineeditattachmentclickablewidgettest.cpp
@@ -5,6 +5,8 @@
 */
 #include "textautogeneratetextlineeditattachmentclickablewidgettest.h"
 #include "widgets/common/textautogeneratetextlineeditattachmentclickablewidget.h"
+#include <QHBoxLayout>
+#include <QLabel>
 #include <QTest>
 using namespace Qt::Literals::StringLiterals;
 
@@ -17,7 +19,19 @@ TextAutoGenerateTextLineEditAttachmentClickableWidgetTest::TextAutoGenerateTextL
 void TextAutoGenerateTextLineEditAttachmentClickableWidgetTest::shouldHaveDefaultValues()
 {
     TextAutoGenerateText::TextAutoGenerateTextLineEditAttachmentClickableWidget w({});
-    // TODO
+
+    auto mainLayout = w.findChild<QHBoxLayout *>(u"mainLayout"_s);
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mClickableLabel = w.findChild<TextAutoGenerateText::TextAutoGenerateTextLineEditAttachmentClickableLabel *>(u"mClickableLabel"_s);
+    QVERIFY(mClickableLabel);
+
+    auto mFileNameLabel = w.findChild<QLabel *>(u"mFileNameLabel"_s);
+    QVERIFY(mFileNameLabel);
+
+    auto mMimetypeLabel = w.findChild<QLabel *>(u"mMimetypeLabel"_s);
+    QVERIFY(mMimetypeLabel);
 }
 
 #include "moc_textautogeneratetextlineeditattachmentclickablewidgettest.cpp"
