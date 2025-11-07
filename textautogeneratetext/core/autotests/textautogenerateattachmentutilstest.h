443aa8e405e992c9bfcee1364166222ee67ec22b
Laurent Montel <montel@kde.org>
2025-11-07 07:53:09 +0100

    Add autotest

---
+++ textautogeneratetext/core/autotests/textautogenerateattachmentutilstest.h
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class TextAutoGenerateAttachmentUtilsTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateAttachmentUtilsTest(QObject *parent = nullptr);
+    ~TextAutoGenerateAttachmentUtilsTest() override = default;
+};
