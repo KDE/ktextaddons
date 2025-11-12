a43a2a07d73a65e6e74746a4f58cd02cf1670c13
Laurent Montel <montel@kde.org>
2025-11-12 08:26:46 +0100

    Add pluginutil class

---
+++ textaddonswidgets/plugins/pluginutil.cpp
@@ -0,0 +1,73 @@
+/*
+  SPDX-FileCopyrightText: 2016-2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "pluginutil.h"
+#include <KConfigGroup>
+#include <KSharedConfig>
+
+bool TextAddonsWidgets::PluginUtil::isPluginActivated(const QStringList &enabledPluginsList,
+                                                      const QStringList &disabledPluginsList,
+                                                      bool isEnabledByDefault,
+                                                      const QString &pluginId)
+{
+    if (pluginId.isEmpty()) {
+        return false;
+    }
+    const bool pluginEnabledByUser = enabledPluginsList.contains(pluginId);
+    const bool pluginDisabledByUser = disabledPluginsList.contains(pluginId);
+    if ((isEnabledByDefault && !pluginDisabledByUser) || (!isEnabledByDefault && pluginEnabledByUser)) {
+        return true;
+    }
+    return false;
+}
+
+TextAddonsWidgets::PluginUtil::PluginsStateList
+TextAddonsWidgets::PluginUtil::loadPluginSetting(const QString &configFileName, const QString &groupName, const QString &prefixSettingKey)
+{
+    PluginUtil::PluginsStateList pair;
+    KSharedConfigPtr config = KSharedConfig::openConfig(configFileName);
+    QStringList enabledPlugins;
+    QStringList disabledPlugins;
+    if (config->hasGroup(groupName)) {
+        const KConfigGroup grp = config->group(groupName);
+        enabledPlugins = grp.readEntry(QStringLiteral("%1Enabled").arg(prefixSettingKey), QStringList());
+        disabledPlugins = grp.readEntry(QStringLiteral("%1Disabled").arg(prefixSettingKey), QStringList());
+    }
+
+    pair.enabledPluginList = enabledPlugins;
+    pair.disabledPluginList = disabledPlugins;
+    return pair;
+}
+
+void TextAddonsWidgets::PluginUtil::savePluginSettings(const QString &configFileName,
+                                                       const QString &groupName,
+                                                       const QString &prefixSettingKey,
+                                                       const QStringList &enabledPluginsList,
+                                                       const QStringList &disabledPluginsList)
+{
+    KSharedConfigPtr config = KSharedConfig::openConfig(configFileName);
+    KConfigGroup grp = config->group(groupName);
+    if (enabledPluginsList.isEmpty()) {
+        grp.deleteEntry(QStringLiteral("%1Enabled").arg(prefixSettingKey));
+    } else {
+        grp.writeEntry(QStringLiteral("%1Enabled").arg(prefixSettingKey), enabledPluginsList);
+    }
+    if (disabledPluginsList.isEmpty()) {
+        grp.deleteEntry(QStringLiteral("%1Disabled").arg(prefixSettingKey));
+    } else {
+        grp.writeEntry(QStringLiteral("%1Disabled").arg(prefixSettingKey), disabledPluginsList);
+    }
+}
+
+TextAddonsWidgets::PluginUtilData TextAddonsWidgets::PluginUtil::createPluginMetaData(const KPluginMetaData &metaData)
+{
+    PluginUtilData pluginData;
+    pluginData.mDescription = metaData.description();
+    pluginData.mName = metaData.name();
+    pluginData.mIdentifier = metaData.pluginId();
+    pluginData.mEnableByDefault = metaData.isEnabledByDefault();
+    return pluginData;
+}
---
+++ textaddonswidgets/plugins/pluginutil.h
@@ -0,0 +1,54 @@
+/*
+  SPDX-FileCopyrightText: 2016-2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "textaddonswidgets_export.h"
+
+#include <KPluginMetaData>
+#include <QString>
+#include <QStringList>
+
+namespace TextAddonsWidgets
+{
+/**
+ * @brief The PluginUtilData class
+ * @author Laurent Montel <montel@kde.org>
+ */
+class TEXTADDONSWIDGETS_EXPORT PluginUtilData
+{
+public:
+    PluginUtilData() = default;
+
+    QStringList mExtraInfo;
+    QString mDescription;
+    QString mIdentifier;
+    QString mName;
+    bool mEnableByDefault = false;
+    bool mHasConfigureDialog = false;
+};
+
+namespace PluginUtil
+{
+struct TEXTADDONSWIDGETS_EXPORT PluginsStateList {
+    QStringList enabledPluginList;
+    QStringList disabledPluginList;
+};
+
+[[nodiscard]] TEXTADDONSWIDGETS_EXPORT bool
+isPluginActivated(const QStringList &enabledPluginsList, const QStringList &disabledPluginsList, bool isEnabledByDefault, const QString &pluginId);
+
+[[nodiscard]] TEXTADDONSWIDGETS_EXPORT PluginsStateList loadPluginSetting(const QString &configFileName,
+                                                                          const QString &groupName,
+                                                                          const QString &prefixSettingKey);
+TEXTADDONSWIDGETS_EXPORT void savePluginSettings(const QString &configFileName,
+                                                 const QString &groupName,
+                                                 const QString &prefixSettingKey,
+                                                 const QStringList &enabledPluginsList,
+                                                 const QStringList &disabledPluginsList);
+[[nodiscard]] TEXTADDONSWIDGETS_EXPORT TextAddonsWidgets::PluginUtilData createPluginMetaData(const KPluginMetaData &metaData);
+}
+}
