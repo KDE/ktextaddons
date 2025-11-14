c21f444e8a5ed25026d36b20f85fb5ad85215595
Laurent Montel <montel@kde.org>
2025-11-14 08:37:26 +0100

    Add test about TextAutoGenerateText::TextAutoGenerateAttachmentUtils::AttachmentElementInfo

--- textautogeneratetext/tools/example/exampletexttoolpluginjob.cpp
+++ textautogeneratetext/tools/example/exampletexttoolpluginjob.cpp
@@ -23,13 +23,24 @@ void ExampleTextToolPluginJob::start()
         return;
     }
     // TODO Need to implement job
+    QList<TextAutoGenerateText::TextAutoGenerateAttachmentUtils::AttachmentElementInfo> attachmentInfo;
+    {
+        const TextAutoGenerateText::TextAutoGenerateAttachmentUtils::AttachmentElementInfo attInfo{
+            .mimeType = "text-plain"_ba,
+            .content = "foo bla kde"_ba,
+            .name = u"test-file1"_s,
+            .attachmentType = TextAutoGenerateText::TextAutoGenerateAttachment::AttachmentType::File,
+        };
+        attachmentInfo.append(attInfo);
+    }
     const TextAutoGenerateText::TextAutoGenerateTextToolPlugin::TextToolPluginInfo info{
         .content = u"Temperature is 35°"_s,
         .messageUuid = mMessageUuid,
         .chatId = mChatId,
         .toolIdentifier = mToolIdentifier,
-        .attachementInfoList = {},
+        .attachementInfoList = attachmentInfo,
     };
+    qDebug() << " TextAutoGenerateText::TextAutoGenerateTextToolPlugin::TextToolPluginInfo " << info;
     Q_EMIT finished(info);
     deleteLater();
 }
