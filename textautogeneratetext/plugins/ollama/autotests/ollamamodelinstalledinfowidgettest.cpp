9d358d4a4279a8677b15a30bb3f405904ca45b9b
Laurent Montel <montel@kde.org>
2025-11-14 13:16:07 +0100

    Improve autotest

--- textautogeneratetext/plugins/ollama/autotests/ollamamodelinstalledinfowidgettest.cpp
+++ textautogeneratetext/plugins/ollama/autotests/ollamamodelinstalledinfowidgettest.cpp
@@ -5,9 +5,13 @@
 */
 #include "ollamamodelinstalledinfowidgettest.h"
 #include "modelsmanager/ollamamodelinstalledinfowidget.h"
+#include "modelsmanager/ollamanetworkurlbutton.h"
+#include <QLabel>
 #include <QTest>
-QTEST_MAIN(OllamaModelInstalledInfoWidgetTest)
+#include <QVBoxLayout>
 
+QTEST_MAIN(OllamaModelInstalledInfoWidgetTest)
+using namespace Qt::Literals::StringLiterals;
 OllamaModelInstalledInfoWidgetTest::OllamaModelInstalledInfoWidgetTest(QObject *parent)
     : QObject{parent}
 {
@@ -16,7 +20,20 @@ OllamaModelInstalledInfoWidgetTest::OllamaModelInstalledInfoWidgetTest(QObject *
 void OllamaModelInstalledInfoWidgetTest::shouldHaveDefaultValues()
 {
     OllamaModelInstalledInfoWidget w;
-    // TODO
+    auto mFamilyNameLabel = w.findChild<QLabel *>(u"mFamilyNameLabel"_s);
+    QVERIFY(mFamilyNameLabel);
+
+    auto mQuantizationLevelLabel = w.findChild<QLabel *>(u"mQuantizationLevelLabel"_s);
+    QVERIFY(mQuantizationLevelLabel);
+
+    auto mModifiedAtLabel = w.findChild<QLabel *>(u"mModifiedAtLabel"_s);
+    QVERIFY(mModifiedAtLabel);
+
+    auto mMainLayout = w.findChild<QVBoxLayout *>(u"mMainLayout"_s);
+    QVERIFY(mMainLayout);
+
+    auto mNetworkUrlButton = w.findChild<OllamaNetworkUrlButton *>(u"mNetworkUrlButton"_s);
+    QVERIFY(mNetworkUrlButton);
 }
 
 #include "moc_ollamamodelinstalledinfowidgettest.cpp"
