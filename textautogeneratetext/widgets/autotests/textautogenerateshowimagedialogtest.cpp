aacb2b28fcde25dc31b3a7ff7252b160611bbaef
Laurent Montel <montel@kde.org>
2025-12-08 08:41:54 +0100

    Add autotest

---
+++ textautogeneratetext/widgets/autotests/textautogenerateshowimagedialogtest.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "textautogenerateshowimagedialogtest.h"
+#include <QTest>
+QTEST_MAIN(TextAutoGenerateShowImageDialogTest)
+
+TextAutoGenerateShowImageDialogTest::TextAutoGenerateShowImageDialogTest(QObject *parent)
+    : QObject{parent}
+{
+}
---
+++ textautogeneratetext/widgets/autotests/textautogenerateshowimagedialogtest.h
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class TextAutoGenerateShowImageDialogTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateShowImageDialogTest(QObject *parent = nullptr);
+    ~TextAutoGenerateShowImageDialogTest() override = default;
+};
