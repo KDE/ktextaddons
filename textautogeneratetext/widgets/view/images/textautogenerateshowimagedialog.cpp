380823326e5a2925026c93030ea57035bd71e030
Laurent Montel <montel@kde.org>
2025-12-08 08:37:45 +0100

    Prepare to implement textautogenerateshowimagedialog

---
+++ textautogeneratetext/widgets/view/images/textautogenerateshowimagedialog.cpp
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "textautogenerateshowimagedialog.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateShowImageDialog::TextAutoGenerateShowImageDialog(QWidget *parent)
+    : QDialog(parent)
+{
+}
+
+TextAutoGenerateShowImageDialog::~TextAutoGenerateShowImageDialog() = default;
---
+++ textautogeneratetext/widgets/view/images/textautogenerateshowimagedialog.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetext_private_export.h"
+#include <QDialog>
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_TESTS_EXPORT TextAutoGenerateShowImageDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateShowImageDialog(QWidget *parent = nullptr);
+    ~TextAutoGenerateShowImageDialog() override;
+};
+}
