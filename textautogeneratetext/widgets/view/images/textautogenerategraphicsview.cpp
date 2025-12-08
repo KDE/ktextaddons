1eb0c54a156113cd6566caecd0622f67503a2f4e
Laurent Montel <montel@kde.org>
2025-12-08 13:40:53 +0100

    Implement zoom support

--- textautogeneratetext/widgets/view/images/textautogenerategraphicsview.cpp
+++ textautogeneratetext/widgets/view/images/textautogenerategraphicsview.cpp
@@ -4,6 +4,8 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "textautogenerategraphicsview.h"
+#include <QScopedValueRollback>
+#include <QWheelEvent>
 
 using namespace Qt::Literals::StringLiterals;
 using namespace TextAutoGenerateText;
@@ -35,5 +37,77 @@ TextAutoGenerateGraphicsView::TextAutoGenerateGraphicsView(QWidget *parent)
 }
 
 TextAutoGenerateGraphicsView::~TextAutoGenerateGraphicsView() = default;
+
+void TextAutoGenerateGraphicsView::wheelEvent(QWheelEvent *e)
+{
+    if (e->modifiers() == Qt::ControlModifier) {
+        const int y = e->angleDelta().y();
+        if (y < 0) {
+            zoomOut(e->position());
+        } else if (y > 0) {
+            zoomIn(e->position());
+        } // else: y == 0 => horizontal scroll => do not handle
+    } else {
+        QGraphicsView::wheelEvent(e);
+    }
+}
+
+void TextAutoGenerateGraphicsView::zoomIn(QPointF centerPos)
+{
+    setZoom(zoom() * 1.1, centerPos);
+}
+
+void TextAutoGenerateGraphicsView::zoomOut(QPointF centerPos)
+{
+    setZoom(zoom() * 0.9, centerPos);
+}
+
+qreal TextAutoGenerateGraphicsView::zoom() const
+{
+    return transform().m11();
+}
+
+qreal TextAutoGenerateGraphicsView::minimumZoom() const
+{
+    return mMinimumZoom;
+}
+
+qreal TextAutoGenerateGraphicsView::maximumZoom() const
+{
+    return mMaximumZoom;
+}
+
+void TextAutoGenerateGraphicsView::setZoom(qreal zoom, QPointF centerPos)
+{
+    // clamp value
+    zoom = qBound(minimumZoom(), zoom, maximumZoom());
+
+    if (qFuzzyCompare(this->zoom(), zoom)) {
+        return;
+    }
+
+    if (mIsUpdatingZoom) {
+        return;
+    }
+
+    QScopedValueRollback<bool> guard(mIsUpdatingZoom, true);
+
+    QPointF targetScenePos;
+    if (!centerPos.isNull()) {
+        targetScenePos = mapToScene(centerPos.toPoint());
+    } else {
+        targetScenePos = sceneRect().center();
+    }
+
+    ViewportAnchor oldAnchor = this->transformationAnchor();
+    setTransformationAnchor(QGraphicsView::NoAnchor);
+
+    QTransform matrix;
+    matrix.translate(targetScenePos.x(), targetScenePos.y()).scale(zoom, zoom).translate(-targetScenePos.x(), -targetScenePos.y());
+    setTransform(matrix);
+
+    setTransformationAnchor(oldAnchor);
+    Q_EMIT zoomChanged(zoom);
+}
 
 #include "moc_textautogenerategraphicsview.cpp"
--- textautogeneratetext/widgets/view/images/textautogenerategraphicsview.h
+++ textautogeneratetext/widgets/view/images/textautogenerategraphicsview.h
@@ -16,8 +16,26 @@ public:
     explicit TextAutoGenerateGraphicsView(QWidget *parent = nullptr);
     ~TextAutoGenerateGraphicsView() override;
 
+    [[nodiscard]] qreal zoom() const;
+    void setZoom(qreal zoom, QPointF centerPos = {});
+    void zoomIn(QPointF centerPos);
+    void zoomOut(QPointF centerPos);
+
+    [[nodiscard]] qreal minimumZoom() const;
+    [[nodiscard]] qreal maximumZoom() const;
+
+Q_SIGNALS:
+    void zoomChanged(qreal zoom);
+    void minimumZoomChanged(qreal zoom);
+    void maximumZoomChanged(qreal zoom);
+
+protected:
+    void wheelEvent(QWheelEvent *e) override;
+
 private:
+    QGraphicsProxyWidget *mGraphicsProxyWidget = nullptr;
     qreal mMinimumZoom;
     qreal mMaximumZoom;
+    bool mIsUpdatingZoom = false;
 };
 }
