fc0e4ca29b4168ece15452704115a36ab6662a78
Laurent Montel <montel@kde.org>
2025-12-22 20:04:27 +0100

    prepare to add file cache support

---
+++ textautogeneratetext/core/textautogeneratefilecache.h
@@ -0,0 +1,17 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "textautogeneratetext_export.h"
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateFileCache
+{
+public:
+    TextAutoGenerateFileCache();
+    ~TextAutoGenerateFileCache();
+};
+}
