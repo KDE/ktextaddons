05ef5ad7fb4a521e05acfa681f4df47877d77633
Laurent Montel <montel@kde.org>
2025-12-23 08:46:34 +0100

    Continue to implement textautogeneratetextopenfilejob

--- textautogeneratetext/widgets/common/textautogeneratetextopenfilejob.cpp
+++ textautogeneratetext/widgets/common/textautogeneratetextopenfilejob.cpp
@@ -35,21 +35,14 @@ void TextAutoGenerateTextOpenFileJob::downloadFile(const QUrl &fileUrl)
 
 void TextAutoGenerateTextOpenFileJob::runApplication(const KService::Ptr &offer)
 {
-    std::unique_ptr<QTemporaryDir> tempDir(new QTemporaryDir(QDir::tempPath() + "/textautogeneratetext_attachment_XXXXXX"_L1));
-    if (!tempDir->isValid()) {
-        qCWarning(TEXTAUTOGENERATETEXT_WIDGET_LOG) << "Impossible to create attachment temporary file";
-        deleteLater();
-        return;
+    if (mManager) {
+        const QString fileName = mManager->generateAttachmentTemporaryFile();
+        auto job = new KIO::ApplicationLauncherJob(offer); // asks the user if offer is nullptr
+        job->setUrls({QUrl::fromLocalFile(fileName)});
+        job->setRunFlags(KIO::ApplicationLauncherJob::DeleteTemporaryFiles);
+        job->setUiDelegate(KIO::createDefaultJobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, mParentWidget));
+        job->start();
     }
-    tempDir->setAutoRemove(false); // can't delete them, same problem as in messagelib ViewerPrivate::attachmentOpenWith
-
-#if 0
-    auto job = new KIO::ApplicationLauncherJob(offer); // asks the user if offer is nullptr
-    job->setUrls({localFileUrl});
-    job->setRunFlags(KIO::ApplicationLauncherJob::DeleteTemporaryFiles);
-    job->setUiDelegate(KIO::createDefaultJobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, mParentWidget));
-    job->start();
-#endif
     deleteLater();
 }
 
