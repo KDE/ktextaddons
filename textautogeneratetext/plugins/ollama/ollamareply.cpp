94846db51e7dc2b71a049d138dd3782583953436
Laurent Montel <montel@kde.org>
2026-01-06 07:27:28 +0100

    improve result

--- textautogeneratetext/plugins/ollama/ollamareply.cpp
+++ textautogeneratetext/plugins/ollama/ollamareply.cpp
@@ -153,14 +153,28 @@ QString OllamaReply::generateModelInfo() const
 {
     QString mardown;
     const QJsonDocument doc = mTokens.constFirst();
-    mardown += u"## %1: \n```\n"_s.arg(i18n("Template")) + doc["template"_L1].toString() + u"\n```\n"_s;
-    mardown += u"## %1: \n```\n"_s.arg(i18n("Modelfile")) + doc["modelfile"_L1].toString() + u"\n```\n"_s;
-    mardown += u"## %1: \n```\n"_s.arg(i18n("Parameters")) + doc["parameters"_L1].toString() + u"\n```\n"_s;
+    QString capabilities;
+    const QJsonArray array = doc["capabilities"_L1].toArray();
+    for (const auto &val : array) {
+        if (!capabilities.isEmpty()) {
+            capabilities += u", "_s;
+        }
+        capabilities += val.toString();
+    }
+    if (!capabilities.isEmpty()) {
+        mardown += u"## %1: \n```\n"_s.arg(i18n("Capabilities")) + capabilities + u"\n```\n\n\n"_s;
+    }
+    mardown += u"## %1: \n```\n"_s.arg(i18n("Template")) + doc["template"_L1].toString() + u"\n```\n\n\n"_s;
+    mardown += u"## %1: \n```\n"_s.arg(i18n("Modelfile")) + doc["modelfile"_L1].toString() + u"\n```\n\n\n"_s;
+    const QString parameters = doc["parameters"_L1].toString();
+    if (!parameters.isEmpty()) {
+        mardown += u"## %1: \n```\n"_s.arg(i18n("Parameters")) + doc["parameters"_L1].toString() + u"\n```\n\n\n"_s;
+    }
     mardown +=
-        u"## %1: \n```\n"_s.arg(i18n("Details")) + QString::fromLatin1(QJsonDocument::fromVariant(doc["details"_L1].toVariant()).toJson()) + u"\n```\n"_s;
-    mardown +=
-        u"## %1: \n```\n"_s.arg(i18n("Model Info")) + QString::fromLatin1(QJsonDocument::fromVariant(doc["model_info"_L1].toVariant()).toJson()) + u"\n```\n"_s;
-    // qDebug() << " mTokens " << mTokens;
+        u"## %1: \n```\n"_s.arg(i18n("Details")) + QString::fromLatin1(QJsonDocument::fromVariant(doc["details"_L1].toVariant()).toJson()) + u"\n```\n\n\n"_s;
+    mardown += u"## %1: \n```\n"_s.arg(i18n("Model Info")) + QString::fromLatin1(QJsonDocument::fromVariant(doc["model_info"_L1].toVariant()).toJson())
+        + u"\n```\n\n\n"_s;
+    qDebug() << " mTokens " << doc;
     return mardown;
 }
 
