855e98cebcd686f4fccc2a46eb72f64b86815b9e
Laurent Montel <montel@kde.org>
2026-02-22 15:46:37 +0100

    Add i18n

--- textautogeneratetext/widgets/common/textautogeneratepowersavermessagewidget.cpp
+++ textautogeneratetext/widgets/common/textautogeneratepowersavermessagewidget.cpp
@@ -4,12 +4,17 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratepowersavermessagewidget.h"
+#include <KLocalizedString>
 
 using namespace TextAutoGenerateText;
 TextAutoGeneratePowerSaverMessageWidget::TextAutoGeneratePowerSaverMessageWidget(QWidget *parent)
     : KMessageWidget(parent)
 {
+    setMessageType(KMessageWidget::MessageType::Warning);
+    setPosition(KMessageWidget::Header);
+    setText(i18n("Power saver mode is enabled, this will slow down message generation."));
 }
 
 TextAutoGeneratePowerSaverMessageWidget::~TextAutoGeneratePowerSaverMessageWidget() = default;
+
 #include "moc_textautogeneratepowersavermessagewidget.cpp"
