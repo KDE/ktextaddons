a9064938f60518a47d5e8ae8e3d7478acc1c16c1
Laurent Montel <montel@kde.org>
2026-03-07 15:30:19 +0100

    Remove comment include

--- textautogeneratetext/widgets/availablemodel/textautogeneratemodelavailablelistview.cpp
+++ textautogeneratetext/widgets/availablemodel/textautogeneratemodelavailablelistview.cpp
@@ -4,7 +4,7 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratemodelavailablelistview.h"
-// #include "genericnetworkmodelavailableinfosdelegate.h"
+
 using namespace TextAutoGenerateText;
 TextAutoGenerateModelAvailableListView::TextAutoGenerateModelAvailableListView(QWidget *parent)
     : QListView(parent)
