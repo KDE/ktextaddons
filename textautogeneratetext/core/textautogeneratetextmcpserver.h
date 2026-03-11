bb349ac6c0b1562857c82ec9b296cb969bd640a3
Laurent Montel <montel@kde.org>
2026-03-11 08:33:28 +0100

    Look at if we can implement mcp support

---
+++ textautogeneratetext/core/textautogeneratetextmcpserver.h
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "textautogeneratetext_private_export.h"
+#include <QObject>
+
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_TESTS_EXPORT TextAutoGenerateTextMcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextAutoGenerateTextMcpServer(QObject *parent = nullptr);
+    ~TextAutoGenerateTextMcpServer() override;
+};
+}
