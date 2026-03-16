fd4b437bc2141051bc0f7b5fe36857ef6ea8611c
Laurent Montel <montel@kde.org>
2026-03-16 06:59:42 +0100

    Prepare autotest

--- textautogeneratetext/mcpprotocol/mcpprotocolimagecontent.cpp
+++ textautogeneratetext/mcpprotocol/mcpprotocolimagecontent.cpp
@@ -5,7 +5,7 @@
 */
 
 #include "mcpprotocolimagecontent.h"
-
+#include <QDebug>
 using namespace McpProtocol;
 McpProtocolImageContent::McpProtocolImageContent() = default;
 
@@ -24,4 +24,23 @@ QString McpProtocolImageContent::mimeType() const
 void McpProtocolImageContent::setMimeType(const QString &newMimeType)
 {
     mMimeType = newMimeType;
+}
+
+QString McpProtocolImageContent::data() const
+{
+    return mData;
+}
+
+void McpProtocolImageContent::setData(const QString &newData)
+{
+    mData = newData;
+}
+
+bool McpProtocolImageContent::operator==(const McpProtocolImageContent &other) const = default;
+
+QDebug operator<<(QDebug d, const McpProtocol::McpProtocolImageContent &t)
+{
+    d.space() << "data:" << t.data();
+    d.space() << "mimeType:" << t.mimeType();
+    return d;
 }
--- textautogeneratetext/mcpprotocol/mcpprotocolimagecontent.h
+++ textautogeneratetext/mcpprotocol/mcpprotocolimagecontent.h
@@ -4,22 +4,50 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #pragma once
+#include "textautogeneratetextmcpprotocol_export.h"
 #include <QByteArray>
 #include <QString>
+class QDebug;
 namespace McpProtocol
 {
-class McpProtocolImageContent
+class TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT McpProtocolImageContent
 {
 public:
+    /*!
+     */
     McpProtocolImageContent();
+    /*!
+     */
     ~McpProtocolImageContent();
 
+    /*!
+     */
     [[nodiscard]] static QByteArray type();
 
+    /*!
+     */
     [[nodiscard]] QString mimeType() const;
+    /*!
+     * \brief setMimeType
+     * \param newMimeType
+     */
     void setMimeType(const QString &newMimeType);
+
+    /*!
+     */
+    [[nodiscard]] QString data() const;
+    /*!
+     */
+    void setData(const QString &newData);
+
+    /*!
+     */
+    [[nodiscard]] bool operator==(const McpProtocolImageContent &other) const;
 
 private:
     QString mMimeType;
+    QString mData;
 };
 }
+Q_DECLARE_TYPEINFO(McpProtocol::McpProtocolImageContent, Q_RELOCATABLE_TYPE);
+TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT QDebug operator<<(QDebug d, const McpProtocol::McpProtocolImageContent &t);
