f87a3c64f22d719bcb010e0bf8fb023a9a0c1d81
Laurent Montel <montel@kde.org>
2026-03-16 07:33:51 +0100

    Add mcpprotocolicon

---
+++ textautogeneratetext/mcpprotocol/mcpprotocolicon.cpp
@@ -0,0 +1,60 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "mcpprotocolicon.h"
+#include <QDebug>
+using namespace McpProtocol;
+McpProtocolIcon::McpProtocolIcon() = default;
+
+McpProtocolIcon::~McpProtocolIcon() = default;
+
+bool McpProtocolIcon::operator==(const McpProtocolIcon &other) const = default;
+
+std::optional<QString> McpProtocolIcon::mimeType() const
+{
+    return mMimeType;
+}
+
+void McpProtocolIcon::setMimeType(std::optional<QString> newMimeType)
+{
+    mMimeType = std::move(newMimeType);
+}
+
+std::optional<QStringList> McpProtocolIcon::sizes() const
+{
+    return mSizes;
+}
+
+void McpProtocolIcon::setSizes(std::optional<QStringList> newSizes)
+{
+    mSizes = std::move(newSizes);
+}
+
+std::optional<QString> McpProtocolIcon::theme() const
+{
+    return mTheme;
+}
+
+void McpProtocolIcon::setTheme(std::optional<QString> newTheme)
+{
+    mTheme = std::move(newTheme);
+}
+
+QString McpProtocolIcon::src() const
+{
+    return mSrc;
+}
+
+void McpProtocolIcon::setSrc(const QString &newSrc)
+{
+    mSrc = newSrc;
+}
+
+QDebug operator<<(QDebug d, const McpProtocol::McpProtocolIcon &t)
+{
+    d.space() << "src:" << t.src();
+    return d;
+}
---
+++ textautogeneratetext/mcpprotocol/mcpprotocolicon.h
@@ -0,0 +1,47 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "textautogeneratetextmcpprotocol_export.h"
+#include <QString>
+#include <QStringList>
+class QDebug;
+namespace McpProtocol
+{
+class TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT McpProtocolIcon
+{
+public:
+    /*!
+     */
+    McpProtocolIcon();
+    /*!
+     */
+    ~McpProtocolIcon();
+
+    /*!
+     */
+    [[nodiscard]] bool operator==(const McpProtocolIcon &other) const;
+
+    [[nodiscard]] std::optional<QString> mimeType() const;
+    void setMimeType(std::optional<QString> newMimeType);
+
+    [[nodiscard]] std::optional<QStringList> sizes() const;
+    void setSizes(std::optional<QStringList> newSizes);
+
+    [[nodiscard]] std::optional<QString> theme() const;
+    void setTheme(std::optional<QString> newTheme);
+
+    [[nodiscard]] QString src() const;
+    void setSrc(const QString &newSrc);
+
+private:
+    QString mSrc;
+    std::optional<QString> mMimeType;
+    std::optional<QStringList> mSizes; // e.g. "48x48", "96x96", "any"
+    std::optional<QString> mTheme; // "light" or "dark"
+};
+}
+Q_DECLARE_TYPEINFO(McpProtocol::McpProtocolIcon, Q_RELOCATABLE_TYPE);
+TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT QDebug operator<<(QDebug d, const McpProtocol::McpProtocolIcon &t);
