b44cdc076ab5a197ea8804297b28b1d1fae93524
Laurent Montel <montel@kde.org>
2026-03-17 08:25:56 +0100

    Prepare McpProtocolToolChoice

---
+++ textautogeneratetext/mcpprotocol/mcpprotocoltoolchoice.cpp
@@ -0,0 +1,41 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "mcpprotocoltoolchoice.h"
+#include <QDebug>
+using namespace Qt::Literals::StringLiterals;
+using namespace McpProtocol;
+McpProtocolToolChoice::McpProtocolToolChoice() = default;
+
+McpProtocolToolChoice::~McpProtocolToolChoice() = default;
+
+bool McpProtocolToolChoice::operator==(const McpProtocolToolChoice &other) const = default;
+
+QDebug operator<<(QDebug d, const McpProtocol::McpProtocolToolChoice &t)
+{
+    d.space() << "mode:" << t.mode();
+    return d;
+}
+
+McpProtocolToolChoice McpProtocolToolChoice::fromJson(const QJsonObject &obj)
+{
+    return {};
+}
+
+QJsonObject McpProtocolToolChoice::toJson(const McpProtocolToolChoice &text)
+{
+    return {};
+}
+
+McpProtocolToolChoice::Mode McpProtocolToolChoice::mode() const
+{
+    return mMode;
+}
+
+void McpProtocolToolChoice::setMode(Mode newMode)
+{
+    mMode = newMode;
+}
---
+++ textautogeneratetext/mcpprotocol/mcpprotocoltoolchoice.h
@@ -0,0 +1,56 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include "mcpprotocolannotations.h"
+#include "textautogeneratetextmcpprotocol_export.h"
+#include <QByteArray>
+#include <QJsonObject>
+#include <QString>
+class QDebug;
+namespace McpProtocol
+{
+class TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT McpProtocolToolChoice
+{
+    Q_GADGET
+public:
+    enum class Mode : uint8_t {
+        Auto = 0,
+        None,
+        Required
+    };
+    Q_ENUM(Mode)
+
+    /*!
+     */
+    McpProtocolToolChoice();
+    /*!
+     */
+    ~McpProtocolToolChoice();
+
+    /*!
+     */
+    [[nodiscard]] bool operator==(const McpProtocolToolChoice &other) const;
+
+    /*!
+     */
+    static McpProtocolToolChoice fromJson(const QJsonObject &obj);
+    /*!
+     */
+    static QJsonObject toJson(const McpProtocolToolChoice &image);
+
+    /*!
+     */
+    [[nodiscard]] Mode mode() const;
+    /*!
+     */
+    void setMode(Mode newMode);
+
+private:
+    Mode mMode = Mode::Auto;
+};
+}
+Q_DECLARE_TYPEINFO(McpProtocol::McpProtocolToolChoice, Q_RELOCATABLE_TYPE);
+TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT QDebug operator<<(QDebug d, const McpProtocol::McpProtocolToolChoice &t);
