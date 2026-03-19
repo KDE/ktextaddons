c53cd28be07ba4b622760f5ca003ddb8ae42ba05
Laurent Montel <montel@kde.org>
2026-03-19 08:53:54 +0100

    Add autotest

---
+++ textautogeneratetext/mcpprotocol/autotests/mcpprotocolnumberschematest.cpp
@@ -0,0 +1,26 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "mcpprotocolnumberschematest.h"
+#include "mcpprotocolnumberschema.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(McpProtocolNumberSchemaTest)
+
+McpProtocolNumberSchemaTest::McpProtocolNumberSchemaTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void McpProtocolNumberSchemaTest::shouldHaveDefaultValues()
+{
+    const McpProtocol::McpProtocolNumberSchema w;
+    QVERIFY(!w.description().has_value());
+    QVERIFY(!w.title().has_value());
+    QVERIFY(!w.defaultValue().has_value());
+    QVERIFY(!w.maximum().has_value());
+    QVERIFY(!w.minimum().has_value());
+    QCOMPARE(w.type(), McpProtocol::McpProtocolNumberSchema::Type::Unknown);
+}
+#include "moc_mcpprotocolnumberschematest.cpp"
