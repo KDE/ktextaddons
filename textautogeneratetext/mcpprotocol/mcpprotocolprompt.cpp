98bdedb3b89d510ae89d6223b8a35a95a9cc4daa
Laurent Montel <montel@kde.org>
2026-03-20 23:20:28 +0100

    Continue to implement mcpprotocolprompt

--- textautogeneratetext/mcpprotocol/mcpprotocolprompt.cpp
+++ textautogeneratetext/mcpprotocol/mcpprotocolprompt.cpp
@@ -7,6 +7,7 @@
 #include "mcpprotocolprompt.h"
 #include "textautogeneratetextmcpprotocol_debug.h"
 #include <QDebug>
+#include <QJsonArray>
 using namespace Qt::Literals::StringLiterals;
 using namespace McpProtocol;
 McpProtocolPrompt::McpProtocolPrompt() = default;
@@ -17,6 +18,12 @@ bool McpProtocolPrompt::operator==(const McpProtocolPrompt &other) const = defau
 
 QDebug operator<<(QDebug d, const McpProtocol::McpProtocolPrompt &t)
 {
+    d.space() << "meta:" << t.meta();
+    d.space() << "arguments:" << t.arguments();
+    d.space() << "description:" << t.description();
+    d.space() << "icons:" << t.icons();
+    d.space() << "name:" << t.name();
+    d.space() << "title:" << t.title();
     return d;
 }
 
@@ -29,6 +36,33 @@ McpProtocolPrompt McpProtocolPrompt::fromJson(const QJsonObject &obj)
 QJsonObject McpProtocolPrompt::toJson(const McpProtocolPrompt &boolean)
 {
     QJsonObject obj;
+    obj["name"_L1] = boolean.name();
+    if (boolean.meta().has_value()) {
+        obj["_meta"_L1] = McpProtocolMeta::toJson(*boolean.meta());
+    }
+    if (boolean.arguments().has_value()) {
+        const auto arguments = *boolean.arguments();
+        QJsonArray arr_arguments;
+        for (const auto &v : arguments) {
+            arr_arguments.append(McpProtocolPromptArgument::toJson(v));
+        }
+        obj["arguments"_L1] = arr_arguments;
+    }
+    if (boolean.description().has_value()) {
+        obj["description"_L1] = *boolean.description();
+    }
+    if (boolean.icons().has_value()) {
+        QJsonArray arr_icons;
+        const auto icons = *boolean.icons();
+        for (const auto &v : icons) {
+            arr_icons.append(McpProtocolIcon::toJson(v));
+        }
+        obj["icons"_L1] = arr_icons;
+    }
+    if (boolean.title().has_value()) {
+        obj["title"_L1] = *boolean.title();
+    }
+
     return obj;
 }
 
