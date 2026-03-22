aa66d751aa3ec894ea0cb2db3556bb86b23e1639
Laurent Montel <montel@kde.org>
2026-03-22 11:36:43 +0100

    Add mcpprotocolresourcelistchangednotification

---
+++ textautogeneratetext/mcpprotocol/autotests/mcpprotocolresourcelistchangednotificationtest.cpp
@@ -0,0 +1,22 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "mcpprotocolresourcelistchangednotificationtest.h"
+#include "mcpprotocolresourcelistchangednotification.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(McpProtocolResourceListChangedNotificationTest)
+using namespace Qt::Literals::StringLiterals;
+McpProtocolResourceListChangedNotificationTest::McpProtocolResourceListChangedNotificationTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void McpProtocolResourceListChangedNotificationTest::shouldHaveDefaultValues()
+{
+    const McpProtocol::McpProtocolResourceListChangedNotification w;
+    QVERIFY(!w.params().has_value());
+    QCOMPARE(w.type(), "notifications/resources/list_changed"_ba);
+}
+#include "moc_mcpprotocolresourcelistchangednotificationtest.cpp"
---
+++ textautogeneratetext/mcpprotocol/mcpprotocolresourcelistchangednotification.cpp
@@ -0,0 +1,67 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "mcpprotocolresourcelistchangednotification.h"
+#include "textautogeneratetextmcpprotocol_debug.h"
+#include <QDebug>
+#include <QJsonArray>
+using namespace Qt::Literals::StringLiterals;
+using namespace McpProtocol;
+McpProtocolResourceListChangedNotification::McpProtocolResourceListChangedNotification() = default;
+
+McpProtocolResourceListChangedNotification::~McpProtocolResourceListChangedNotification() = default;
+
+QByteArray McpProtocolResourceListChangedNotification::type()
+{
+    return "notifications/resources/list_changed"_ba;
+}
+
+bool McpProtocolResourceListChangedNotification::operator==(const McpProtocolResourceListChangedNotification &other) const = default;
+
+QDebug operator<<(QDebug d, const McpProtocol::McpProtocolResourceListChangedNotification &t)
+{
+    d.space() << "params:" << t.params();
+    return d;
+}
+
+McpProtocolResourceListChangedNotification McpProtocolResourceListChangedNotification::fromJson(const QJsonObject &obj)
+{
+    McpProtocolResourceListChangedNotification prompt;
+
+    if (obj.value("jsonrpc"_L1).toString() != "2.0"_L1) {
+        qCWarning(TEXTAUTOGENERATEMCPPROTOCOL_LOG) << "Field 'jsonrpc' must be '2.0', got: " << obj.value("jsonrpc"_L1).toString();
+        return {};
+    }
+    if (obj.value("method"_L1).toString() != QString::fromLatin1(McpProtocolResourceListChangedNotification::type())) {
+        qCWarning(TEXTAUTOGENERATEMCPPROTOCOL_LOG) << "Field 'method' must be 'notifications/tools/list_changed', got: " << obj.value("method"_L1).toString();
+        return {};
+    }
+    if (obj.contains("params"_L1) && obj["params"_L1].isObject()) {
+        prompt.setParams(McpProtocolNotificationParams::fromJson(obj["params"_L1].toObject()));
+    }
+    return prompt;
+}
+
+QJsonObject McpProtocolResourceListChangedNotification::toJson(const McpProtocolResourceListChangedNotification &boolean)
+{
+    QJsonObject obj;
+    obj["jsonrpc"_L1] = u"2.0"_s;
+    obj["method"_L1] = QString::fromLatin1(McpProtocolResourceListChangedNotification::type());
+    if (boolean.params().has_value()) {
+        obj["params"_L1] = McpProtocolNotificationParams::toJson(*boolean.params());
+    }
+    return obj;
+}
+
+std::optional<McpProtocolNotificationParams> McpProtocolResourceListChangedNotification::params() const
+{
+    return mParams;
+}
+
+void McpProtocolResourceListChangedNotification::setParams(std::optional<McpProtocolNotificationParams> newParams)
+{
+    mParams = std::move(newParams);
+}
