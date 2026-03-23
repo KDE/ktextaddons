b9d9ec8fc661069939dde256a823e5aacf508a66
Laurent Montel <montel@kde.org>
2026-03-23 08:50:21 +0100

    Add textautogeneratetexttoolinternal

---
+++ textautogeneratetext/core/tools/textautogeneratetexttoolinternal.cpp
@@ -0,0 +1,12 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "textautogeneratetexttoolinternal.h"
+
+using namespace TextAutoGenerateText;
+TextAutoGenerateTextToolInternal::TextAutoGenerateTextToolInternal() = default;
+
+TextAutoGenerateTextToolInternal::~TextAutoGenerateTextToolInternal() = default;
---
+++ textautogeneratetext/core/tools/textautogeneratetexttoolinternal.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "textautogeneratetext_export.h"
+namespace TextAutoGenerateText
+{
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateTextToolInternal
+{
+public:
+    TextAutoGenerateTextToolInternal();
+    virtual ~TextAutoGenerateTextToolInternal();
+
+    virtual void execute() = 0;
+};
+}
