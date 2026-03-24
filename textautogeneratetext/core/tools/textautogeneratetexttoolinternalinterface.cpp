a379709b1050b53ccedee4d922a6577bd7003844
Laurent Montel <montel@kde.org>
2026-03-24 08:04:50 +0100

    Improve textautogeneratetexttoolinternalinterface

--- textautogeneratetext/core/tools/textautogeneratetexttoolinternalinterface.cpp
+++ textautogeneratetext/core/tools/textautogeneratetexttoolinternalinterface.cpp
@@ -5,8 +5,34 @@
 */
 
 #include "textautogeneratetexttoolinternalinterface.h"
-
+#include "core/tools/textautogeneratetexttoolsloadingjob.h"
 using namespace TextAutoGenerateText;
-TextAutoGenerateTextToolInternalInterface::TextAutoGenerateTextToolInternalInterface() = default;
+TextAutoGenerateTextToolInternalInterface::TextAutoGenerateTextToolInternalInterface(QObject *parent)
+    : QObject(parent)
+{
+}
 
 TextAutoGenerateTextToolInternalInterface::~TextAutoGenerateTextToolInternalInterface() = default;
+
+void TextAutoGenerateTextToolInternalInterface::loadInternalTools(const QString &filename)
+{
+    auto job = new TextAutoGenerateTextToolsLoadingJob(this);
+    job->setFileName(filename);
+    connect(job, &TextAutoGenerateTextToolsLoadingJob::tools, this, &TextAutoGenerateTextToolInternalInterface::slotToolsLoaded);
+    job->start();
+}
+
+void TextAutoGenerateTextToolInternalInterface::slotToolsLoaded(const QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> &tools)
+{
+    mTools = tools;
+}
+
+QList<TextAutoGenerateTextToolInternal> TextAutoGenerateTextToolInternalInterface::tools() const
+{
+    return mTools;
+}
+
+void TextAutoGenerateTextToolInternalInterface::setTools(const QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> &newTools)
+{
+    mTools = newTools;
+}
--- textautogeneratetext/core/tools/textautogeneratetexttoolinternalinterface.h
+++ textautogeneratetext/core/tools/textautogeneratetexttoolinternalinterface.h
@@ -6,14 +6,41 @@
 
 #pragma once
 #include "textautogeneratetext_export.h"
+#include <QList>
+#include <QObject>
+#include <TextAutoGenerateText/TextAutoGenerateTextToolInternal>
 namespace TextAutoGenerateText
 {
-class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateTextToolInternalInterface
+class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateTextToolInternalInterface : public QObject
 {
+    Q_OBJECT
 public:
-    TextAutoGenerateTextToolInternalInterface();
+    /*!
+     */
+    explicit TextAutoGenerateTextToolInternalInterface(QObject *parent = nullptr);
+    /*!
+     */
     virtual ~TextAutoGenerateTextToolInternalInterface();
 
+    /*!
+     */
     virtual void execute() = 0;
+
+    /*!
+     */
+    void loadInternalTools(const QString &filename);
+
+    /*!
+     */
+    [[nodiscard]] QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> tools() const;
+    /*!
+     */
+    void setTools(const QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> &newTools);
+
+protected:
+    QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> mTools;
+
+private:
+    TEXTAUTOGENERATETEXT_NO_EXPORT void slotToolsLoaded(const QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> &list);
 };
 }
