42515e6599b046ad41987abea047c80465f9b310
Laurent Montel <montel@kde.org>
2026-03-24 07:56:17 +0100

    Add tools2.json test

--- textautogeneratetext/core/autotests/textautogeneratetexttoolsloadingjobtest.cpp
+++ textautogeneratetext/core/autotests/textautogeneratetexttoolsloadingjobtest.cpp
@@ -22,33 +22,81 @@ void TextAutoGenerateTextToolsLoadingJobTest::shouldLoadTools_data()
     QTest::addColumn<QString>("name");
     QTest::addColumn<const QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal>>("tools");
 
-    QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> tools;
     {
-        TextAutoGenerateText::TextAutoGenerateTextToolInternal tool;
-        tool.setToolNameId("calendar_plugin"_ba);
-        tool.setDescription(u"bla bla"_s);
-        QStringList lst;
-        lst << QStringLiteral("calendarinfo") << QStringLiteral("username");
-        tool.setRequired(lst);
-        QList<TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty> properties;
+        QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> tools;
         {
-            TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
-            property.setDescription(u"calendar info"_s);
-            property.setName(u"calendarinfo"_s);
-            property.setTypeElements({u"email"_s});
-            properties.append(property);
+            TextAutoGenerateText::TextAutoGenerateTextToolInternal tool;
+            tool.setToolNameId("calendar_plugin"_ba);
+            tool.setDescription(u"bla bla"_s);
+            QStringList lst;
+            lst << QStringLiteral("calendarinfo") << QStringLiteral("username");
+            tool.setRequired(lst);
+            QList<TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty> properties;
+            {
+                TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
+                property.setDescription(u"calendar info"_s);
+                property.setName(u"calendarinfo"_s);
+                property.setTypeElements({u"email"_s});
+                properties.append(property);
+            }
+            {
+                TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
+                property.setDescription(u"Name of user"_s);
+                property.setName(u"username"_s);
+                properties.append(property);
+            }
+            tool.setProperties(properties);
+            tools.append(tool);
         }
-        {
-            TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
-            property.setDescription(u"Name of user"_s);
-            property.setName(u"username"_s);
-            properties.append(property);
-        }
-        tool.setProperties(properties);
-        tools.append(tool);
-    }
 
-    QTest::addRow("tools1") << u"tools1"_s << tools;
+        QTest::addRow("tools1") << u"tools1"_s << tools;
+    }
+    {
+        QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal> tools;
+        {
+            TextAutoGenerateText::TextAutoGenerateTextToolInternal tool;
+            tool.setToolNameId("calendar_plugin"_ba);
+            tool.setDescription(u"bla bla"_s);
+            QStringList lst;
+            lst << QStringLiteral("calendarinfo") << QStringLiteral("username");
+            tool.setRequired(lst);
+            QList<TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty> properties;
+            {
+                TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
+                property.setDescription(u"calendar info"_s);
+                property.setName(u"calendarinfo"_s);
+                property.setTypeElements({u"email"_s});
+                properties.append(property);
+            }
+            {
+                TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
+                property.setDescription(u"Name of user"_s);
+                property.setName(u"username"_s);
+                properties.append(property);
+            }
+            tool.setProperties(properties);
+            tools.append(tool);
+        }
+        {
+            TextAutoGenerateText::TextAutoGenerateTextToolInternal tool;
+            tool.setToolNameId("example_tool"_ba);
+            tool.setDescription(u"example"_s);
+            QStringList lst;
+            lst << QStringLiteral("city");
+            tool.setRequired(lst);
+            QList<TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty> properties;
+            {
+                TextAutoGenerateText::TextAutoGenerateTextToolInternalProperty property;
+                property.setDescription(u"The name of the city"_s);
+                property.setName(u"city"_s);
+                properties.append(property);
+            }
+            tool.setProperties(properties);
+            tools.append(tool);
+        }
+
+        QTest::addRow("tools2") << u"tools2"_s << tools;
+    }
 }
 
 void TextAutoGenerateTextToolsLoadingJobTest::shouldLoadTools()
@@ -64,3 +112,5 @@ void TextAutoGenerateTextToolsLoadingJobTest::shouldLoadTools()
     QCOMPARE(spy.count(), 1);
     QCOMPARE(spy.at(0).at(0).value<QList<TextAutoGenerateText::TextAutoGenerateTextToolInternal>>(), tools);
 }
+
+#include "moc_textautogeneratetexttoolsloadingjobtest.cpp"
