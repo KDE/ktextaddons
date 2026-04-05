ead006a8abf671056bb019a677097bdf20475ff7
Laurent Montel <montel@kde.org>
2026-04-05 10:55:01 +0200

    Add layout

--- textautogeneratetext/mcpprotocol/tests/client/mainwidget.cpp
+++ textautogeneratetext/mcpprotocol/tests/client/mainwidget.cpp
@@ -4,10 +4,19 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "mainwidget.h"
+#include <QSplitter>
+#include <QVBoxLayout>
 
 MainWidget::MainWidget(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QVBoxLayout(this);
+    auto splitter = new QSplitter(this);
+    mainLayout->addWidget(splitter);
+    splitter->setChildrenCollapsible(false);
+    // TODO
 }
 
 MainWidget::~MainWidget() = default;
+
+#include "moc_mainwidget.cpp"
