61b608a0250c2b5d230c44810666c11584718042
Laurent Montel <montel@kde.org>
2026-04-05 10:27:51 +0200

    Add test apps

---
+++ textautogeneratetext/mcpprotocol/tests/client/main.cpp
@@ -0,0 +1,26 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
+
+int main(int argc, char **argv)
+{
+    const QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.process(app);
+
+    // auto w = new TextAutoGenerateAskJobWidget;
+    // w->resize(600, 400);
+    // w->show();
+    app.exec();
+    // delete w;
+    return 0;
+}
