af6b980ca51964536a2e993fb84e19767704b7f8
Laurent Montel <montel@kde.org>
2026-04-15 05:17:32 +0200

    Add mcpbase class

---
+++ textautogeneratetext/mcpprotocol/common/mcpbase.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include "textautogeneratetextmcpprotocol_export.h"
+#include <QObject>
+namespace McpProtocol
+{
+class TEXTAUTOGENERATETEXTMCPPROTOCOL_EXPORT McpBase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit McpBase(QObject *parent = nullptr);
+    ~McpBase() override;
+};
+}
