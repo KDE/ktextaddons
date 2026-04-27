afaea86291029f493af16567f9e2455ba9319ac8
Laurent Montel <montel@kde.org>
2026-04-27 03:30:38 +0200

    Prepare widgets

---
+++ textautogeneratetext/mcpprotocol/widgets/addmcpprotocolwidget.cpp
@@ -0,0 +1,11 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "addmcpprotocolwidget.h"
+
+AddMcpProtocolWidget::AddMcpProtocolWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
---
+++ textautogeneratetext/mcpprotocol/widgets/addmcpprotocolwidget.h
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QWidget>
+
+class AddMcpProtocolWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AddMcpProtocolWidget(QWidget *parent = nullptr);
+};
