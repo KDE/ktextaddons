0d8f50f87c3b376ed783922f973108cf117ae526
Laurent Montel <montel@kde.org>
2026-04-30 17:19:08 +0200

    Add export symbole

--- textautogeneratetext/mcpprotocol/widgets/addmcpprotocolclientwidget.cpp
+++ textautogeneratetext/mcpprotocol/widgets/addmcpprotocolclientwidget.cpp
@@ -4,10 +4,13 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "addmcpprotocolclientwidget.h"
+#include <QVBoxLayout>
 using namespace TextAutoGenerateTextMcpProtocolWidgets;
 AddMcpProtocolClientWidget::AddMcpProtocolClientWidget(QWidget *parent)
     : QWidget{parent}
 {
 }
+
+AddMcpProtocolClientWidget::~AddMcpProtocolClientWidget() = default;
 
 #include "moc_addmcpprotocolclientwidget.cpp"
--- textautogeneratetext/mcpprotocol/widgets/addmcpprotocolclientwidget.h
+++ textautogeneratetext/mcpprotocol/widgets/addmcpprotocolclientwidget.h
@@ -4,14 +4,15 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #pragma once
-
+#include "textautogeneratetextmcpprotocolwidgets_export.h"
 #include <QWidget>
 namespace TextAutoGenerateTextMcpProtocolWidgets
 {
-class AddMcpProtocolClientWidget : public QWidget
+class TEXTAUTOGENERATETEXTMCPPROTOCOLWIDGETS_EXPORT AddMcpProtocolClientWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit AddMcpProtocolClientWidget(QWidget *parent = nullptr);
+    ~AddMcpProtocolClientWidget() override;
 };
 }
