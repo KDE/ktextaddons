4c0eb96dbfa6ca35c3f56ab163d80a72344ed837
Laurent Montel <montel@kde.org>
2026-05-02 17:47:04 +0200

    Add combobox

---
+++ textautogeneratetextmcpprotocol/widgets/common/selecttypecombobox.cpp
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "selecttypecombobox.h"
+
+using namespace TextAutoGenerateTextMcpProtocolWidgets;
+
+SelectTypeComboBox::SelectTypeComboBox(QWidget *parent)
+    : QComboBox(parent)
+{
+}
+
+SelectTypeComboBox::~SelectTypeComboBox() = default;
