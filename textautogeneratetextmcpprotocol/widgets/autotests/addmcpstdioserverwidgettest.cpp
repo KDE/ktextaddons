00ed679da8b7499f5fd2e66039c8c0123d4ed295
Laurent Montel <montel@kde.org>
2026-05-06 08:45:07 +0200

    Add autotest

--- textautogeneratetextmcpprotocol/widgets/autotests/addmcpstdioserverwidgettest.cpp
+++ textautogeneratetextmcpprotocol/widgets/autotests/addmcpstdioserverwidgettest.cpp
@@ -5,9 +5,11 @@
 */
 #include "addmcpstdioserverwidgettest.h"
 #include "server/addmcpstdioserverwidget.h"
+#include <QFormLayout>
+#include <QLineEdit>
 #include <QTest>
 QTEST_MAIN(AddMcpStdioServerWidgetTest)
-
+using namespace Qt::Literals::StringLiterals;
 AddMcpStdioServerWidgetTest::AddMcpStdioServerWidgetTest(QObject *parent)
     : QObject{parent}
 {
@@ -16,6 +18,17 @@ AddMcpStdioServerWidgetTest::AddMcpStdioServerWidgetTest(QObject *parent)
 void AddMcpStdioServerWidgetTest::shouldHaveDefaultValues()
 {
     const TextAutoGenerateTextMcpProtocolWidgets::AddMcpStdioServerWidget w;
-    // TODO
+    auto mainLayout = w.findChild<QFormLayout *>(u"mainLayout"_s);
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->spacing(), 0);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mCommandLineEdit = w.findChild<QLineEdit *>(u"mCommandLineEdit"_s);
+    QVERIFY(mCommandLineEdit);
+    QVERIFY(mCommandLineEdit->text().isEmpty());
+
+    auto mArgumentsLineEdit = w.findChild<QLineEdit *>(u"mArgumentsLineEdit"_s);
+    QVERIFY(mArgumentsLineEdit);
+    QVERIFY(mArgumentsLineEdit->text().isEmpty());
 }
 #include "moc_addmcpstdioserverwidgettest.cpp"
