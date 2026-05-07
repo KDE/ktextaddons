5d1d68ed4c683c3b57e672ebf5bf9a5df2a273cd
Laurent Montel <montel@kde.org>
2026-05-07 08:40:40 +0200

    Assign model

--- textautogeneratetextmcpprotocol/widgets/server/mcpserverwidget.cpp
+++ textautogeneratetextmcpprotocol/widgets/server/mcpserverwidget.cpp
@@ -49,6 +49,7 @@ McpServerWidget::McpServerWidget(TextAutoGenerateTextMcpProtocolCore::McpServerM
     connect(addMcpServerButton, &QToolButton::clicked, this, &McpServerWidget::slotAddServer);
 
     mMcpServerListView->setObjectName(u"mMcpServerListView"_s);
+    mMcpServerListView->setModel(mModel);
     mainLayout->addWidget(mMcpServerListView);
 
     connect(mMcpServerListView, &McpServerListView::addServer, this, &McpServerWidget::slotAddServer);
