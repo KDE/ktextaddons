4339e28d0ebafebbd7f66f6c603d3526dafe56f6
Laurent Montel <montel@kde.org>
2026-05-26 13:35:09 +0200

    Add tests apps

---
+++ textaddonswidgets/whatsnewng/tests/whatsnewnggui.cpp
@@ -0,0 +1,26 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "whatsnewnggui.h"
+#include <QApplication>
+#include <QStandardPaths>
+
+WhatsNewNgGui::WhatsNewNgGui(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+WhatsNewNgGui::~WhatsNewNgGui() = default;
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+
+    WhatsNewNgGui w;
+    w.show();
+    return app.exec();
+}
