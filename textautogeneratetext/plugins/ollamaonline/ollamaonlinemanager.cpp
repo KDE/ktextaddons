8e7ff7fc57eea5c6e5b46e8b2136ec3efdf80f79
Laurent Montel <montel@kde.org>
2026-06-05 07:44:47 +0200

    Debug--

--- textautogeneratetext/plugins/ollamaonline/ollamaonlinemanager.cpp
+++ textautogeneratetext/plugins/ollamaonline/ollamaonlinemanager.cpp
@@ -140,7 +140,6 @@ TextAutoGenerateText::TextAutoGenerateReply *OllamaOnlineManager::getChatComplet
         OllamaCommonReply::RequestTypes::StreamingChat,
         this};
     connect(reply, &OllamaCommonReply::finished, this, [this, reply]() {
-        qDebug() << " FINISHED******";
         Q_EMIT finished(reply->readResponse());
     });
     connect(reply, &OllamaCommonReply::errorOccurred, this, [this](QNetworkReply::NetworkError e, const QString &errorString) {
