9804929e36e45e1d484af3702d4b30a6295aad70
Laurent Montel <montel@kde.org>
2026-06-08 20:05:12 +0200

    Continue to add mcpprotocolelicitrequestformparams

--- textautogeneratetextmcpprotocol/core/impl/mcpprotocolelicitrequestformparams.cpp
+++ textautogeneratetextmcpprotocol/core/impl/mcpprotocolelicitrequestformparams.cpp
@@ -5,6 +5,7 @@
 */
 #include "mcpprotocolelicitrequestformparams.h"
 #include <QDebug>
+#include <QJsonObject>
 
 using namespace Qt::Literals::StringLiterals;
 using namespace TextAutoGenerateTextMcpProtocolCore;
@@ -15,4 +16,46 @@ QDebug operator<<(QDebug d, const TextAutoGenerateTextMcpProtocolCore::McpProtoc
 {
     // TODO
     return d;
+}
+
+std::optional<McpProtocolUtils::ProgressToken> McpProtocolElicitRequestFormParams::Meta::progressToken() const
+{
+    return mProgressToken;
+}
+
+void McpProtocolElicitRequestFormParams::Meta::setProgressToken(std::optional<McpProtocolUtils::ProgressToken> newProgressToken)
+{
+    mProgressToken = std::move(newProgressToken);
+}
+
+McpProtocolElicitRequestFormParams McpProtocolElicitRequestFormParams::fromJson(const QJsonObject &obj)
+{
+    // TODO
+    return {};
+}
+
+QJsonObject McpProtocolElicitRequestFormParams::toJson(const McpProtocolElicitRequestFormParams &image)
+{
+    // TODO
+    return {};
+}
+
+QString McpProtocolElicitRequestFormParams::message() const
+{
+    return mMessage;
+}
+
+void McpProtocolElicitRequestFormParams::setMessage(const QString &newMessage)
+{
+    mMessage = newMessage;
+}
+
+std::optional<McpProtocolTaskMetadata> McpProtocolElicitRequestFormParams::task() const
+{
+    return mTask;
+}
+
+void McpProtocolElicitRequestFormParams::setTask(std::optional<McpProtocolTaskMetadata> newTask)
+{
+    mTask = std::move(newTask);
 }
--- textautogeneratetextmcpprotocol/core/impl/mcpprotocolelicitrequestformparams.h
+++ textautogeneratetextmcpprotocol/core/impl/mcpprotocolelicitrequestformparams.h
@@ -7,6 +7,8 @@
 
 #include "textautogeneratetextmcpprotocolcore_export.h"
 #include <QString>
+#include <TextAutoGenerateTextMcpProtocolCore/McpProtocolTaskMetadata>
+#include <TextAutoGenerateTextMcpProtocolCore/McpProtocolUtils>
 class QDebug;
 class QJsonObject;
 namespace TextAutoGenerateTextMcpProtocolCore
@@ -14,8 +16,53 @@ namespace TextAutoGenerateTextMcpProtocolCore
 class TEXTAUTOGENERATETEXTMCPPROTOCOLCORE_EXPORT McpProtocolElicitRequestFormParams
 {
 public:
+    struct Meta {
+        std::optional<McpProtocolUtils::ProgressToken> mProgressToken;
+
+        [[nodiscard]] std::optional<McpProtocolUtils::ProgressToken> progressToken() const;
+        void setProgressToken(std::optional<McpProtocolUtils::ProgressToken> newProgressToken);
+        [[nodiscard]] bool operator==(const McpProtocolElicitRequestFormParams::Meta &other) const;
+
+        /*!
+         */
+        [[nodiscard]] static McpProtocolElicitRequestFormParams::Meta fromJson(const QJsonObject &obj);
+        /*!
+         */
+        [[nodiscard]] static QJsonObject toJson(const McpProtocolElicitRequestFormParams::Meta &image);
+    };
+    /*!
+     */
     McpProtocolElicitRequestFormParams();
+    /*!
+     */
     ~McpProtocolElicitRequestFormParams();
+
+    /*!
+     */
+    [[nodiscard]] static McpProtocolElicitRequestFormParams fromJson(const QJsonObject &obj);
+    /*!
+     */
+    [[nodiscard]] static QJsonObject toJson(const McpProtocolElicitRequestFormParams &image);
+
+    /*!
+     */
+    [[nodiscard]] QString message() const;
+    /*!
+     */
+    void setMessage(const QString &newMessage);
+
+    /*!
+     */
+    [[nodiscard]] std::optional<McpProtocolTaskMetadata> task() const;
+    /*!
+     */
+    void setTask(std::optional<McpProtocolTaskMetadata> newTask);
+
+private:
+    std::optional<Meta> mMeta;
+    QString mMessage;
+    // TODO RequestedSchema _requestedSchema;
+    std::optional<McpProtocolTaskMetadata> mTask;
 };
 }
 
