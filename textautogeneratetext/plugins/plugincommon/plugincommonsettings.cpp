80a548d8bf5902eba31c12900cdcc316a9356965
Laurent Montel <montel@kde.org>
2026-06-12 13:16:41 +0200

    Add plugincommonsettings

---
+++ textautogeneratetext/plugins/plugincommon/plugincommonsettings.cpp
@@ -0,0 +1,52 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "plugincommonsettings.h"
+using namespace Qt::Literals::StringLiterals;
+
+PluginCommonSettings::PluginCommonSettings() = default;
+PluginCommonSettings::~PluginCommonSettings() = default;
+
+PluginCommonSettings::ShareNameType PluginCommonSettings::shareNameType() const
+{
+    return mShareNameType;
+}
+
+PluginCommonSettings::ShareNameType PluginCommonSettings::defaultShareNameType() const
+{
+    return PluginCommonSettings::ShareNameType::DoNotShare;
+}
+
+void PluginCommonSettings::setShareNameType(const PluginCommonSettings::ShareNameType &newShareNameType)
+{
+    mShareNameType = newShareNameType;
+}
+
+PluginCommonSettings::ShareNameType PluginCommonSettings::convertShareNameTypeFromString(const QString &str)
+{
+    if (str == "DoNotShare"_L1) {
+        return PluginCommonSettings::ShareNameType::DoNotShare;
+    } else if (str == "UserName"_L1) {
+        return PluginCommonSettings::ShareNameType::UserName;
+    } else if (str == "FullName"_L1) {
+        return PluginCommonSettings::ShareNameType::FullName;
+    }
+    return PluginCommonSettings::ShareNameType::DoNotShare;
+}
+
+QString PluginCommonSettings::convertShareNameTypeToString(PluginCommonSettings::ShareNameType type)
+{
+    switch (type) {
+    case ShareNameType::DoNotShare:
+        return u"DoNotShare"_s;
+    case ShareNameType::UserName:
+        return u"UserName"_s;
+    case ShareNameType::FullName:
+        return u"FullName"_s;
+    }
+
+    return {};
+}
---
+++ textautogeneratetext/plugins/plugincommon/plugincommonsettings.h
@@ -0,0 +1,34 @@
+/*
+  SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include "textautogenerateplugincommon_export.h"
+#include <QString>
+#include <QUrl>
+class TEXTAUTOGENERATEPLUGINCOMMON_EXPORT PluginCommonSettings
+{
+    Q_GADGET
+public:
+    enum class ShareNameType : uint8_t {
+        DoNotShare = 0,
+        UserName,
+        FullName,
+    };
+    Q_ENUM(ShareNameType)
+
+    PluginCommonSettings();
+    ~PluginCommonSettings();
+
+    [[nodiscard]] PluginCommonSettings::ShareNameType shareNameType() const;
+    void setShareNameType(const PluginCommonSettings::ShareNameType &newShareNameType);
+    [[nodiscard]] ShareNameType defaultShareNameType() const;
+
+    [[nodiscard]] static PluginCommonSettings::ShareNameType convertShareNameTypeFromString(const QString &str);
+    [[nodiscard]] static QString convertShareNameTypeToString(PluginCommonSettings::ShareNameType type);
+
+protected:
+    PluginCommonSettings::ShareNameType mShareNameType = ShareNameType::DoNotShare;
+};
