6de8522f5150e13cc37aaf20e52410c063eb1ca9
Laurent Montel <montel@kde.org>
2026-07-17 13:09:51 +0200

    Use a QToolButton

--- textautogeneratetext/widgets/autotests/textautogeneratetextlineeditattachmentclickablewidgettest.cpp
+++ textautogeneratetext/widgets/autotests/textautogeneratetextlineeditattachmentclickablewidgettest.cpp
@@ -8,6 +8,7 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QTest>
+#include <QToolButton>
 using namespace Qt::Literals::StringLiterals;
 
 QTEST_MAIN(TextAutoGenerateTextLineEditAttachmentClickableWidgetTest)
@@ -24,14 +25,16 @@ void TextAutoGenerateTextLineEditAttachmentClickableWidgetTest::shouldHaveDefaul
     QVERIFY(mainLayout);
     QCOMPARE(mainLayout->contentsMargins(), QMargins{});
 
-    auto mClickableLabel = w.findChild<TextAutoGenerateText::TextAutoGenerateTextLineEditAttachmentClickableLabel *>(u"mClickableLabel"_s);
-    QVERIFY(mClickableLabel);
-
     auto mFileNameLabel = w.findChild<QLabel *>(u"mFileNameLabel"_s);
     QVERIFY(mFileNameLabel);
 
     auto mMimetypeLabel = w.findChild<QLabel *>(u"mMimetypeLabel"_s);
     QVERIFY(mMimetypeLabel);
+
+    auto removeBtn = w.findChild<QToolButton *>(u"removeBtn"_s);
+    QVERIFY(removeBtn);
+    QVERIFY(removeBtn->autoRaise());
+    QVERIFY(!removeBtn->toolTip().isEmpty());
 }
 
 #include "moc_textautogeneratetextlineeditattachmentclickablewidgettest.cpp"
