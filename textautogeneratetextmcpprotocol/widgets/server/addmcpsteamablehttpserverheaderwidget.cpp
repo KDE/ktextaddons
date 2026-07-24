f5e0da5efb5d44f73f784136970a3f6f042535ec
Laurent Montel <montel@kde.org>
2026-07-24 13:22:31 +0200

    Prepare buttons

--- textautogeneratetextmcpprotocol/widgets/server/addmcpsteamablehttpserverheaderwidget.cpp
+++ textautogeneratetextmcpprotocol/widgets/server/addmcpsteamablehttpserverheaderwidget.cpp
@@ -7,6 +7,7 @@
 #include "addmcpsteamablehttpserverheaderwidget.h"
 #include <KLocalizedString>
 #include <QListWidget>
+#include <QPushButton>
 #include <QVBoxLayout>
 using namespace Qt::Literals::StringLiterals;
 using namespace TextAutoGenerateTextMcpProtocolWidgets;
@@ -14,12 +15,29 @@ AddMcpSteamableHttpServerHeaderWidget::AddMcpSteamableHttpServerHeaderWidget(QWi
     : QWidget{parent}
     , mListBox(new QListWidget(this))
 {
-    auto mainLayout = new QVBoxLayout(this);
+    auto mainLayout = new QHBoxLayout(this);
     mainLayout->setObjectName(u"mainLayout"_s);
     mainLayout->setContentsMargins(QMargins{});
 
     mListBox->setObjectName(u"mListBox"_s);
     mainLayout->addWidget(mListBox);
+
+    auto buttonsLayout = new QVBoxLayout;
+    buttonsLayout->setObjectName(u"buttonsLayout"_s);
+    buttonsLayout->setContentsMargins(QMargins{});
+
+    auto addHeaderButton = new QPushButton(i18nc("@action:button", "Add"), this);
+    addHeaderButton->setObjectName(u"addHeaderButton"_s);
+    buttonsLayout->addWidget(addHeaderButton);
+
+    auto modifyHeaderButton = new QPushButton(i18nc("@action:button", "Modify"), this);
+    modifyHeaderButton->setObjectName(u"modifyHeaderButton"_s);
+    buttonsLayout->addWidget(modifyHeaderButton);
+
+    auto removeHeaderButton = new QPushButton(i18nc("@action:button", "Remove"), this);
+    removeHeaderButton->setObjectName(u"removeHeaderButton"_s);
+    buttonsLayout->addWidget(removeHeaderButton);
+    buttonsLayout->addStretch(1);
 }
 
 AddMcpSteamableHttpServerHeaderWidget::~AddMcpSteamableHttpServerHeaderWidget() = default;
