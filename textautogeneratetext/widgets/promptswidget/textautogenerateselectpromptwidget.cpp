4c432ac70ae888fb3716fe952513b6d90a6ad3ec
Laurent Montel <montel@kde.org>
2026-07-29 13:19:26 +0200

    Add widget

--- textautogeneratetext/widgets/promptswidget/textautogenerateselectpromptwidget.cpp
+++ textautogeneratetext/widgets/promptswidget/textautogenerateselectpromptwidget.cpp
@@ -4,11 +4,20 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogenerateselectpromptwidget.h"
+#include "textautogenerateselectpromptlistwidget.h"
 #include <QVBoxLayout>
 using namespace TextAutoGenerateText;
+using namespace Qt::Literals::StringLiterals;
 TextAutoGenerateSelectPromptWidget::TextAutoGenerateSelectPromptWidget(QWidget *parent)
     : QWidget{parent}
+    , mListWidget(new TextAutoGenerateSelectPromptListWidget(this))
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(u"mainLayout"_s);
+    mainLayout->setContentsMargins({});
+
+    mListWidget->setObjectName(u"mainLayout"_s);
+    mainLayout->addWidget(mListWidget);
 }
 
 TextAutoGenerateSelectPromptWidget::~TextAutoGenerateSelectPromptWidget() = default;
--- textautogeneratetext/widgets/promptswidget/textautogenerateselectpromptwidget.h
+++ textautogeneratetext/widgets/promptswidget/textautogenerateselectpromptwidget.h
@@ -10,11 +10,15 @@
 #include <QWidget>
 namespace TextAutoGenerateText
 {
+class TextAutoGenerateSelectPromptListWidget;
 class TEXTAUTOGENERATETEXT_TESTS_EXPORT TextAutoGenerateSelectPromptWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit TextAutoGenerateSelectPromptWidget(QWidget *parent = nullptr);
     ~TextAutoGenerateSelectPromptWidget() override;
+
+private:
+    TextAutoGenerateSelectPromptListWidget *const mListWidget;
 };
 }
