509245edb19d9a41ba133d45d34cfb7eea1dd08a
Laurent Montel <montel@kde.org>
2026-07-31 13:20:37 +0200

    Add loaddialogsizeutils, allow to load default value when scale is != 100
    (cherry picked from commit d5df8f2d9ab0016e9985ee88dc6df9239d10b28a)

---
+++ textaddonswidgets/misc/loaddialogsizeutils.cpp
@@ -0,0 +1,25 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "loaddialogsizeutils.h"
+#include "textaddonswidgets_debug.h"
+#include <KConfigGroup>
+#include <KSharedConfig>
+#include <KWindowConfig>
+#include <QScreen>
+#include <QWindow>
+
+void TextAddonsWidgets::LoadDialogSizeUtils::loadDialogSizeScaled(QWidget *w, const QString &key, int width, int height)
+{
+    if (!w || !w->windowHandle()) {
+        qCWarning(TEXTADDONSWIDGETS_LOG) << "widget is not define or windowHandle not defined. It's a bug";
+        return;
+    }
+    const qreal scaleFactor = w->windowHandle()->screen()->devicePixelRatio();
+    w->windowHandle()->resize(QSize(width * scaleFactor, height * scaleFactor));
+    const KConfigGroup group(KSharedConfig::openStateConfig(), key);
+    KWindowConfig::restoreWindowSize(w->windowHandle(), group);
+    w->resize(w->windowHandle()->size()); // workaround for QTBUG-40584
+}
