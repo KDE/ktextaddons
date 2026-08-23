ba7f61da43ffb1aac2240793823bfada622e72c0
Laurent Montel <montel@kde.org>
2026-08-23 21:53:08 +0200

    Fix warning

--- textaddonswidgets/verifynewversion/verifynewversionutils.cpp
+++ textaddonswidgets/verifynewversion/verifynewversionutils.cpp
@@ -1,19 +1,19 @@
 /*
-  SPDX-FileCopyrightText: 2024-2026 Laurent Montel <montel@kde.org>
+    SPDX-FileCopyrightText: 2024-2026 Laurent Montel <montel@kde.org>
 
-  SPDX-License-Identifier: GPL-2.0-or-later
-  code based on kdenlive
+    SPDX-License-Identifier: GPL-2.0-or-later
+    code based on kdenlive
 */
 
 #include "verifynewversionutils.h"
 
 #include <QUrl>
-using namespace Qt::Literals::StringLiterals;
 using namespace TextAddonsWidgets;
 bool VerifyNewVersionUtils::canVerifyNewVersion()
 {
 #if defined(Q_OS_WIN) || defined(Q_OS_MACOS)
     return true;
-#endif
+#else
     return false;
+#endif
 }
