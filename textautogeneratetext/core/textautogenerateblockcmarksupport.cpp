ebe7c96cb4c3b970cdf11c1c23dca0d979c29660
Laurent Montel <montel@kde.org>
2026-09-02 13:26:50 +0200

    Don't recalculate all the time highlighter

--- textautogeneratetext/core/textautogenerateblockcmarksupport.cpp
+++ textautogeneratetext/core/textautogenerateblockcmarksupport.cpp
@@ -202,13 +202,13 @@ QString TextAutoGenerateBlockCMarkSupport::addHighlighter(const QString &str,
         userHighlightStringIndexBackgroundColor = scheme.background(KColorScheme::PositiveBackground).color().name();
     }
 
+    auto definition = TextUtils::TextUtilsSyntaxHighlightingManager::self()->def(language);
+    if (!definition.isValid()) {
+        definition = TextUtils::TextUtilsSyntaxHighlightingManager::self()->defaultDef();
+    }
+    qCDebug(TEXTAUTOGENERATETEXT_CORE_CMARK_LOG) << " definition.name() " << definition.name();
+    highlighter.setDefinition(definition);
     auto addCodeChunk = [&](const QString &chunk) {
-        auto definition = TextUtils::TextUtilsSyntaxHighlightingManager::self()->def(language);
-        if (!definition.isValid()) {
-            definition = TextUtils::TextUtilsSyntaxHighlightingManager::self()->defaultDef();
-        }
-        qCDebug(TEXTAUTOGENERATETEXT_CORE_CMARK_LOG) << " definition.name() " << definition.name();
-        highlighter.setDefinition(definition);
         // Qt's support for borders is limited to tables, so we have to jump through some hoops...
         richTextStream << "<table><tr><td style='background-color:"_L1 << codeBackgroundColorName << "; padding: 5px; border: 1px solid "_L1 << codeBorderColor
                        << "'>"_L1 << highlight(chunk) << "</td></tr></table>"_L1;
