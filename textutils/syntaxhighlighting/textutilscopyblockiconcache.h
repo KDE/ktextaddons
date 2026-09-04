86f8931ed1e32b5f927fe8043b58d5d1d41d7453
Laurent Montel <montel@kde.org>
2026-09-04 13:11:59 +0200

    Fix compile on windows

--- textutils/syntaxhighlighting/textutilscopyblockiconcache.h
+++ textutils/syntaxhighlighting/textutilscopyblockiconcache.h
@@ -34,6 +34,11 @@ public:
      * \return Pointer to the singleton instance
      */
     static TextUtilsCopyBlockIconCache *self();
+
+    // The class is dllexported: without deleting them explicitly, MSVC instantiates
+    // the implicit copy members which can't copy the std::unique_ptr map.
+    TextUtilsCopyBlockIconCache(const TextUtilsCopyBlockIconCache &) = delete;
+    TextUtilsCopyBlockIconCache &operator=(const TextUtilsCopyBlockIconCache &) = delete;
     /*!
      * \brief Clears the cached icon
      */
@@ -47,6 +52,9 @@ public:
     [[nodiscard]] QString iconUrl(TextUtilsCopyBlockIconCache::IconType type);
 
 private:
+    TextUtilsCopyBlockIconCache() = default;
+    ~TextUtilsCopyBlockIconCache() = default;
+
     [[nodiscard]] TEXTUTILS_NO_EXPORT QString saveIconToTempFile(TextUtilsCopyBlockIconCache::IconType type);
     [[nodiscard]] TEXTUTILS_NO_EXPORT QString iconName(TextUtilsCopyBlockIconCache::IconType type) const;
     QMap<IconType, QString> mIconUrlMap;
