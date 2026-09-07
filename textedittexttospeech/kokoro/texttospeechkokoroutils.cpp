55fdf541fc8e8293bc76cbe17a3120cf468ed9ba
Laurent Montel <montel@kde.org>
2026-09-07 07:45:57 +0200

    Prepare utils class

---
+++ textedittexttospeech/kokoro/texttospeechkokoroutils.cpp
@@ -0,0 +1,12 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "texttospeechkokoroutils.h"
+
+using namespace TextEditTextToSpeech;
+
+TextToSpeechKokoroUtils::TextToSpeechKokoroUtils()
+{
+}
