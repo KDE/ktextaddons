58968f816df249f711ac0f524e6d407b04171fd2
Laurent Montel <montel@kde.org>
2026-09-08 19:24:22 +0200

    Add more autotest

---
+++ textedittexttospeech/kokoro/autotests/texttospeechkokoroinstallpythondialogtest.cpp
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "texttospeechkokoroinstallpythondialogtest.h"
+#include <QTest>
+QTEST_MAIN(TextToSpeechKokoroInstallPythonDialogTest)
+
+TextToSpeechKokoroInstallPythonDialogTest::TextToSpeechKokoroInstallPythonDialogTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+#include "moc_texttospeechkokoroinstallpythondialogtest.cpp"
---
+++ textedittexttospeech/kokoro/autotests/texttospeechkokoroinstallpythondialogtest.h
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class TextToSpeechKokoroInstallPythonDialogTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextToSpeechKokoroInstallPythonDialogTest(QObject *parent = nullptr);
+    ~TextToSpeechKokoroInstallPythonDialogTest() override = default;
+};
