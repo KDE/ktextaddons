30fad0322f69ccb3c89f42410162db8ce52241fa
Laurent Montel <montel@kde.org>
2026-09-12 09:18:18 +0200

    Remove const for allowing std::move

--- textspeechtotext/speechtotext/plugins/vosk/voskengineutils.cpp
+++ textspeechtotext/speechtotext/plugins/vosk/voskengineutils.cpp
@@ -74,7 +74,7 @@ QVector<VoskEngineUtils::LanguageInstalled> VoskEngineUtils::languageLocallyStor
     for (const auto &name : list) {
         // qCDebug(LIBVOSKSPEECHTOTEXT_LOG) << " name " << dir;
         const QString modelLanguagePath{dir.absolutePath() + u'/' + name};
-        if (const VoskEngineUtils::LanguageInstalled info = loadInstalledLanguageInfo(modelLanguagePath); info.isValid()) {
+        if (VoskEngineUtils::LanguageInstalled info = loadInstalledLanguageInfo(modelLanguagePath); info.isValid()) {
             languages.append(std::move(info));
         }
     }
