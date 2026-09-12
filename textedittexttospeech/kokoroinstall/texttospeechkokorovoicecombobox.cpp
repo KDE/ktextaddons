15a6b1d0aab3ef706edac3efdcc1d6dae1be2ad7
Laurent Montel <montel@kde.org>
2026-09-12 14:34:10 +0200

    Add gender in display name

--- textedittexttospeech/kokoroinstall/texttospeechkokorovoicecombobox.cpp
+++ textedittexttospeech/kokoroinstall/texttospeechkokorovoicecombobox.cpp
@@ -47,7 +47,7 @@ void TextToSpeechKokoroVoiceComboBox::fill()
 {
     const QList<TextToSpeechKokoroUtils::KokoroVoice> listVoices = TextToSpeechKokoroUtils::kokoroVoices();
     for (const auto &voice : listVoices) {
-        addItem(TextToSpeechKokoroUtils::voiceIcon(voice), voice.name, voice.identifier);
+        addItem(TextToSpeechKokoroUtils::voiceIcon(voice), i18nc("@info:tooltip <voice name> (<gender>)", "%1 (%2)", voice.name, genderName(voice.gender)));
         setItemData(count() - 1,
                     i18nc("@info:tooltip <voice name> (<language>, <gender>)", "%1 (%2, %3)", voice.name, localeName(voice.locale), genderName(voice.gender)),
                     Qt::ToolTipRole);
