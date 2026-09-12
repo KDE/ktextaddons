0963320c91796080cdaa30d17f0cd2fc5292049c
Laurent Montel <montel@kde.org>
2026-09-12 12:16:25 +0200

    We need a combobox which define voice

---
+++ textedittexttospeech/kokoroinstall/texttospeechkokorovoicecombobox.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "texttospeechkokorovoicecombobox.h"
+#include <KLocalizedString>
+
+using namespace TextEditTextToSpeech;
+TextToSpeechKokoroVoiceComboBox::TextToSpeechKokoroVoiceComboBox(QWidget *parent)
+    : QComboBox(parent)
+{
+}
+
+TextToSpeechKokoroVoiceComboBox::~TextToSpeechKokoroVoiceComboBox() = default;
+
+void TextToSpeechKokoroVoiceComboBox::fill()
+{
+    // TODO
+}
---
+++ textedittexttospeech/kokoroinstall/texttospeechkokorovoicecombobox.h
@@ -0,0 +1,25 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "kokoroinstalltexttospeech_export.h"
+#include <QComboBox>
+
+namespace TextEditTextToSpeech
+{
+class KOKOROINSTALLTEXTTOSPEECH_EXPORT TextToSpeechKokoroVoiceComboBox : public QComboBox
+{
+    Q_OBJECT
+public:
+    explicit TextToSpeechKokoroVoiceComboBox(QWidget *parent = nullptr);
+    ~TextToSpeechKokoroVoiceComboBox() override;
+
+private:
+    void fill();
+};
+
+}
