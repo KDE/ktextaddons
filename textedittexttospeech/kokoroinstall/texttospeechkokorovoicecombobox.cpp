9b3fc819301e31db6eb3e02e1b89b53d6a205643
Laurent Montel <montel@kde.org>
2026-09-12 12:30:55 +0200

    Prepare to add combobox from voice

--- textedittexttospeech/kokoroinstall/texttospeechkokorovoicecombobox.cpp
+++ textedittexttospeech/kokoroinstall/texttospeechkokorovoicecombobox.cpp
@@ -4,17 +4,25 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "texttospeechkokorovoicecombobox.h"
+#include "texttospeechkokoroutils.h"
 #include <KLocalizedString>
 
 using namespace TextEditTextToSpeech;
 TextToSpeechKokoroVoiceComboBox::TextToSpeechKokoroVoiceComboBox(QWidget *parent)
     : QComboBox(parent)
 {
+    fill();
 }
 
 TextToSpeechKokoroVoiceComboBox::~TextToSpeechKokoroVoiceComboBox() = default;
 
 void TextToSpeechKokoroVoiceComboBox::fill()
 {
-    // TODO
+    const QList<TextToSpeechKokoroUtils::KokoroVoice> listVoices = TextToSpeechKokoroUtils::kokoroVoices();
+    for (const auto &voice : listVoices) {
+        // TODO add icon from local + gender
+        addItem(voice.name, voice.name);
+    }
 }
+
+#include "moc_texttospeechkokorovoicecombobox.cpp"
