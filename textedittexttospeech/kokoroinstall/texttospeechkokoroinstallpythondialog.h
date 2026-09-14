2d5507cacfab36985e91b69cc35d831570716f9d
Volker Krause <vkrause@kde.org>
2026-09-14 20:03:48 +0200

    Export TextToSpeechKokoroInstallPythonDialog not just for tests
    It's used in KF6TextEditTextToSpeech.
    
    Should fix the seed builds on the CI (which have tests disabled).

--- textedittexttospeech/kokoroinstall/texttospeechkokoroinstallpythondialog.h
+++ textedittexttospeech/kokoroinstall/texttospeechkokoroinstallpythondialog.h
@@ -5,14 +5,14 @@
 */
 #pragma once
 
-#include "textedittexttospeech_kokoroinstall_private_export.h"
+#include "kokoroinstalltexttospeech_export.h"
 #include <QDialog>
 #include <QStringList>
 class QPushButton;
 namespace TextEditTextToSpeech
 {
 class TextToSpeechKokoroInstallPythonWidget;
-class KOKOROINSTALLTEXTTOSPEECH_TESTS_EXPORT TextToSpeechKokoroInstallPythonDialog : public QDialog
+class KOKOROINSTALLTEXTTOSPEECH_EXPORT TextToSpeechKokoroInstallPythonDialog : public QDialog
 {
     Q_OBJECT
 public:
