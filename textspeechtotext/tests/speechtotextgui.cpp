9592d68b7375584659a9092db28b79b185f339cb
Laurent Montel <montel@kde.org>
2026-09-15 13:16:10 +0200

    Improve test apps

--- textspeechtotext/tests/speechtotextgui.cpp
+++ textspeechtotext/tests/speechtotextgui.cpp
@@ -8,21 +8,36 @@
 #include "speechtotext/widgets/speechtotextconfiguredialog.h"
 #include <QApplication>
 #include <QCommandLineParser>
+#include <QPushButton>
 #include <QStandardPaths>
 #include <QTextEdit>
 #include <QVBoxLayout>
-
+#include <TextSpeechToText/SpeechToTextManager>
+#include <TextSpeechToText/SpeechToTextToolButtonWidget>
+using namespace Qt::Literals::StringLiterals;
 SpeechToTextGui::SpeechToTextGui(QWidget *parent)
     : QWidget(parent)
 {
     auto layout = new QVBoxLayout(this);
     auto textEdit = new QTextEdit(this);
+
+    auto configureButton = new QPushButton(u"Configure"_s, this);
+    connect(configureButton, &QPushButton::clicked, this, [this]() {
+        auto w = new TextSpeechToText::SpeechToTextConfigureDialog(this);
+        w->exec();
+        delete w;
+    });
+
+    layout->addWidget(configureButton, 0, Qt::AlignLeft);
+    auto speechToTextButton = new TextSpeechToText::SpeechToTextToolButtonWidget(this);
+    layout->addWidget(speechToTextButton, 0, Qt::AlignLeft);
     layout->addWidget(textEdit);
+    connect(TextSpeechToText::SpeechToTextManager::self(), &TextSpeechToText::SpeechToTextManager::textToSpeechDone, this, [textEdit](const QString &result) {
+        textEdit->append(result);
+    });
 }
 
-SpeechToTextGui::~SpeechToTextGui()
-{
-}
+SpeechToTextGui::~SpeechToTextGui() = default;
 
 int main(int argc, char **argv)
 {
