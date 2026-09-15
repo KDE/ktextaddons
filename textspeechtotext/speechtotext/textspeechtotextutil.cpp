4004e6fb2203f27a24b156e9c1cbc7e98292698a
Laurent Montel <montel@kde.org>
2026-09-15 13:22:31 +0200

    Save/load settings

--- textspeechtotext/speechtotext/textspeechtotextutil.cpp
+++ textspeechtotext/speechtotext/textspeechtotextutil.cpp
@@ -6,6 +6,9 @@
 
 #include "textspeechtotextutil.h"
 
+#include <KConfigGroup>
+#include <KSharedConfig>
+
 using namespace Qt::Literals::StringLiterals;
 using namespace TextSpeechToText;
 QString TextSpeechToTextUtil::groupTranslateName()
@@ -21,4 +24,26 @@ QString TextSpeechToTextUtil::engineTextToSpeechName()
 QString TextSpeechToTextUtil::defaultEngineName()
 {
     return {};
+}
+
+QString TextSpeechToTextUtil::soundGroupName()
+{
+    return u"Speech To Text"_s;
+}
+
+QString TextSpeechToTextUtil::loadEngineName()
+{
+    const KConfigGroup group(KSharedConfig::openConfig(), TextSpeechToTextUtil::groupTranslateName());
+    return group.readEntry(TextSpeechToTextUtil::engineTextToSpeechName(), TextSpeechToTextUtil::defaultEngineName());
+}
+
+void TextSpeechToTextUtil::saveEngineName(const QString &engineName)
+{
+    KConfigGroup group(KSharedConfig::openConfig(), TextSpeechToTextUtil::groupTranslateName());
+    if (engineName.isEmpty()) {
+        group.deleteEntry(TextSpeechToTextUtil::engineTextToSpeechName());
+    } else {
+        group.writeEntry(TextSpeechToTextUtil::engineTextToSpeechName(), engineName);
+    }
+    group.sync();
 }
