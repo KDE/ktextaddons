7c7d9b5b11d91629ae80a99e817d6cd26cb48813
Laurent Montel <montel@kde.org>
2026-09-16 20:55:10 +0200

    Don't normalize text which is hidden

--- textaddonswidgets/plugins/configurepluginstreewidgetsearchline.cpp
+++ textaddonswidgets/plugins/configurepluginstreewidgetsearchline.cpp
@@ -4,6 +4,7 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "configurepluginstreewidgetsearchline.h"
+#include <QTreeWidget>
 #include <QTreeWidgetItem>
 #include <TextUtils/ConvertText>
 using namespace TextAddonsWidgets;
@@ -20,23 +21,34 @@ bool ConfigurePluginsTreeWidgetSearchLine::itemMatches(const QTreeWidgetItem *it
         return true;
     }
 
+    const QTreeWidget *const treeWidget = item->treeWidget();
+    if (!treeWidget) {
+        return false;
+    }
+
+    const int columnCount = treeWidget->columnCount();
     const QString newPattern = TextUtils::ConvertText::normalize(pattern);
+    const Qt::CaseSensitivity sensitivity = caseSensitivity();
 
     // If the search column list is populated, search just the columns
     // specified.  If it is empty default to searching all of the columns.
 
     if (const QList<int> columns = searchColumns(); !columns.isEmpty()) {
-        QList<int>::ConstIterator it = columns.constBegin();
-        for (; it != columns.constEnd(); ++it) {
-            if (const QString currentText = TextUtils::ConvertText::normalize(item->text(*it)); *it < item->treeWidget()->columnCount() //
-                && currentText.indexOf(newPattern, 0, caseSensitivity()) >= 0) {
+        for (const int column : columns) {
+            if (column >= columnCount) {
+                continue;
+            }
+            // Normalizing allocates, so only do it for the columns we actually search.
+            if (const QString currentText = TextUtils::ConvertText::normalize(item->text(column)); currentText.indexOf(newPattern, 0, sensitivity) >= 0) {
                 return true;
             }
         }
     } else {
-        for (int i = 0; i < item->treeWidget()->columnCount(); i++) {
-            if (const QString currentText = TextUtils::ConvertText::normalize(item->text(i)); item->treeWidget()->columnWidth(i) > 0 //
-                && currentText.indexOf(newPattern, 0, caseSensitivity()) >= 0) {
+        for (int i = 0; i < columnCount; ++i) {
+            if (treeWidget->columnWidth(i) <= 0) {
+                continue;
+            }
+            if (const QString currentText = TextUtils::ConvertText::normalize(item->text(i)); currentText.indexOf(newPattern, 0, sensitivity) >= 0) {
                 return true;
             }
         }
