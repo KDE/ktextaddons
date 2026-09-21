af084c05555f1089102e61cc2d4003a87e8d80d8
Laurent Montel <montel@kde.org>
2026-09-21 19:50:50 +0200

    Serialize/deserialize tag

--- textautogeneratetext/core/tags/textautogeneratetag.cpp
+++ textautogeneratetext/core/tags/textautogeneratetag.cpp
@@ -4,10 +4,18 @@
   SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "textautogeneratetag.h"
+#include <QCborValue>
 #include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QUuid>
 
+using namespace Qt::Literals::StringLiterals;
 using namespace TextAutoGenerateText;
-TextAutoGenerateTag::TextAutoGenerateTag() = default;
+TextAutoGenerateTag::TextAutoGenerateTag()
+    : mIdentifier(QUuid::createUuid().toByteArray(QUuid::Id128))
+{
+}
 
 QString TextAutoGenerateTag::name() const
 {
@@ -39,9 +47,43 @@ void TextAutoGenerateTag::setColor(const QColor &newColor)
     mColor = newColor;
 }
 
+bool TextAutoGenerateTag::operator==(const TextAutoGenerateTag &other) const
+{
+    return other.mIdentifier == mIdentifier && other.mName == mName && other.mColor == mColor;
+}
+
+QByteArray TextAutoGenerateTag::serialize(const TextAutoGenerateTag &tag, bool toBinary)
+{
+    QJsonObject o;
+    o["identifier"_L1] = QString::fromLatin1(tag.mIdentifier);
+    o["name"_L1] = tag.mName;
+    if (tag.mColor.isValid()) {
+        o["color"_L1] = tag.mColor.name(QColor::HexArgb);
+    }
+
+    if (toBinary) {
+        return QCborValue::fromJsonValue(o).toCbor();
+    }
+    QJsonDocument d;
+    d.setObject(o);
+    return d.toJson(QJsonDocument::Indented);
+}
+
+TextAutoGenerateTag TextAutoGenerateTag::deserialize(const QJsonObject &o)
+{
+    TextAutoGenerateTag tag;
+    tag.setIdentifier(o["identifier"_L1].toString().toLatin1());
+    tag.setName(o["name"_L1].toString());
+    if (const QString colorName = o["color"_L1].toString(); !colorName.isEmpty()) {
+        tag.setColor(QColor::fromString(colorName));
+    }
+    return tag;
+}
+
 QDebug operator<<(QDebug d, const TextAutoGenerateText::TextAutoGenerateTag &t)
 {
     d.space() << "mName" << t.name();
     d.space() << "mIdentifier" << t.identifier();
+    d.space() << "mColor" << t.color();
     return d;
 }
--- textautogeneratetext/core/tags/textautogeneratetag.h
+++ textautogeneratetext/core/tags/textautogeneratetag.h
@@ -9,6 +9,7 @@
 #include <QColor>
 #include <QString>
 class QDebug;
+class QJsonObject;
 namespace TextAutoGenerateText
 {
 /*!
@@ -21,16 +22,64 @@ namespace TextAutoGenerateText
 class TEXTAUTOGENERATETEXT_EXPORT TextAutoGenerateTag
 {
 public:
+    /*!
+     * \brief TextAutoGenerateTag
+     */
     TextAutoGenerateTag();
 
+    /*!
+     * \brief name
+     * \return
+     */
     [[nodiscard]] QString name() const;
+    /*!
+     * \brief setName
+     * \param newName
+     */
     void setName(const QString &newName);
 
+    /*!
+     * \brief identifier
+     * \return
+     */
     [[nodiscard]] QByteArray identifier() const;
+    /*!
+     * \brief setIdentifier
+     * \param newIdentifier
+     */
     void setIdentifier(const QByteArray &newIdentifier);
 
+    /*!
+     * \brief color
+     * \return
+     */
     [[nodiscard]] QColor color() const;
+    /*!
+     * \brief setColor
+     * \param newColor
+     */
     void setColor(const QColor &newColor);
+
+    /*!
+     * Compares this TextAutoGenerateTag with another object for equality.
+     * \param other The TextAutoGenerateTag object to compare with
+     * \return true if both objects are equal, false otherwise
+     */
+    [[nodiscard]] bool operator==(const TextAutoGenerateTag &other) const;
+
+    /*!
+     * Serializes the given TextAutoGenerateTag object.
+     * \param tag The TextAutoGenerateTag object to serialize
+     * \param toBinary true to serialize to binary format, false for JSON format
+     * \return The serialized data as a QByteArray
+     */
+    static QByteArray serialize(const TextAutoGenerateTag &tag, bool toBinary = true);
+    /*!
+     * Deserializes a TextAutoGenerateTag object from JSON.
+     * \param source The JSON object to deserialize from
+     * \return The deserialized TextAutoGenerateTag object
+     */
+    [[nodiscard]] static TextAutoGenerateTag deserialize(const QJsonObject &source);
 
 private:
     QString mName;
