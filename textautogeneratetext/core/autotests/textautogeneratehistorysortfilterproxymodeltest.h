f9a12f065ee4bf98baaf8ef0417329bc2d50fa68
Laurent Montel <montel@kde.org>
2026-09-25 07:51:39 +0200

    Hide ephemeral in history

--- textautogeneratetext/core/autotests/textautogeneratehistorysortfilterproxymodeltest.h
+++ textautogeneratetext/core/autotests/textautogeneratehistorysortfilterproxymodeltest.h
@@ -18,5 +18,6 @@ public:
 private Q_SLOTS:
     void shouldHaveDefaultValues();
     void shouldFilterByTags();
+    void shouldHideEphemeralChats();
     void shouldDropChatOnProjectThroughTheWholeChain();
 };
