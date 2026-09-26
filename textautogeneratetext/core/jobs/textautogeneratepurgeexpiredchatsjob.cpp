7f5eb40f0d30ba0e86cdf5b31d26370e7acbd2e8
Laurent Montel <montel@kde.org>
2026-09-26 10:16:36 +0200

    implement logic for purge expired chat.
    WE need to exclude favorite (if we activated option) ephemeral chat

--- textautogeneratetext/core/jobs/textautogeneratepurgeexpiredchatsjob.cpp
+++ textautogeneratetext/core/jobs/textautogeneratepurgeexpiredchatsjob.cpp
@@ -5,10 +5,14 @@
  */
 
 #include "textautogeneratepurgeexpiredchatsjob.h"
+#include "core/localdatabase/textautogeneratelocaldatabasemanager.h"
+#include "core/models/textautogeneratechatsmodel.h"
+#include "core/textautogeneratemanager.h"
 
 using namespace TextAutoGenerateText;
-TextAutoGeneratePurgeExpiredChatsJob::TextAutoGeneratePurgeExpiredChatsJob(QObject *parent)
+TextAutoGeneratePurgeExpiredChatsJob::TextAutoGeneratePurgeExpiredChatsJob(TextAutoGenerateManager *manager, QObject *parent)
     : QObject{parent}
+    , mManager(manager)
 {
 }
 
@@ -26,7 +30,32 @@ void TextAutoGeneratePurgeExpiredChatsJob::setExcludeFavoriteChats(bool newExclu
 
 void TextAutoGeneratePurgeExpiredChatsJob::start()
 {
-    // TODO
+    if (mManager && mHistoryRetentionDays > 0) {
+        const QDateTime limitDateTime = QDateTime::currentDateTime().addDays(-mHistoryRetentionDays);
+        const auto chats = mManager->textAutoGenerateChatsModel()->chats();
+        QList<QByteArray> expiredChatIds;
+        for (const auto &chat : chats) {
+            // Ephemeral chats aren't stored, and a chat in progress is still in use.
+            if (chat.isEphemeral() || chat.inProgress() || (chat.favorite() && mExcludeFavoriteChats)) {
+                continue;
+            }
+            // Read the date from the database: messages are only loaded when a chat is opened.
+            qint64 lastDateTime = mManager->databaseManager()->lastMessageDateTime(chat.identifier());
+            if (lastDateTime <= 0) {
+                continue;
+            }
+            // Some persisted data may still contain millisecond timestamps.
+            if (lastDateTime > 100000000000LL) {
+                lastDateTime /= 1000;
+            }
+            if (QDateTime::fromSecsSinceEpoch(lastDateTime) < limitDateTime) {
+                expiredChatIds.append(chat.identifier());
+            }
+        }
+        for (const auto &chatId : std::as_const(expiredChatIds)) {
+            mManager->removeDiscussion(chatId);
+        }
+    }
     deleteLater();
 }
 
